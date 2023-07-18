c3351c3ab699a00a5932b0dc3dbe895385dfa156
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-18 23:13:35 +0200

    add: logo component

---
+++ components/logo.vue
@@ -0,0 +1,31 @@
+<template>
+    <img :src="logoPath" alt="Logo" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'default',
+    },
+    color: {
+        type: String,
+        default: 'color',
+    },
+});
+
+const allowedColors = ['black', 'white', 'color'];
+const allowedTypes = ['logo', 'symbol', 'logotype'];
+
+const logoPath = computed(() => {
+    const color = allowedColors.includes(props.color) ? props.color : 'color';
+    const type = allowedTypes.includes(props.type) ? props.type : 'logo';
+
+    // noviva uses different naming convention for logo files thus this if statement
+    if (type === 'logotype') {
+        return `/img/logos/color_logotype_${color}.svg`;
+    }
+
+    return `/img/logos/${color}_${type}.svg`;
+});
+</script>
