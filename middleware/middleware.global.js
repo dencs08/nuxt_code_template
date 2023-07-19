d3d78fb6f1ba7cb55c610c33baed86f66b6dec2d
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-19 14:18:03 +0200

    add: example global middleware

---
+++ middleware/middleware.global.js
@@ -0,0 +1,10 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    // console.log(from.name + " > " + to.name);
+    // if (to.name === "Start") {
+    // return navigateTo("/");
+    // }
+    // const pathNames: string[] = ["Login", "Calendar", "Register", "konto"];
+    // if (pathNames.includes(to.name as string)) {
+    //     return navigateTo({ name: "PageInProgress" });
+    // }
+});
