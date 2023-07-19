05700fe7c56ebac4a1d495f8e24430ee241fccc4
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-19 14:17:52 +0200

    update nuxt.config moved most config outside for better readibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import i18n from './config/i18n';
+import auth from './config/auth';
+import runtime from './config/runtime';
+import modules from './config/modules';
+import components from './config/components';
+import hooks from './config/hooks';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  runtime,
+  modules,
+  components,
+  i18n,
+  auth,
+  hooks,
+
+  css: ['@/assets/css/main.css'],
+
+  //for sitemap generation
+  site: {
+    url: import.meta.env.APP_URL,
+  },
+
+  devtools: {
+    enabled: true
+  },
+
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+
+  nitro: {
+    prerender: {
+      routes: ["/"],
+    },
+  },
+
+  ssr: false,
+
+  pinia: {
+    autoImports: ["defineStore", ["defineStore", "definePiniaStore"]],
+  },
 })
