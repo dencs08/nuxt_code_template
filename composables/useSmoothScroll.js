70ef57e3b48d6f6ab6c45231a5e2e52286af5493
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-19 14:16:00 +0200

    add: useSmoothScroll composable

---
+++ composables/useSmoothScroll.js
@@ -0,0 +1,27 @@
+export function useSmoothScroll() {
+    const scrollToElement = (elementId, behavior = 'smooth') => {
+        const element = document.getElementById(elementId);
+
+        if (element) {
+            const y = element.getBoundingClientRect().top + window.pageYOffset - 75;
+
+            window.scrollTo({ top: y, behavior });
+        }
+    }
+
+    onMounted(() => {
+        const hash = ref(window.location.hash)
+
+        if (hash.value) {
+            scrollToElement(hash.value.slice(1))
+        }
+
+        window.onhashchange = () => {
+            scrollToElement(window.location.hash.slice(1))
+        }
+    })
+
+    return {
+        scrollToElement
+    }
+}
