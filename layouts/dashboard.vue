e5983e2969ba46c7ec05c864444339a50c03a849
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-20 08:25:15 +0200

    add: dashboard layout

---
+++ layouts/dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <DashboardSidebar />
+        <div class="lg:pl-64">
+            <main class="pt-20 min-h-screen">
+                <div class="px-4 sm:px-6 lg:px-8 pb-8">
+                    <slot></slot>
+                </div>
+            </main>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
