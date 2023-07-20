61e1ff29dc8bcba1835cfc0e047b8b3af26323f9
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-20 08:24:15 +0200

    add: legal pages + menu

---
+++ pages/cookies-policy.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Cookies policy</h1>
+</template>
--- utils/menus.ts
+++ utils/menus.ts
@@ -2,9 +2,34 @@ export function useNavigation() {
     const localePath = useLocalePath()
 
     const navigation = computed(() => [
-        { name: 'Test', to: localePath('/lang-test') },
-        { name: 'Tt', to: localePath('/te-st') },
+        { name: 'Kontakt', to: localePath('/contact') },
     ])
 
-    return { navigation }
+    const legal = computed(() => [
+        { name: 'Polityka prywatności', href: localePath('/privacy-policy') },
+        { name: 'Warunki korzystania', href: localePath('/terms-and-conditions') },
+        { name: 'Polityka ciasteczek', href: localePath('/cookies-policy')},
+        { name: 'Ustawienia ciasteczek', href: localePath('/contact') },
+    ])
+
+    const social = computed(() => [
+        {
+            name: 'Facebook',
+            href: '#',
+            icon: 'ic:round-facebook'
+        },
+        {
+            name: 'Instagram',
+            href: '#',
+            icon:  'mdi:instagram'
+        },
+
+        {
+            name: 'LinkedIn',
+            href: '#',
+            icon:  'mdi:linkedin'
+        },
+    ])
+
+    return { navigation, legal, social }
 }
