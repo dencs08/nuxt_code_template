d0ce0fac66b26cd5d8a3bae3b4e6ca363fcf6e70
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-20 08:25:41 +0200

    add: login page

---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  auth: {
+    // unauthenticatedOnly: true,
+    // navigateAuthenticatedTo: '/dashboard',
+  }
+})
+</script>
+
+<template>
+  <section class="h-screen">
+    <div class="h-full overflow-hidden">
+      <AuthorizationLogin/>
+    </div>
+  </section>
+</template>
