dd97987a59357a0dda964f4656e88891e56de4ba
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-21 16:48:49 +0200

    update: dashboard layout slightly increased content space

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
         <DashboardSidebar />
-        <div class="lg:pl-64">
+        <div class="lg:pl-56">
             <main class="pt-20 min-h-screen">
                 <div class="px-4 sm:px-6 lg:px-8 pb-8">
+                    <Breadcrumbs class="mb-2"/>
                     <slot></slot>
                 </div>
             </main>
