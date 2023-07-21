ced814ad638f1d24771a53ace245912d825bd2a7
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-21 16:46:37 +0200

    update: routes.ts for login and contact pages

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,11 +1,17 @@
-export const routes = [{
-    name: 'langTest',
-    newName: 'langT',
-    path: '/lang-test',
-    file: '@/pages/langTest.vue',
-}, {
-    name: 'test',
-    newName: 'tt',
-    path: '/te-st',
-    file: '@/pages/test.vue',
-}]
+//when changing this file you need to restart the server it doesnt support hot reload yet.
+
+//extending page hook of nuxt
+export const routes = [
+    {
+        name: 'login', //actual page name
+        newName: 'login', //assign new name
+        path: '/login', //change the path
+        file: '@/pages/login.vue', //specify the file
+    },
+    {
+        name: 'contact', //actual page name
+        newName: 'contact', //assign new name
+        path: '/contact', //change the path
+        file: '@/pages/contact.vue', //specify the file
+    },
+]
