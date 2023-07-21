3ac2fe816a4f1abebfb67a47daf27eabce5157a5
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-21 16:49:21 +0200

    update: card component now supports better customazability

--- components/design-components/card.vue
+++ components/design-components/card.vue
@@ -1,10 +1,10 @@
 <template>
-    <Transition name="fade" :duration="125" mode="out-in">
+    <Transition name="fade" :duration="75" mode="out-in">
         <div v-if="props.loading">
             <AnimatedPlaceholder class="h-full w-full rounded-lg py-5 px-6" />
         </div>
-        <div v-else class="bg-white px-6 py-5 border border-gray-200 rounded-lg" :class="{ 'shadow-sm': shadow }">
-            <slot />
+        <div v-else class="px-6 py-5 rounded-lg" :class="[styling, { 'shadow-sm': shadow }]">
+          <slot />
         </div>
     </Transition>
 </template>
@@ -12,11 +12,15 @@
 const props = defineProps({
     shadow: {
         type: Boolean,
-        default: false,
+        default: true,
     },
     loading: {
         type: Boolean,
         default: false,
+    },
+    styling:{
+      type: String,
+      default: 'bg-white border border-gray-200'
     }
 })
 </script>
