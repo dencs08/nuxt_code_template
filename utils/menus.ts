92ea6abd72246c7b9b1f774e34f91f0ff9a24dc5
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-21 16:46:52 +0200

    add: menus for dashboard navigation

--- utils/menus.ts
+++ utils/menus.ts
@@ -31,5 +31,73 @@ export function useNavigation() {
         },
     ])
 
-    return { navigation, legal, social }
+    const dashboardNavigation = computed(() =>[
+        {
+            name: 'Main',
+            items: [
+                {
+                    name: 'Dashboard',
+                    to: localePath('/dashboard/home'),
+                    icon: 'ic:round-home'
+                },
+                {
+                    name: 'Reports',
+                    to: localePath('/dashboard/reports'),
+                    icon: 'ic:round-data-saver-off',
+                    children: [
+                        {
+                            name: 'test',
+                            to: localePath('/dashboard/reports/test')
+                        },
+                        {
+                            name: 'test2',
+                            to: localePath('/dashboard/reports/test2')
+                        }
+                    ]
+                },
+                {
+                    name: 'Marketing',
+                    to: localePath('/dashboard/marketing'),
+                    icon: 'ic:round-people'
+                },
+            ]
+        },
+        {
+            name: 'Administrative',
+            items: [
+                {
+                    name: 'Users',
+                    to: localePath('/dashboard/users'),
+                    icon: 'ic:round-data-saver-off',
+                    children: [
+                        {
+                            name: 'Create user',
+                            to: localePath('/dashboard/users')
+                        },
+                    ]
+                },
+            ]
+        }
+    ])
+
+    const dashboardSettings = computed(() =>[
+        {
+            name: 'Settings',
+            items: [
+                {
+                    name: 'Settings',
+                    to: localePath('/dashboard/settings'),
+                    icon: 'ic:round-settings',
+                    children: [
+                        {
+                            name: 'Sample settings',
+                            to: localePath('/dashboard/settings')
+                        },
+                    ]
+                },
+            ]
+        }
+    ])
+
+    return { navigation, legal, social, dashboardNavigation, dashboardSettings }
 }
