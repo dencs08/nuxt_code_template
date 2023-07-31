c6509016bd2551478bec24195dc9dcd0fd1a7096
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-07-31 17:13:29 +0200

    add: register page

---
+++ pages/register.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="h-screen">
+    <div class="h-full overflow-hidden">
+      <AuthorizationRegister/>
+    </div>
+  </section>
+</template>
