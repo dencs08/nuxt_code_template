fe95deb88d65e36dcdbc851813313193cd659888
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-08-01 20:15:11 +0200

    update: improve code structure in modules.ts

--- config/modules.ts
+++ config/modules.ts
@@ -1,16 +1,18 @@
 // config/modules.ts
 
 export default [
+    '@nuxtjs/supabase',
     "@nuxt/devtools",
-    // "@vite-pwa/nuxt",
-    // "nuxt-gtag",
     "@nuxtjs/robots",
     '@nuxt/image',
     "@nuxtjs/i18n",
     "@nuxtjs/tailwindcss",
-    // "@sidebase/nuxt-auth",
     "@pinia/nuxt",
     "@vueuse/nuxt",
     'nuxt-simple-sitemap',
-    "nuxt-icon"
+    "nuxt-icon",
+
+    // "@vite-pwa/nuxt", //uncomment for generating a PWA for mobiles.
+    // "nuxt-gtag", //uncomment for adding gtag integration
+    // "@sidebase/nuxt-auth", //uncomment for when not using supabase or other authentication.
 ];
