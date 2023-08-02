697ecb3515b4dae88d2daafc84889e5f402f974f
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-08-02 22:48:20 +0200

    update: contact page is not using auth

--- plugins/primeVue.ts
+++
@@ -1,7 +0,0 @@
-import PrimeVue from "primevue/config";
-import PrimeCalendar from 'primevue/calendar';
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, { ripple: true });
-    nuxtApp.vueApp.component("PrimeCalendar", PrimeCalendar);
-});
