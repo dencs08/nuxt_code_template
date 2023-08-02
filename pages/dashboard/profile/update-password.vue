3057d3c8a8c10da4c5e97638103697fef1803b7c
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-08-02 22:47:05 +0200

    add: update-password profile page

---
+++ pages/dashboard/profile/update-password.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dashboard'
+})
+</script>
+
+<template>
+  <section class="flex h-[80vh] flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md h-3/5">
+      <PasswordUpdate/>
+    </div>
+  </section>
+</template>
