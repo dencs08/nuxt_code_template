fcc2281f67d94003f88cd8da31be5f38a04d7aa3
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-08-07 23:52:31 +0200

    add: useSubmit composable

---
+++ composables/useSubmit.ts
@@ -0,0 +1,36 @@
+export function useSubmit() {
+    const { addToast } = useToastService();
+    const localePath = useLocalePath();
+
+    const defaultSuccessHandler = (message: string, redirectPath?: string, external?: boolean) => (response: any) => {
+        addToast('success', 'Success', message);
+        if (redirectPath) {
+            navigateTo(localePath(redirectPath), { external: external ?? false });
+        }
+    };
+
+    const defaultErrorHandler = (error: any) => {
+        const errorMessage = error.message || 'An unknown error occurred.';
+        addToast('error', 'Error', errorMessage);
+    };
+
+    async function handleSubmit(
+        action: (data: any) => Promise<any>,
+        data: any,
+        successMessage: string,
+        redirectPath?: string,
+        external?: boolean,
+        errorMessage?: string,
+        onSuccess: (response: any) => void = defaultSuccessHandler(successMessage, redirectPath, external),
+        onError: (error: any) => void = defaultErrorHandler
+    ) {
+        try {
+            const response = await action(data);
+            onSuccess(response);
+        } catch (error) {
+            onError(error);
+        }
+    }
+
+    return { handleSubmit };
+}
