8a735763fee30fd7cebd069b2e4b6335467b91a9
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-08-07 23:51:57 +0200

    add: authentication.ts service

---
+++ services/authentication.ts
@@ -0,0 +1,43 @@
+type AuthProvider = 'supabase' | 'firebase';
+import { Provider as OAuthProvider } from "@supabase/gotrue-js";
+
+interface IAuthenticationService {
+    signIn: (email: string, password: string) => Promise<any>;
+    signUp: (email: string, password: string) => Promise<any>;
+    signInWithOAuth: (provider: OAuthProvider) => Promise<any>;
+    signOut: () => Promise<any>;
+    resetPassword: (email: string) => Promise<any>;
+    updateUser: (payload: any) => Promise<any>;
+}
+
+type SupabaseClientType = ReturnType<typeof useSupabaseClient>;
+
+//if firebase or any other provider put them here
+// type FirebaseClientType = ReturnType<typeof useFirebaseClient>;
+// type ClientType = SupabaseClientType | FirebaseClientType;
+
+type ClientType = SupabaseClientType
+
+export function authenticationService(provider: AuthProvider): IAuthenticationService {
+    let client: ClientType;
+
+    switch (provider) {
+        case 'supabase':
+            client = useSupabaseClient();
+            break;
+        // case 'firebase':
+        //     client = useFirebaseClient();
+        //     break;
+        default:
+            throw new Error(`Unsupported auth provider: ${provider}`);
+    }
+
+    return {
+        signIn: (email, password) => client.auth.signInWithPassword({ email, password }),
+        signUp: (email, password) => client.auth.signUp({ email, password }),
+        signInWithOAuth: (oauthProvider: OAuthProvider) => client.auth.signInWithOAuth({ provider: oauthProvider }),
+        signOut: () => client.auth.signOut(),
+        resetPassword: (email) => client.auth.resetPasswordForEmail(email),
+        updateUser: (payload) => client.auth.updateUser(payload),
+    };
+}
