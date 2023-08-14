3c6902c7ac227e694f5aa84c0aca324d50a13c8a
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-08-14 23:33:42 +0200

    add: UserSessionStore

---
+++ stores/UserSessionStore.js
@@ -0,0 +1,35 @@
+export const useUsersStore = defineStore({
+    id: 'usersStore',
+
+    state: () => ({
+        user: [],
+        loading: false
+    }),
+
+    getters: {
+        userEmail(state) {
+            return state.user ? state.user.email : 'No email provided.';
+        },
+    },
+
+    actions: {
+        async fetchUser() {
+            this.loading = true
+
+            const client = useSupabaseClient();
+            const { data, error } = await client.from("users").select("*")
+
+            const {addToast} = useToastService();
+            if (error) {
+                addToast('error', 'error', error.message)
+            }
+
+            this.user = data
+            this.loading = false
+        },
+        addUser() {
+        },
+        deleteUser() {
+        },
+    }
+});
