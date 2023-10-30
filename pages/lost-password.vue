9cf3c042c87beba408bc0ac6e611cd5023b29278
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-10-30 18:17:39 +0100

    update: forgot password layout and styles

--- pages/lost-password.vue
+++ pages/lost-password.vue
@@ -5,13 +5,36 @@ definePageMeta({
     navigateAuthenticatedTo: '/dashboard/home'
   }
 })
+const localePath = useLocalePath()
+
 </script>
 
 <template>
   <section class="flex h-[80vh] flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      Forgot password
       <PasswordReset/>
     </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+:deep(.p-steps) {
+  .p-menuitem-link{
+    background-color: transparent !important;
+
+    &:focus{
+      outline: none !important;
+      box-shadow: none !important;
+    }
+  }
+
+  .p-steps-title{
+    opacity: 0;
+  }
+
+  .p-steps-number{
+    background: #f8f8f8 !important;
+    font-size: 0.85rem !important;
+  }
+}
+</style>
