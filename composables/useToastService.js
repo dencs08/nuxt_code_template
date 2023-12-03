97fb43faba41374097d220629d5d0a40d49ab23c
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-04 00:53:00 +0100

    update toastService default life

--- composables/useToastService.js
+++ composables/useToastService.js
@@ -4,8 +4,8 @@ export function useToastService() {
     const toast = useToast();
 
     //severity options: success, info, warn, error
-    const addToast = (severity, summary, detail) => {
-        toast.add({ severity, summary, detail, life: 3000 })
+    const addToast = (severity, summary, detail, life) => {
+        toast.add({ severity, summary, detail, life: life ? life : 5000 })
     }
 
     return { addToast };
