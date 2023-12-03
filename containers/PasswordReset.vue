0646e12b4d2c5f8fb1075227230cab5cc0bc4d07
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-04 00:52:23 +0100

    add: password reset form

--- containers/PasswordReset.vue
+++ containers/PasswordReset.vue
@@ -1,11 +1,41 @@
-<script setup lang="ts">
+<script setup>
+const localePath = useLocalePath()
+const {lostPassword} = useAuthentication();
+const {handleSubmit} = useSubmit();
 
+async function handleForm(data) {
+  await handleSubmit(lostPassword, { email: data.email }, 'Reset link sent');
+}
 </script>
 
 <template>
-Reset your password here!
+  <div class="flex flex-col gap-6 h-full">
+    <div>
+      <h1 class="text-clamp-3xl md:text-clamp-xl">Forgot password?</h1>
+      <p class="text-gray-600">No worries, you can reset it here.</p>
+    </div>
+
+    <div class="space-y-3">
+      <FormWrapper :handleSubmit="handleForm">
+        <template #default="{ getNode }">
+          <div class="mb-2">
+            <FormKit
+                class="w-full"
+                type='primeInputText'
+                name= 'email'
+                validation= 'required|email'
+                validationVisibility= 'submit'
+                placeholder= 'Email'
+                @node="getNode">
+            </FormKit>
+          </div>
+        </template>
+      </FormWrapper>
+
+      <NuxtLink :to="localePath('/login')" class="flex items-center justify-center gap-1 text-gray-600 text-sm group">
+        <Icon name="ic:round-arrow-back" class="h-auto w-5 group-hover:-translate-x-1 transition-transform duration-150"/>
+        <span>Back to log in</span>
+      </NuxtLink>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
