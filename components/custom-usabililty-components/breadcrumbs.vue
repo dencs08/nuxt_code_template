cfe6aaf112c7df42ab85c245ca992377f6f51ca3
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-15 16:58:05 +0100

    add: breadcrumbs.vue internalization integration

--- components/custom-usabililty-components/breadcrumbs.vue
+++ components/custom-usabililty-components/breadcrumbs.vue
@@ -12,12 +12,28 @@
 <script setup lang="ts">
 const route = useRoute()
 const localePath = useLocalePath()
+const { locale, getBrowserLocale } = useI18n()
+
+const breadcrumbs = ref([])
 
 const crumbs = computed(() => {
+  // Fetch supported locales from i18n module
+  const languageCodes = getBrowserLocale()
+
   const pathArray = route.path.split('/').filter(n => n)
+
+  // If the first element of pathArray is a language code, remove it.
+  if (languageCodes.includes(pathArray[0])) {
+    pathArray.shift()
+  }
+
   let breadcrumbs: any[] = []
   pathArray.forEach((path, index) => {
     let crumbPath = `/${pathArray.slice(0, index + 1).join('/')}`
+    // if current locale is not default locale, prefix crumbPath with locale
+    if (locale.value !== 'en') {
+      crumbPath = `/${locale.value}${crumbPath}`
+    }
     breadcrumbs.push({
       label: path,
       path: crumbPath
