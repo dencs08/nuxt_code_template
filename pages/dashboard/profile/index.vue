04f7119d71959d6d4e372e12e91c807542890677
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-15 17:06:18 +0100

    update: dashboard/profile initial functionality

--- pages/dashboard/profile/index.vue
+++ pages/dashboard/profile/index.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
-        Your profile
+      <h2 class="text-db-h2">Your profile</h2>
+      <NuxtLink :to="localePath('/dashboard/profile/update-password')" class="link-primary">Password reset</NuxtLink>
+
+      <div v-if="user !== null">
+        <pre>{{ user }}</pre>
+        <pre>{{ activity }}</pre>
+      </div>
     </div>
 </template>
-<script setup>
+<script setup lang="ts">
 definePageMeta({
     layout: "dashboard",
 });
+
+const localePath = useLocalePath()
+const {fetchUser} = useUser();
+
+const client = useSupabaseClient()
+const activity = ref(null);
+const user = ref(null)
+
+onMounted(async ()=>{
+  // user.value = await fetchUser();
+  user.value = await client.from("users").select("*");
+
+  activity.value = await client.from("userActivity").select("*");
+})
 </script>
 <style lang="">
     
