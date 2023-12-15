59e1db11b632339e41101fd684e0279238d8c517
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-15 16:59:05 +0100

    update: useAuthentication.js better error handling, code improvement

--- composables/useAuthentication.js
+++ composables/useAuthentication.js
@@ -1,79 +1,74 @@
+import { authenticationService } from '@/services/authentication';
+import {userService} from '@/services/insertUserMeta'
 export function useAuthentication() {
-    const client = useSupabaseClient();
+    const authService = authenticationService('supabase');
+    const { handleRequestError } = useRequests();
 
-    async function signIn(email,password){
-        const { error } = await client.auth.signInWithPassword({
-            email: email,
-            password: password,
-        });
-        if (error) {
-            throw error;
-        }
+    async function signIn({ email, password }) {
+        const response = await authService.signIn(email, password);
+        handleRequestError(response);
     }
 
-    async function signUp(email, password) {
-        try {
-            const { data, error } = await client.auth.signUp({
-                email: email,
-                password: password,
-            });
-            if (error) throw new Error(error.message);
-            return "Check your email to confirm your account.";
-        } catch (error) {
-            throw new Error(error.message);
-        }
+    async function signUp({ email, password }) {
+        const response = await authService.signUp(email, password);
+        handleRequestError(response);
     }
 
-    const signInWithOAuth = (provider) => {
-        client.auth.signInWithOAuth({ provider })
+    const signInWithOAuth = async provider => {
+        const response = await authService.signInWithOAuth(provider);
+        handleRequestError(response);
+    }
+
+    const signInWithOAuthPopup = async (provider) => {
+        const response = await authService.signInWithOAuthWithPopup(provider);
+        handleRequestError(response);
+        redirectInPopup(response.data.url);
     }
 
     const signOut = async () => {
-        const {error} = await client.auth.signOut();
+        const response = await authService.signOut();
+        handleRequestError(response);
     }
 
-    const lostPassword = async (email) => {
-        try {
-            const { data, error } = await client.auth.resetPasswordForEmail(email);
-            if (error) throw new Error(error.message);
-            return "Reset password instructions has been sent to your email.";
-        } catch (error) {
-            throw new Error(error.message);
-        }
+    async function lostPassword({email}) {
+        const response = await authService.resetPassword(email);
+        handleRequestError(response);
     }
 
-    const updatePassword = async (password) => {
-        try {
-            const { data, error } = await client.auth.updateUser({
-                password: password,
-            })
-            if (error) throw new Error(error.message);
-            return "Password has been updated.";
-        } catch (error) {
-            throw new Error(error.message);
-        }
+    async function updateProfile(payload) {
+        const response = await authService.updateUser(payload);
+        handleRequestError(response);
+        if (payload.password) return "Password has been updated.";
+        if (payload.email) return "Email has been updated.";
     }
 
-    const updateEmail = async (email) => {
-        try {
-            const { data, error } = await client.auth.updateUser({
-                email: email,
-                data: { route: 'world' }
-            })
-            if (error) throw new Error(error.message);
-            return "Email has been updated.";
-        } catch (error) {
-            throw new Error(error.message);
-        }
-    }
+    const updatePassword = password => updateProfile(password);
+    const updateEmail = email => updateProfile(email);
 
     return {
         signIn,
         signInWithOAuth,
+        signInWithOAuthPopup,
         signUp,
         signOut,
         lostPassword,
         updatePassword,
         updateEmail
     };
+}
+
+function redirectInPopup(url) {
+    // Define dimensions and positioning for the popup
+    const width = 600;
+    const height = 600;
+    const left = (window.innerWidth - width) / 2;
+    const top = (window.innerHeight - height) / 2;
+
+    const popup = window.open(url, '_blank', `width=${width},height=${height},left=${left},top=${top}`);
+
+    const customFunction = (event, session) => {
+        popup.close()
+    };
+    const {handleAuthStateChange} = useAuthListeners();
+    handleAuthStateChange('SIGNED_IN', customFunction)
 }
