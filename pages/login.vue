f0c9d2dc70e321139dfcd1ed4686b66987ea9f51
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-15 17:01:46 +0100

    update: login page no longer listen to auth events

--- pages/login.vue
+++ pages/login.vue
@@ -7,16 +7,17 @@ definePageMeta({
 })
 
 const customFunction = (event, session) => {
-  window.location.reload();
+  // console.log('signed in');
+  // window.location.reload();
 };
-const {handleAuthStateChange} = useAuthListeners();
-handleAuthStateChange('SIGNED_IN', customFunction)
+// const { handleAuthStateChange } = useAuthListeners();
+// handleAuthStateChange('SIGNED_IN', customFunction)
 </script>
 
 <template>
   <section class="flex h-[80vh] flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <AuthorizationLogin/>
+      <AuthorizationLogin />
     </div>
   </section>
 </template>
