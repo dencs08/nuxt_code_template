0e5eb968a43c5b085c3e4bf86ae5d9c7e8000d74
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-15 17:03:31 +0100

    update: nuxt config, prerendering options, pinia primevue and css imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,22 @@ export default defineNuxtConfig({
   // pwa, //when using nuxt pwa module
   // auth, //when using nuxt auth module
 
-  css: ['@/assets/css/main.css'],
+  css: [
+      '@/assets/css/main.css',
+      '@/assets/css/primeVue_theme.css',
+      'primeicons/primeicons.css',
+      // '@sfxcode/formkit-primevue/dist/sass/formkit-prime-inputs.scss',
+      // '@sfxcode/formkit-primevue/dist/sass/formkit-primevue.scss'
+  ],
+
+  build: {
+    transpile: ["primevue"]
+  },
+  primevue: {
+    config: {
+      ripple: false
+    }
+  },
 
   //for sitemap generation
   site: {
@@ -34,15 +49,35 @@ export default defineNuxtConfig({
     pageTransition: { name: "page", mode: "out-in" },
   },
 
-  nitro: {
-    prerender: {
-      routes: ["/"],
-    },
+  // nitro: {
+  //   prerender: {
+  //     routes: ["/"],
+  //   },
+  // },
+
+  routeRules: {
+    // Homepage pre-rendered at build time
+    '/': { prerender: true  },
+    '/contact': { prerender: true  },
+    // Product page generated on-demand, revalidates in background
+    // '/products/**': { swr: 3600 },
+    // // Blog post generated on-demand once until next deploy
+    // '/blog/**': { isr: true },
+    // // Admin dashboard renders only on client-side
+    // '/admin/**': { ssr: false },
+    // // Add cors headers on API routes
+    // '/api/**': { cors: true },
+    // // Redirects legacy urls
+    // '/old-page': { redirect: '/new-page' }
   },
 
-  ssr: false,
+  // ssr: false,
 
   pinia: {
     autoImports: ["defineStore", ["defineStore", "definePiniaStore"]],
   },
+
+  imports: {
+    dirs:['pinia']
+  }
 })
