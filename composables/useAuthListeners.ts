51f75db415bc74141d6f5e3c1aa1b1c2ce3554d1
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-15 16:59:32 +0100

    update: useAuthListeners.js possibility of canceling the subscription

--- composables/useAuthListeners.ts
+++ composables/useAuthListeners.ts
@@ -1,13 +1,23 @@
 export function useAuthListeners() {
     const client = useSupabaseClient();
 
-    const handleAuthStateChange = (authEvent, callback = () => {}) => {
-        client.auth.onAuthStateChange((event, session) => {
-            if (event == authEvent) callback(event, session);
-        })
-    }
+    const handleAuthStateChange = (
+        authEvent: string,
+        callback: (event: string, session: any) => void
+    ) => {
+        const authSubscription = client.auth.onAuthStateChange((event, session) => {
+            if (event === authEvent) callback(event, session);
+        }) as unknown as { cancel: () => void };
+
+        // Return a function to cancel the subscription
+        return () => {
+            if (authSubscription) {
+                // authSubscription.cancel(); // no way ATM to cancel in JS
+            }
+        };
+    };
 
     return {
-        handleAuthStateChange
+        handleAuthListener: handleAuthStateChange,
     };
 }
