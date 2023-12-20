989d683a51c4410de6ff3b0a140e5e348355a7fe
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-20 21:14:35 +0100

    add: getUserSession, updateUser, fetchUserSession functions in UsersStore

--- stores/UsersStore.js
+++ stores/UsersStore.js
@@ -3,7 +3,8 @@ export const useUsersStore = defineStore({
 
     state: () => ({
         users: [],
-        loading: false
+        loading: false,
+        userSession: null
     }),
 
     getters: {
@@ -12,6 +13,9 @@ export const useUsersStore = defineStore({
         },
         totalCount(state) {
             return state.users.length;
+        },
+        getUserSession: (state) => {
+            return state.userSession;
         }
     },
 
@@ -20,21 +24,73 @@ export const useUsersStore = defineStore({
             this.loading = true;
             const client = useSupabaseClient();
 
-            // Fetch user records from own 'users' table.
-            let { data: userData, error: userError } = await client.from('users').select('*');
-            if (userError) {
-                throw userError;
-            }
+            try {
+                let { data: userData, error: userError } = await client
+                    .from('users')
+                    .select(`
+                        *,
+                        user_roles!inner(
+                            role
+                        )
+                    `);
 
-            this.users = userData;
-            this.loading = false;
+                if (userError) {
+                    throw userError;
+                }
+
+                this.users = userData.map(user => ({
+                    ...user,
+                    role: user.user_roles.role,
+                    user_roles: undefined,
+                }));
+
+            } catch (error) {
+                throw error;
+            } finally {
+                this.loading = false;
+            }
         }),
         addUser() {
         },
         deleteUser() {
         },
-        updateUser() {
+        async updateUser(data) {
+            this.loading = true;
+            const client = useSupabaseClient();
+            const { data: sessionData, error: sessionError } = await client.auth.getSession();
 
+            if (sessionError || !sessionData || !sessionData.session || !sessionData.session.access_token) {
+                throw new Error(sessionError || 'Invalid session data');
+            }
+
+            const { data: updateData, pending, error: updateError, refresh } = useFetch('/api/update-user', {
+                method: 'POST',
+                body: {
+                    "token": sessionData.session.access_token
+                }
+            });
+
+            await pending.value;
+
+            if (updateError.value) {
+                throw new Error(updateError.value.message || 'Error updating user');
+            }
+
+            this.loading = false;
+            // return updateData.value || 'User updated successfully';
+        },
+        async fetchUserSession() {
+            this.loading = true;
+            const { userSession } = useUser();
+            try {
+                const user = await userSession();
+                this.userSession = user;
+            } catch (error) {
+                console.error('Error in fetchAuthenticatedUser:', error);
+                this.userSession = null;
+            } finally {
+                this.loading = false;
+            }
         }
     }
 });
