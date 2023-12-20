9c4789889395876911e633e74c05a0f1731ba238
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-20 21:10:44 +0100

    add: test-component for testing

---
+++ components/test-component.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useUsersStore } from "~/stores/UsersStore";
+const userStore = useUsersStore();
+const { handleSubmit } = useSubmit();
+
+const handleForm = async (data) => {
+    await handleSubmit(userStore.updateUser, { data }, 'User updated');
+};
+
+const fetchForm = async () => {
+    await handleSubmit(userStore.fetchUserSession, {}, 'User fetched');
+};
+
+const userSession = computed(() => userStore.getUserSession);
+</script>
+
+<template>
+    <div>
+        <FormWrapper :handleSubmit="handleForm" :submit-attrs="{ inputClass: 'btn-primary' }" submit-label="Update user">
+        </FormWrapper>
+
+        <FormWrapper :handleSubmit="fetchForm" :submit-attrs="{ inputClass: 'btn-primary' }" submit-label="fetch user">
+        </FormWrapper>
+    </div>
+</template>
