63d550ba4607b180704d0a9cb877e759c7e31a58
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-20 21:12:19 +0100

    add: userSession function in useUser

--- composables/useUser.ts
+++ composables/useUser.ts
@@ -1,13 +1,29 @@
 export function useUser() {
     const client = useSupabaseClient();
 
-    const fetchUser = async () => {
+    const userSession = async () => {
         interface User {
-            role?: string; // add more fields based on your user structure
+            id: string;
+            name: string;
+            email: string;
+            phone: string;
+            role?: string;
+            user_roles?: { role: string };
         }
 
         try {
-            const {data: user} = await client.from("users").select("*").single() as {data: User | null};
+            const { data: user } = (await client
+                .from("users")
+                .select(`*, user_roles!inner(role)`)
+                .single()) as {
+                data: User | null;
+            };
+
+            if (user) {
+                user.role = user.user_roles.role;
+                delete user.user_roles;
+            }
+
             return user;
         } catch (error) {
             if (error instanceof Error) {
@@ -18,5 +34,24 @@ export function useUser() {
         }
     };
 
-    return { fetchUser };
+    const fetchUser = async () => {
+        interface User {
+            role?: string; // add more fields based on your user structure
+        }
+
+        try {
+            const { data: user } = (await client.from("users").select("*").single()) as {
+                data: User | null;
+            };
+            return user;
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            } else {
+                throw error;
+            }
+        }
+    };
+
+    return { fetchUser, userSession };
 }
