8f2a356a4e6a4a4aae75927618e846fa495e651a
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-23 16:53:52 +0100

    fix: footer appearing on top

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,24 +10,25 @@ const title = computed(() => t('layouts.default.title', { title: t(route.meta.ti
 </script>
 
 <template>
-    <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
-    <Head>
-      <Title>{{ title }}</Title>
-      <template v-for="link in head.link" :key="link.id">
-        <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
-      </template>
-      <template v-for="meta in head.meta" :key="meta.id">
-        <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
-      </template>
-    </Head>
-    <Body>
-      <main>
-        <Navbar />
-        <slot></slot>
-        <section>
-          <Footer/>
-        </section>
-      </main>
-    </Body>
-    </Html>
+  <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+
+  <Head>
+    <Title>{{ title }}</Title>
+    <template v-for="link in head.link" :key="link.id">
+      <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+    </template>
+    <template v-for="meta in head.meta" :key="meta.id">
+      <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
+    </template>
+  </Head>
+
+  <Body>
+    <Navbar />
+    <main>
+      <slot />
+    </main>
+    <Footer />
+  </Body>
+
+  </Html>
 </template>
