8f0ac1fad68ff364f8f40226e3b665b26fbc938a
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-25 19:19:32 +0100

    remove: authListener from register page

--- pages/register.vue
+++ pages/register.vue
@@ -5,18 +5,12 @@ definePageMeta({
     navigateAuthenticatedTo: '/dashboard/home'
   }
 })
-
-const customFunction = (event, session) => {
-  window.location.reload();
-};
-const {handleAuthStateChange} = useAuthListeners();
-handleAuthStateChange('SIGNED_IN', customFunction)
 </script>
 
 <template>
   <section class="flex h-[80vh] flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <AuthorizationRegister/>
+      <AuthorizationRegister />
     </div>
   </section>
 </template>
