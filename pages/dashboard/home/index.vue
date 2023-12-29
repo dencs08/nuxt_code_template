88cc1d445798e7194220e8b1d311bd0ac1ba7479
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-29 14:43:27 +0100

    prettify index.vue

--- pages/dashboard/home/index.vue
+++ pages/dashboard/home/index.vue
@@ -2,37 +2,39 @@
     <div class="flex flex-col lg:flex-row gap-8">
         <div class="w-full lg:w-4/6 space-y-8">
             <div class="space-y-2">
-                <h5>Hi, {{user?.email}}</h5>
+                <h5>Hi, {{ user?.email }}</h5>
                 <h2 class="text-db-h2">Main information</h2>
                 <div class="flex gap-4 flex-wrap justify-center">
-<!--                    <NuxtLink v-for="element in information" class="flex flex-col flex-grow w-1/2 sm:w-1/3 xl:w-1/4"-->
-<!--                        :to="element.link">-->
-<!--                        <Stat :label="element.label" :icon="element.icon" :value="element.value" :change="element.change"-->
-<!--                            :changeType="element.changeType" :loading="loading"></Stat>-->
-<!--                    </NuxtLink>-->
+                    <!--                    <NuxtLink v-for="element in information" class="flex flex-col flex-grow w-1/2 sm:w-1/3 xl:w-1/4"-->
+                    <!--                        :to="element.link">-->
+                    <!--                        <Stat :label="element.label" :icon="element.icon" :value="element.value" :change="element.change"-->
+                    <!--                            :changeType="element.changeType" :loading="loading"></Stat>-->
+                    <!--                    </NuxtLink>-->
                 </div>
             </div>
 
-<!--            <TableApplicants :recent="true" />-->
-          <div class="space-y-2">
-            <h2 class="text-db-h2">Quick Actions</h2>
-            <HorizontalScroller :breakpoint="768">
-              <div class="flex gap-3 md:flex-wrap md:justify-center">
-                <NuxtLink v-for="action in quickActions" :to="action.link" class="flex-none md:flex-grow md:w-1/3 xl:w-1/4">
-                  <Card class="flex flex-col md:flex-row items-center gap-2 text-white transition-colors" :styling="action.color">
-                    <Icon :name="action.icon" class="h-6 w-auto"/>
-                    <p class="text-sm md:text-base">{{ action.title }}</p>
-                  </Card>
-                </NuxtLink>
-              </div>
-            </HorizontalScroller>
-          </div>
+            <!--            <TableApplicants :recent="true" />-->
+            <div class="space-y-2">
+                <h2 class="text-db-h2">Quick Actions</h2>
+                <HorizontalScroller :breakpoint="768">
+                    <div class="flex gap-3 md:flex-wrap md:justify-center">
+                        <NuxtLink v-for="action in quickActions" :to="action.link"
+                            class="flex-none md:flex-grow md:w-1/3 xl:w-1/4">
+                            <Card class="flex flex-col md:flex-row items-center gap-2 text-white transition-colors"
+                                :styling="action.color">
+                                <Icon :name="action.icon" class="h-6 w-auto" />
+                                <p class="text-sm md:text-base">{{ action.title }}</p>
+                            </Card>
+                        </NuxtLink>
+                    </div>
+                </HorizontalScroller>
+            </div>
         </div>
 
         <div class="w-full lg:w-1/3">
             <h2 class="text-db-h2">Actions</h2>
             <Card class="w-full">
-<!--                <Feed />-->
+                <!--                <Feed />-->
             </Card>
         </div>
     </div>
@@ -46,14 +48,14 @@ const user = useSupabaseUser();
 
 const information = ref([
     {
-    label: "Opened recruitments",
-    icon: "ic:round-supervisor-account",
-    // link: "DashboardSettings",
-}, {
-    label: "Candidates applied",
-    icon: "ic:round-supervisor-account",
-    // link: "DashboardSettings",
-}])
+        label: "Opened recruitments",
+        icon: "ic:round-supervisor-account",
+        // link: "DashboardSettings",
+    }, {
+        label: "Candidates applied",
+        icon: "ic:round-supervisor-account",
+        // link: "DashboardSettings",
+    }])
 
 onMounted(async () => {
     // await fetchRecruitments();
