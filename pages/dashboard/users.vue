a1d0be8fa30ca2c6fdc84d8425df9cfc3da53bf5
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-29 14:43:49 +0100

    update: users dashboard page with toolbar and confirmation of actions

--- pages/dashboard/users.vue
+++ pages/dashboard/users.vue
@@ -2,7 +2,7 @@
 // import { UserRole } from '@/utils/UserRoles.ts';
 import { useUsersStore } from "~/stores/UsersStore";
 const userStore = useUsersStore();
-const selectedProduct = ref();
+const selected = ref();
 
 definePageMeta({
   layout: "dashboard",
@@ -13,49 +13,46 @@ onMounted(async () => {
   // console.log(toRaw(userStore.users[0]));
 })
 
-const createUser = async () => {
-  const userPayload = {
-    email: 'test@test.pl',
-    first_name: 'first name test',
-    last_name: 'lastName',
-    photo: 'avatar_url'
-  };
-
-  const { data, error } = await client
-    .from('users')
-    .insert([userPayload]);
-  if (error) {
-    console.error('Error inserting records:', error);
-  } else {
-    console.log('Inserted records:', data);
-  }
-}
+const confirm = useConfirm();
+const confirm2 = (event) => {
+  confirm.require({
+    target: event.currentTarget,
+    message: 'Do you want to delete this record?',
+    icon: 'pi pi-info-circle',
+    acceptClass: 'p-button-danger p-button-sm',
+    accept: () => {
+      const selectedIds = selected.value.map(user => user.id);
+      selectedIds.forEach(id => {
+        userStore.deleteUser(id);
+      });
+      selected.value = [];
+    },
+    reject: () => {
+      selected.value = [];
+    }
+  });
+};
 </script>
 
 <template>
   <div>
-    <!--        <div v-if="loading">-->
-    <!--            <AnimatedPlaceholder class="h-6 w-full rounded" />-->
-    <!--        </div>-->
-
-    <!--        <div v-else-if="user.role === UserRole.ADMIN">-->
-    <!--            <UserCreator class="mb-3" />-->
-    <!--            <TableUsers />-->
-    <!--        </div>-->
-
-    <!--        <div v-else-if="user.role === UserRole.RECRUITER">-->
-    <!--            <TableUsers />-->
-    <!--        </div>-->
-
-    <!--        <div v-else-if="user.role === UserRole.SUBSCRIBER">-->
-    <!--            <TableUsers />-->
-    <!--        </div>-->
-
     <!--        <div v-else>-->
     <!--            You cannot view this resource.-->
     <!--        </div>-->
+    <Toolbar class="mb-4">
+      <template #start>
+        <UserCreator class="mr-2" />
+        <MyButton label="Delete" styling="danger" @click="confirm2" :disabled="!selected || !selected.length">
+          Delete
+        </MyButton>
+      </template>
+      <template #end>
+        <TestComponent />
 
-    <DataTable :value="userStore.users" v-model:selection="selectedProduct" dataKey="id" :loading="userStore.loading"
+        <!-- invite the user? -->
+      </template>
+    </Toolbar>
+    <DataTable :value="userStore.users" v-model:selection="selected" dataKey="id" :loading="userStore.loading"
       removableSort tableStyle="min-width: 50rem">
       <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
       <Column field="name" header="Name" :sortable="true" style="width: 20%"></Column>
@@ -64,15 +61,5 @@ const createUser = async () => {
       <Column field="created_at" header="Created at" :sortable="true" style="width: 20%"></Column>
       <Column field="role" header="Role" :sortable="true" style="width: 20%"></Column>
     </DataTable>
-
-    <!-- invite the user? -->
-    <!-- <Button @click="createUser">Create test user</Button> -->
-    <!-- <Button @click="updateUser">Update user</Button> -->
-
-    <div class="flex gap-4">
-      <TestComponent />
-      <UserCreator />
-    </div>
-
   </div>
 </template>
