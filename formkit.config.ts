7e0badb533b77e07968c13bc6e9cefbeca52a28b
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-12-29 14:41:23 +0100

    Update formkit.config.ts

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,11 +1,11 @@
 // formkit.config.ts
-import type { DefaultConfigOptions } from '@formkit/vue'
-import { primeInputs } from '@sfxcode/formkit-primevue'
-import { en,pl,de } from '@formkit/i18n'
+import type { DefaultConfigOptions } from "@formkit/vue";
+import { primeInputs } from "@sfxcode/formkit-primevue";
+import { en, pl, de } from "@formkit/i18n";
 
-import { generateClasses } from '@formkit/themes'
-import { genesisIcons } from '@formkit/icons'
-import myTailwindTheme from './custom_tailwindcss/formKit'
+import { generateClasses } from "@formkit/themes";
+import { genesisIcons } from "@formkit/icons";
+import myTailwindTheme from "./custom_tailwindcss/formKit";
 
 const config: DefaultConfigOptions = {
     icons: {
@@ -17,6 +17,6 @@ const config: DefaultConfigOptions = {
 
     inputs: primeInputs,
     locales: { de, pl, en },
-}
+};
 
-export default config
+export default config;
