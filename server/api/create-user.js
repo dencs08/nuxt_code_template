48705688ad5c5c2026b033b90c93b22844b5241a
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-05 21:56:19 +0100

    add: create-user.js server api

---
+++ server/api/create-user.js
@@ -0,0 +1,36 @@
+import { serverSupabaseServiceRole, serverSupabaseUser } from "#supabase/server";
+
+export default eventHandler(async (event) => {
+    const client = serverSupabaseServiceRole(event);
+    const body = await readBody(event);
+    const userSession = await serverSupabaseUser(event);
+
+    try {
+        const { data, error } = await client.auth.admin.createUser({
+            email: body.email,
+            password: body.password,
+            email_confirm: true,
+            user_metadata: {
+                full_name: body.name,
+                avatar_url: body.photo
+            }
+        })
+
+        if (error) {
+            throw new Error('Error creating user: ' + error.message);
+        }
+
+        const { error: roleError } = await client
+            .from('user_roles')
+            .update({ role: body.role })
+            .eq('user_id', data.user.id)
+
+        if (roleError) {
+            throw new Error('Error updating the role: ' + error.message);
+        }
+
+        return { response: 'User created', data: data.user.id, body: body.role };
+    } catch (err) {
+        return { error: 'An error occurred during the creation process', response: err.message };
+    }
+});
