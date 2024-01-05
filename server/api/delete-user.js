c79540c6cf8becea4c25b63a646885e2a83a6d41
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-05 22:00:00 +0100

    update: delete-user better error handling and actual api call

--- server/api/delete-user.js
+++ server/api/delete-user.js
@@ -2,17 +2,25 @@ import { serverSupabaseServiceRole, serverSupabaseUser } from "#supabase/server"
 
 export default eventHandler(async (event) => {
     const client = serverSupabaseServiceRole(event);
-    // const body = await readBody(event);
+    const body = await readBody(event);
     const userSession = await serverSupabaseUser(event);
 
     try {
-        const { data, error } = await client.auth.admin.deleteUser(userId)
-
+        const { data, error } = await client.auth.admin.deleteUser(body.userId);
         if (error) {
-            throw new Error('Error deleting user data');
+            throw new Error('Error deleting user data: ' + error.message);
         }
-        return { response: 'User updated' };
+
+        const { data: deletedUser, error: deleteError } = await client
+            .from('users')
+            .delete()
+            .eq('id', body.userId);
+        if (deleteError) {
+            throw new Error('Error deleting user from public.users: ' + deleteError.message);
+        }
+
+        return { response: 'User deleted', data: deletedUser };
     } catch (err) {
-        return { error: 'An error occurred during the deletion process' };
+        return { error: 'An error occurred during the deletion process', response: err.message };
     }
 });
