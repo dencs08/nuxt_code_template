361983848c0ca9dd8ef109952fd502cbbcf3326f
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-09 23:38:42 +0100

    add: color-mode-selector component for switching between darkmode

---
+++ components/custom-usabililty-components/color-mode-selector.vue
@@ -0,0 +1,10 @@
+<template>
+    <button @click="toggle()">
+        <Icon v-auto-animate :name="isDark ? 'ic:outline-dark-mode' : 'ic:outline-light-mode'" class="text-lg" />
+    </button>
+</template>
+
+<script setup>
+const isDark = useDark()
+const toggle = useToggle(isDark)
+</script>
