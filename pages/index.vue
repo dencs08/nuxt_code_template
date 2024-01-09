1049cec355c0dbeace3940ab8fdd4688cf8c773e
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-09 23:39:56 +0100

    add: DesignTableDisplay on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <section class="container mx-auto min-h-screen grid place-content-center text-center">
-      <h1 v-motion="slideTop({delay: 400})">
+      <h1 v-motion="slideTop({ delay: 400 })">
         {{ t('pages.index.title') }}
       </h1>
-      <p class="font-body text-3xl font-bold" v-motion="fade({delay: 425})">
+      <p class="font-body text-3xl font-bold" v-motion="fade({ delay: 425 })">
         {{ t('pages.index.description') }}
       </p>
-      <p class="font-accent text-2xl font-black" v-motion="slideBottom({delay: 450})">
+      <p class="font-accent text-2xl font-black" v-motion="slideBottom({ delay: 450 })">
         {{ $t('welcome') }}
       </p>
     </section>
+
+    <section class="container mx-auto">
+      <DesignTableDisplay />
+    </section>
+
     <section>
       Need to have for class registration of tailwind
       <p class="hover:shadow-sm hover:bg-blue-700">a</p>
@@ -40,5 +45,5 @@ useHead({
   ]
 })
 
-const { slideTop, slideLeft, slideRight,fade, slideBottom} = useAnimations();
+const { slideTop, slideLeft, slideRight, fade, slideBottom } = useAnimations();
 </script>
