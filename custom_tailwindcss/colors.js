2899683b541b4dd130953b0237142daa30b4716c
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-09 23:40:37 +0100

    update: further improvements into colors.js for more control over generating colors

--- custom_tailwindcss/colors.js
+++ custom_tailwindcss/colors.js
@@ -14,24 +14,34 @@ function rgbToHex(r, g, b) {
 export function generateColorVariants(colorDefinitions) {
     const variants = {};
 
+    let initialBlackFactor = 0.1;
+    let slowDownFactor = 0.9; //smaller the slowDownFactor, the slower the growth of blackFactor.
+    const maxDarkening = 0.9; //how dark the last 'black - colorVariant-950' variant will be (higher values = darker)
+    const maxWhitening = 0.95; //how white the first 'white - colorVariant-0' variant will be (higher values = lighter)
+
+    let blackFactorStep = (1 - initialBlackFactor) / ((950 - 500) / 50 - 1);
+
     colorDefinitions.forEach(({ colorName, mainHex }) => {
         const baseRgb = hexToRgb(mainHex);
 
-        // Start from 0, end at 950, increment by 50
         for (let i = 0; i <= 950; i += 50) {
-            // Skip the iteration if i is not a multiple of 100, except for 0, 50 and 950
             if (i % 100 !== 0 && i !== 50 && i !== 950) continue;
 
             let r, g, b;
             if (i <= 500) {
-                // Interpolate the base color with white for variants 0 to 500
-                const whiteFactor = (500 - i) / 500 * 0.85; // limit to 80% white
+                let whiteFactor = 1 - (0.05 + 0.95 * (i / 500));
+                if (whiteFactor > maxWhitening) whiteFactor *= maxWhitening;
+                // console.log(`whiteFactor: ${whiteFactor}`);
+
                 r = Math.round(baseRgb[0] * (1 - whiteFactor) + 255 * whiteFactor);
                 g = Math.round(baseRgb[1] * (1 - whiteFactor) + 255 * whiteFactor);
                 b = Math.round(baseRgb[2] * (1 - whiteFactor) + 255 * whiteFactor);
             } else {
-                // Interpolate the base color with black for variants 500 to 950
-                const blackFactor = (i - 500) / 450 * 0.85; // limit to 80% black
+                let blackFactor;
+                blackFactor = initialBlackFactor + blackFactorStep * Math.pow((i - 550) / 50, slowDownFactor);
+                if (blackFactor > maxDarkening) blackFactor *= maxDarkening;
+                // console.log(`blackFactor: ${blackFactor}`);
+
                 r = Math.round(baseRgb[0] * (1 - blackFactor));
                 g = Math.round(baseRgb[1] * (1 - blackFactor));
                 b = Math.round(baseRgb[2] * (1 - blackFactor));
