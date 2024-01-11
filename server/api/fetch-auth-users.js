ed1d24d6f8b2500796173cd1caed71c1169f3aa8
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-11 21:43:21 +0100

    update: fetch-auth-users api route

---
+++ server/api/fetch-auth-users.js
@@ -0,0 +1,16 @@
+import { serverSupabaseServiceRole } from "#supabase/server";
+import { serverSupabaseUser } from "#supabase/server";
+
+export default eventHandler(async (event) => {
+    const client = serverSupabaseServiceRole(event);
+    const user = serverSupabaseUser(event);
+
+    await checkUserRole(event, client, "admin");
+
+    let { data: authData, error: authError } = await client.auth.admin.listUsers();
+    if (authError) {
+        throw authError;
+    }
+
+    return authData;
+});
