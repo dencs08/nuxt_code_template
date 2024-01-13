ebb2abae8c5ca3195f47e00865d9bbb5002fe06a
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-13 12:45:38 +0100

    add: role-dropdown component

---
+++ components/custom-usabililty-components/dropdowns/role-dropdown.vue
@@ -0,0 +1,25 @@
+<template>
+    <Dropdown :modelValue="selectedRole" @update:modelValue="updateRole" :options="roles" optionLabel="label"
+        optionValue="value">
+        <template #option="slotProps">
+            <Tag :value="slotProps.option.label" :severity="getRoleSeverity(slotProps.option.value)" />
+        </template>
+    </Dropdown>
+</template>
+  
+<script setup>
+const props = defineProps({
+    modelValue: String
+});
+
+
+const { roles, getRoleSeverity } = useRoles();
+const emit = defineEmits(['update:modelValue']);
+
+const selectedRole = ref(props.modelValue);
+
+const updateRole = (newRole) => {
+    selectedRole.value = newRole;
+    emit('update:modelValue', newRole);
+};
+</script>
