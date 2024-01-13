4f0c7d4e88ee64e4ec291bf48cb1ff16edc77150
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-13 12:52:19 +0100

    update: dashboard layout HTML tags + i18n + seo + global error handler + auto fetch userSession

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,18 +1,50 @@
 <template>
-    <div>
-        <DashboardSidebar />
-        <div class="lg:pl-56">
-            <main class="pt-20 min-h-screen">
-                <div class="px-4 sm:px-6 lg:px-8 pb-8">
-                    <Breadcrumbs class="mb-2" />
-                    <slot></slot>
-                </div>
-            </main>
+    <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+
+    <Head>
+        <Title>{{ title }}</Title>
+        <template v-for="link in head.link" :key="link.id">
+            <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+        </template>
+        <template v-for="meta in head.meta" :key="meta.id">
+            <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
+        </template>
+    </Head>
+
+    <Body class="bg-gray-50 text-surface-900 dark:bg-surface-900 dark:text-surface-50">
+        <div>
+            <DashboardSidebar />
+            <div class="lg:pl-56">
+                <main class="pt-20 min-h-screen">
+                    <div class="px-4 sm:px-6 lg:px-8 pb-8">
+                        <Breadcrumbs class="mb-2" />
+                        <slot></slot>
+                    </div>
+                </main>
+            </div>
         </div>
-    </div>
+    </Body>
+
+    </Html>
 </template>
 
 <script setup>
+const route = useRoute()
+const { t } = useI18n()
+const head = useLocaleHead({
+    addDirAttribute: true,
+    identifierAttribute: 'id',
+    addSeoAttributes: true
+})
+const title = computed(() => t('layouts.default.title', { title: t(route.meta.title ?? 'TBD') }))
+
+const userStore = useUsersStore();
+if (!userStore.userSession) {
+    await userStore.fetchUserSession();
+}
+
+const { errorHandler } = useErrorHandler();
+errorHandler();
 </script>
 
 <style lang="scss" scoped></style>
