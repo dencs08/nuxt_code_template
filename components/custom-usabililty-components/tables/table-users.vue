c866dffc909e0471aab4ffb94a2a0b180703c08f
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-14 14:16:28 +0100

    fix: table users no longer block renderning

--- components/custom-usabililty-components/tables/table-users.vue
+++ components/custom-usabililty-components/tables/table-users.vue
@@ -66,7 +66,7 @@ const { handleSubmit } = useSubmit();
 const { getRoleSeverity } = useRoles();
 const { hasAccess } = useRoleCheck('admin');
 const userStore = useUsersStore();
-await userStore.fetchUsers();
+userStore.fetchUsers();
 
 const editingRows = ref([]);
 const selected = ref();
