94f39492bc3d45e54a412f87ef548186f7292d99
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-16 01:14:05 +0100

    fix: useErrorHandler no longer gets all of the thrown errors

--- composables/useErrorHandler.js
+++ composables/useErrorHandler.js
@@ -1,14 +1,19 @@
 export function useErrorHandler() {
     const { addToast } = useToastService();
+    const { CustomError } = useCustomError();
 
     const errorHandler = () => {
         onMounted(() => {
             window.addEventListener('unhandledrejection', event => {
                 // Prevent default handling (so it doesn't appear in console)
                 event.preventDefault();
-
-                const errorMessage = event.reason.message;
-                addToast('error', 'Error', errorMessage, 30000);
+                const error = event.reason;
+                if (error instanceof CustomError) {
+                    const errorMessage = error.message;
+                    addToast('error', 'Error', errorMessage, 30000);
+                } else {
+                    // Do something else
+                }
             });
         });
     };
