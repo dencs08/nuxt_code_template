0304782f807f77fc280f746055e6aefb4d7aca45
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-16 21:14:34 +0100

    Add user session fetching in Nuxt hook plugin

--- plugins/hooks.ts
+++ plugins/hooks.ts
@@ -1,8 +1,15 @@
-let i = 0;
+import type { Pinia } from "pinia";
+import { useUsersStore } from "~/stores/UsersStore";
+
+//TODO get the all of the users settings + darkmode
+
+/**
+ * Nuxt plugin which gets all the necessary data before the application is created.
+ * @param {object} nuxtApp - The Nuxt application instance.
+ */
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.hook("page:start", () => {
-        console.log("Nuxt App is created!");
-        console.log(i);
-        i++;
+    nuxtApp.hook("app:beforeMount", async () => {
+        const usersStore = useUsersStore(nuxtApp.$pinia as Pinia);
+        await usersStore.fetchUserSession();
     });
 });
