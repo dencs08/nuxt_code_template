71f57eff75cfd6843c2d2463c8ae93ceeb356e95
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-16 01:18:00 +0100

    Refactor useSubmit and add CustomError to UsersStore

--- composables/useSubmit.ts
+++ composables/useSubmit.ts
@@ -2,16 +2,17 @@ export function useSubmit() {
     const { addToast } = useToastService();
     const localePath = useLocalePath();
 
-    const defaultSuccessHandler = (message: string, redirectPath?: string, external?: boolean) => (response: any) => {
-        addToast('success', 'Success', message);
-        if (redirectPath) {
-            navigateTo(localePath(redirectPath), { external: external ?? false });
-        }
-    };
+    const defaultSuccessHandler =
+        (message: string, redirectPath?: string, external?: boolean) => (response: any) => {
+            addToast("success", "Success", message);
+            if (redirectPath) {
+                navigateTo(localePath(redirectPath), { external: external ?? false });
+            }
+        };
 
     const defaultErrorHandler = (error: any) => {
-        const errorMessage = error.message || 'An unknown error occurred.';
-        addToast('error', 'Error', errorMessage, 30000);
+        const errorMessage = error.message || "An unknown error occurred.";
+        addToast("error", "Error", errorMessage, 30000);
     };
 
     async function handleSubmit(
@@ -21,7 +22,11 @@ export function useSubmit() {
         redirectPath?: string,
         external?: boolean,
         errorMessage?: string,
-        onSuccess: (response: any) => void = defaultSuccessHandler(successMessage, redirectPath, external),
+        onSuccess: (response: any) => void = defaultSuccessHandler(
+            successMessage,
+            redirectPath,
+            external
+        ),
         onError: (error: any) => void = defaultErrorHandler
     ) {
         try {
