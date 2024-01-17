34da8386a086bf117a071bf8262366c71213b558
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-17 03:16:40 +0100

    Refactor color mode selector component to support switch type

--- components/custom-usabililty-components/color-mode-selector.vue
+++ components/custom-usabililty-components/color-mode-selector.vue
@@ -1,10 +1,20 @@
 <template>
-    <button @click="toggle()">
-        <Icon v-auto-animate :name="isDark ? 'ic:outline-dark-mode' : 'ic:outline-light-mode'" class="text-lg" />
-    </button>
+    <div>
+        <button v-if="type === 'button'" @click="toggle()" class="grid place-content-center">
+            <Icon v-auto-animate :name="isDark ? 'ic:outline-dark-mode' : 'ic:outline-light-mode'" class="text-lg" />
+        </button>
+        <InputSwitch v-else-if="type === 'switch'" v-model="isDark" />
+    </div>
 </template>
-
+  
 <script setup>
-const isDark = useDark()
-const toggle = useToggle(isDark)
+const isDark = useDark();
+const toggle = useToggle(isDark);
+
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'button'
+    }
+})
 </script>
