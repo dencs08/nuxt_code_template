21fce4e3baf0642f3c3b21c393c1bcadd5d5286b
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-19 13:03:31 +0100

    Add dashboard index pagewith reroute to /home

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template></template>
+<script setup lang=ts>
+definePageMeta({
+    layout: "dashboard",
+});
+
+const localePath = useLocalePath();
+onMounted(() => {
+    navigateTo(localePath("/dashboard/home'"));
+});
+</script>
+<style lang="">
+    
+</style>
