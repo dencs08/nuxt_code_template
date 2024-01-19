264cecd8b3f36a5a1ba3228e665cf812694c7e54
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-19 13:00:48 +0100

    Commented out unused code in middleware.global.js

--- middleware/middleware.global.js
+++ middleware/middleware.global.js
@@ -1,23 +1,24 @@
+//TODO think about if i need this middleware if index.vue in dashboard folder is now used for redirecting to dashboard/home
 export default defineNuxtRouteMiddleware((to, from) => {
-    const localePath = useLocalePath()
-    const segments = to.path.split('/')
+    // const localePath = useLocalePath()
+    // const segments = to.path.split('/')
 
-    // Get the language segment and the rest of the path
-    let pathAfterLanguagePrefix
+    // // Get the language segment and the rest of the path
+    // let pathAfterLanguagePrefix
 
-    // Check if the path contains language prefix
-    if (segments[1].length === 2 && segments.length > 2) {
-        pathAfterLanguagePrefix = '/' + segments.slice(2).join('/')
-    } else {
-        pathAfterLanguagePrefix = to.path
-    }
+    // // Check if the path contains language prefix
+    // if (segments[1].length === 2 && segments.length > 2) {
+    //     pathAfterLanguagePrefix = '/' + segments.slice(2).join('/')
+    // } else {
+    //     pathAfterLanguagePrefix = to.path
+    // }
 
-    // If the path is dashboard or dashboard/ after the language prefix
-    if (
-        pathAfterLanguagePrefix === '/dashboard' ||
-        pathAfterLanguagePrefix === '/dashboard/'
-    ) {
-        // Navigate to the dashboard/home in the same language
-        return navigateTo(localePath('/dashboard/home'))
-    }
+    // // If the path is dashboard or dashboard/ after the language prefix
+    // if (
+    //     pathAfterLanguagePrefix === '/dashboard' ||
+    //     pathAfterLanguagePrefix === '/dashboard/'
+    // ) {
+    //     // Navigate to the dashboard/home in the same language
+    //     return navigateTo(localePath('/dashboard/home'))
+    // }
 })
