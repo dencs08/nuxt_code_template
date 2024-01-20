2a4d2024af853868dd1994de1652d6cdb238a15e
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-20 12:44:37 +0100

    Refactor breadcrumbs component to use primevue Breadcrumb component

--- components/custom-usabililty-components/breadcrumbs.vue
+++ components/custom-usabililty-components/breadcrumbs.vue
@@ -1,12 +1,20 @@
 <template>
-  <nav aria-label="breadcrumb">
-    <ol class="flex flex-row items-center gap-1 text-gray-400 text-xs">
-      <li class="capitalize" v-for="(crumb, index) in crumbs" :key="index">
-        <nuxt-link :to="crumb.path" class="hover:text-primary-700 transition-color duration-75" :class="{ 'text-primary-500': index === crumbs.length - 1 }">{{ crumb.label }}</nuxt-link>
-        <span v-if="index !== crumbs.length - 1" class="ml-1">/</span>
-      </li>
-    </ol>
-  </nav>
+  <Breadcrumb :home="home" :model="items">
+    <template #item="{ item, props }">
+      <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+        <a :href="href" v-bind="props.action" @click="navigate">
+          <span :class="[item.icon, 'text-color']" />
+          <span
+            class="text-surface-700 dark:text-surface-100/70 font-light text-xs hover:text-primary-500 dark:hover:text-primary-500 transition">{{
+              item.label
+            }}</span>
+        </a>
+      </router-link>
+      <a v-else :href="item.url" :target="item.target" v-bind="props.action">
+        <span class="text-surface-700 dark:text-surface-0/80">{{ item.label }}</span>
+      </a>
+    </template>
+  </Breadcrumb>
 </template>
 
 <script setup lang="ts">
@@ -14,32 +22,28 @@ const route = useRoute()
 const localePath = useLocalePath()
 const { locale, getBrowserLocale } = useI18n()
 
-const breadcrumbs = ref([])
+const home = ref({
+  icon: 'pi pi-home',
+  route: localePath('/dashboard/home')
+});
 
-const crumbs = computed(() => {
-  // Fetch supported locales from i18n module
-  const languageCodes = getBrowserLocale()
+const items = computed(() => {
+  // Split the path into segments
+  const segments = route.path.split('/').filter(segment => segment);
 
-  const pathArray = route.path.split('/').filter(n => n)
-
-  // If the first element of pathArray is a language code, remove it.
-  if (languageCodes.includes(pathArray[0])) {
-    pathArray.shift()
+  // Remove the locale from the segments
+  const localeIndex = segments.indexOf(locale.value as string);
+  if (localeIndex !== -1) {
+    segments.splice(localeIndex, 1);
   }
 
-  let breadcrumbs: any[] = []
-  pathArray.forEach((path, index) => {
-    let crumbPath = `/${pathArray.slice(0, index + 1).join('/')}`
-    // if current locale is not default locale, prefix crumbPath with locale
-    if (locale.value !== 'en') {
-      crumbPath = `/${locale.value}${crumbPath}`
-    }
-    breadcrumbs.push({
-      label: path,
-      path: crumbPath
-    })
-  })
-  return breadcrumbs
-})
+  // Build the items array
+  return segments.map((segment, index) => {
+    return {
+      label: segment,
+      route: localePath('/' + segments.slice(0, index + 1).join('/'))
+    };
+  });
+});
 </script>
 
