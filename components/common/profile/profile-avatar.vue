aa0ba2a21a61810bd6bc428fe870173b9815ca5b
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-24 15:22:47 +0100

    Add profile-avatar component

---
+++ components/common/profile/profile-avatar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div
+        class="border border-surface-500 dark:border-surface-600 rounded overflow-hidden shadow-[0_1px_10px_-1px_rgba(0,0,0,0.5)]">
+        <Skeleton v-if="!userSession && !photoSrc || userStore.loading" height="100%" width="100%" class="object-cover">
+        </Skeleton>
+        <nuxt-img v-else-if="!photoSrc" :src="'img/avatar.svg'" class="h-full w-full object-cover"></nuxt-img>
+        <nuxt-img v-else :src="photoSrc" class="h-full w-full object-cover"></nuxt-img>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    photo: String,
+})
+
+const userStore = useUsersStore();
+const userSession = await userStore.getUserSession;
+const photoSrc = ref(props.photo || userSession.photo);
+
+watch(() => userStore.userSession.photo, (newPhoto) => {
+    photoSrc.value = newPhoto;
+});
+</script>
+
+<style></style>
