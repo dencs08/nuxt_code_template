e0efb9dd59d5f1916dbbc7f00ef75e2846e5b6f1
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-24 15:26:27 +0100

    Add delete account API endpoint

---
+++ server/api/delete-account.js
@@ -0,0 +1,24 @@
+import { serverSupabaseServiceRole, serverSupabaseUser } from "#supabase/server";
+
+export default eventHandler(async (event) => {
+    const client = serverSupabaseServiceRole(event);
+    const userSession = await serverSupabaseUser(event)
+
+    await checkUserRole(event, client, 'user');
+
+    try {
+        const { data, error } = await client.auth.admin.deleteUser(userSession.id)
+
+        const { data: deletedUser, error: deleteError } = await client
+            .from('users')
+            .delete()
+            .eq('id', userSession.id);
+        if (deleteError) {
+            throw new Error('Error deleting user from public.users: ' + deleteError.message);
+        }
+
+        return { response: 'User deleted', deletedUser: deletedUser, deletedAuth: data };
+    } catch (err) {
+        return { error: 'An error occurred during the deletion process', response: err.message };
+    }
+});
