852948ada477e2ef43bcd6390479b36f80ea83ad
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-25 02:35:40 +0100

    Add useAuthEvent function to handle specific auth events

--- composables/useAuthListeners.ts
+++ composables/useAuthListeners.ts
@@ -1,23 +1,58 @@
+import { type Session } from "@supabase/supabase-js";
+type AuthEvent =
+    | "INITIAL_SESSION"
+    | "SIGNED_IN"
+    | "SIGNED_OUT"
+    | "PASSWORD_RECOVERY"
+    | "TOKEN_REFRESHED"
+    | "USER_UPDATED";
+
+type AuthSubscription = {
+    unsubscribe: () => void;
+};
+
 export function useAuthListeners() {
     const client = useSupabaseClient();
 
     const handleAuthStateChange = (
-        authEvent: string,
-        callback: (event: string, session: any) => void
+        authEvent: AuthEvent,
+        callback: (event: AuthEvent, session: any) => void
     ) => {
-        const authSubscription = client.auth.onAuthStateChange((event, session) => {
+        const {
+            data: { subscription },
+        } = client.auth.onAuthStateChange((event, session) => {
             if (event === authEvent) callback(event, session);
-        }) as unknown as { cancel: () => void };
+        });
+
+        const authSubscription = subscription as AuthSubscription;
 
         // Return a function to cancel the subscription
         return () => {
             if (authSubscription) {
-                // authSubscription.cancel(); // no way ATM to cancel in JS
+                authSubscription.unsubscribe();
             }
         };
     };
 
+    const useAuthEvent = (
+        authEvent: AuthEvent,
+        callback: (event: AuthEvent, session: Session | null) => void
+    ) => {
+        let unsubscribe: (() => void) | undefined;
+
+        onMounted(() => {
+            unsubscribe = handleAuthStateChange(authEvent, callback);
+        });
+
+        onUnmounted(() => {
+            if (unsubscribe) {
+                unsubscribe();
+            }
+        });
+    };
+
     return {
         handleAuthListener: handleAuthStateChange,
+        useAuthEvent,
     };
 }
