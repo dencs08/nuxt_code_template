62633c51e832934ef1b590914d803adabf7091ad
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-25 02:34:44 +0100

    Update module dependencies in config/modules.ts

--- config/modules.ts
+++ config/modules.ts
@@ -1,21 +1,21 @@
 // config/modules.ts
 
 export default [
-    "@nuxtjs/supabase",
     "@nuxt/devtools",
-    "@formkit/nuxt",
-    // '@sfxcode/nuxt-primevue', //deprecated in favor of nuxt-primevue (no longer maintained npm package)
-    "nuxt-primevue",
-    "@nuxtjs/robots",
+    "@nuxt/content",
     "@nuxt/image",
+    "@nuxtjs/supabase",
     "@nuxtjs/i18n",
     "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/robots",
+    "nuxt-icon",
+    "nuxt-primevue",
+    "nuxt-simple-sitemap",
+    "@formkit/nuxt",
     "@pinia/nuxt",
     "@vueuse/nuxt",
-    "nuxt-simple-sitemap",
-    "nuxt-icon",
 
     // "@vite-pwa/nuxt", //uncomment for generating a PWA for mobiles.
     // "nuxt-gtag", //uncomment for adding gtag integration
-    // "@sidebase/nuxt-auth", //uncomment for when not using supabase or other authentication.
 ];
