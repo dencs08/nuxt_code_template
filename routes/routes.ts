ab338f0857e3e0f2c70d862ed2bb87f151118f3f
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-25 02:32:23 +0100

    Update routes.ts file with new comments

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,17 +1,19 @@
-//when changing this file you need to restart the server it doesnt support hot reload yet.
+//! note: when changing this file you need to restart the server it doesnt support hot reload yet.
 
 //extending page hook of nuxt
+//useful if want to assign new name to the page, redirect to another page, or change the path.
+//! note: i18n is buggy when using this
 export const routes = [
     {
-        name: 'login', //actual page name
-        newName: 'login', //assign new name
-        path: '/login', //change the path
-        file: '@/pages/login.vue', //specify the file
+        name: "login", //actual page name
+        newName: "login", //assign new name
+        path: "/login", //change the path
+        file: "@/pages/login.vue", //specify the file
     },
     {
-        name: 'contact', //actual page name
-        newName: 'contact', //assign new name
-        path: '/contact', //change the path
-        file: '@/pages/contact.vue', //specify the file
+        name: "contact", //actual page name
+        newName: "contact", //assign new name
+        path: "/contact", //change the path
+        file: "@/pages/contact.vue", //specify the file
     },
-]
+];
