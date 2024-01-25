8e5cd98609985e2129999f5d00c860f310bc09bb
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-25 02:37:04 +0100

    Update button padding and disabled state styling

--- components/design/my_button.vue
+++ components/design/my_button.vue
@@ -35,24 +35,23 @@ const computedSize = computed(() => {
     return allowedSizes.includes(props.size) ? props.size : 'md';
 });
 </script>
-  
 
 <style lang="scss">
 .btn {
-    padding-top: clamp(0.5em, 0.6vw, 1em);
-    padding-right: clamp(1.25em, 1.75vw, 2em);
-    padding-bottom: clamp(0.5em, 0.6vw, 1em);
-    padding-left: clamp(1.25em, 1.75vw, 2em);
+    padding-top: clamp(0.5em, 0.5vw, 0.8em);
+    padding-right: clamp(1.25em, 1.3vw, 2em);
+    padding-bottom: clamp(0.5em, 0.5vw, 0.8em);
+    padding-left: clamp(1.25em, 1.3vw, 2em);
     @apply transition-all duration-75 font-medium;
 }
 
 .btn-xs {
     font-size: clamp(0.75rem, 0.7vw, 1.5rem);
 
-    padding-top: clamp(0.5em, 0.6vw, 1.5em);
-    padding-right: clamp(1.75em, 1.25vw, 2.5em);
-    padding-bottom: clamp(0.5em, 0.6vw, 1.5em);
-    padding-left: clamp(1.75em, 1.25vw, 2.5em);
+    padding-top: clamp(0.5em, 0.4vw, 0.7em);
+    padding-right: clamp(1.75em, 1.1vw, 2em);
+    padding-bottom: clamp(0.5em, 0.4vw, 0.7em);
+    padding-left: clamp(1.75em, 1.1vw, 2em);
 }
 
 .btn-sm {
@@ -70,6 +69,14 @@ const computedSize = computed(() => {
 .btn-primary {
     @apply bg-gradient-to-r from-primary-500 via-primary-500 to-85% to-secondary-300;
     @apply bg-[length:150%_150%] bg-[0px_0px];
+
+    &:disabled {
+        @apply from-surface-200/50 to-surface-200/50 bg-surface-200/50 text-surface-50;
+
+        &:hover {
+            @apply bg-surface-500/50 saturate-100 shadow-none;
+        }
+    }
 }
 
 .btn-primary:hover {
