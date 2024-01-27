1ef2e7df96637494296eb38c34453d03c1b4b7e9
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-28 00:00:09 +0100

    Add Remember Me checkbox and radio button to user dashboard for debugging

--- pages/dashboard/users.vue
+++ pages/dashboard/users.vue
@@ -8,6 +8,15 @@ definePageMeta({
   <div>
     <div class="rounded-lg overflow-hidden">
       <TableUsers />
+
+      <label for="chkbox1">Remember Me</label>
+      <Checkbox inputId="chkbox1" />
+
+      <span id="chkbox2">Remember Me</span>
+      <Checkbox aria-labelledby="chkbox2" />
+
+      <Checkbox aria-label="Remember Me" />
+      <RadioButton />
     </div>
   </div>
 </template>
