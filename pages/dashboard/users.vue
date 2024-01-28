f15fe2f1bc05c56671785ef2628a4dfe4c94bf21
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-28 16:47:52 +0100

    Remove unused code related to checkbox testing

--- pages/dashboard/users.vue
+++ pages/dashboard/users.vue
@@ -8,15 +8,6 @@ definePageMeta({
   <div>
     <div class="rounded-lg overflow-hidden">
       <TableUsers />
-
-      <label for="chkbox1">Remember Me</label>
-      <Checkbox inputId="chkbox1" />
-
-      <span id="chkbox2">Remember Me</span>
-      <Checkbox aria-labelledby="chkbox2" />
-
-      <Checkbox aria-label="Remember Me" />
-      <RadioButton />
     </div>
   </div>
 </template>
