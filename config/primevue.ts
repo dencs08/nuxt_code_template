83738ef9108e863ba74febd1cac9e5f413e5c3d0
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-28 16:48:05 +0100

    Update PrimeVue configuration

--- config/primevue.ts
+++ config/primevue.ts
@@ -1,12 +1,12 @@
 // config/primevue.ts
 export default {
+    importPT: { from: "@/assets/primevue/lara", as: "Lara" }, //import and apply preset
     options: {
-        ripple: true,
+        ripple: false,
         // inputStyle: "outlined",
         // cssLayerOrder: "reset,primevue",
         unstyled: true,
     },
-    importPT: { from: "@/assets/primevue/lara" }, //import and apply preset
     components: {
         // prefix: "prime", //breaks sfxformkit componenets...
         include: "*",
