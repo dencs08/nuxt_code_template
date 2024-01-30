de2a1ac1a6f42e897debaf684c4def9bace0302e
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-30 02:57:40 +0100

    Remove unused composable functions

--- composables/useRequests.ts
+++
@@ -1,8 +0,0 @@
-
-export function useRequests() {
-    const handleRequestError = (response: any) => {
-        if (response.error) throw new Error(response.error.message)
-    };
-
-    return { handleRequestError };
-}
