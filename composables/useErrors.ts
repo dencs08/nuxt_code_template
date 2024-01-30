84df44e06577095337e2b9e1513185db93d880c8
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-30 02:57:45 +0100

    Add useErrors.ts with custom error handling

---
+++ composables/useErrors.ts
@@ -0,0 +1,34 @@
+// useError.ts
+export class CustomError extends Error {
+    value: any;
+
+    constructor(message: string, value: any) {
+        super(message);
+        this.value = value;
+    }
+}
+
+export function useCustomError() {
+    return { CustomError };
+}
+
+export function useErrorHandler() {
+    const { addToast } = useToastService();
+    const { CustomError } = useCustomError();
+
+    const errorHandler = () => {
+        onMounted(() => {
+            window.addEventListener("unhandledrejection", (event) => {
+                event.preventDefault();
+                const error = event.reason;
+                if (error instanceof CustomError) {
+                    const errorMessage = error.message;
+                    addToast("error", "Error", errorMessage, 30000);
+                } else {
+                    // Do something else
+                }
+            });
+        });
+    };
+    return { errorHandler };
+}
