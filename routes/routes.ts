cc6e619fe4526a2e98c3862fbc04199ba08ae911
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-30 12:22:04 +0100

    Update page routes with new routes

--- routes/routes.ts
+++ routes/routes.ts
@@ -2,18 +2,78 @@
 
 //extending page hook of nuxt
 //useful if want to assign new name to the page, redirect to another page, or change the path.
-//! note: i18n is buggy when using this
 export const routes = [
+    //design pages
     {
-        name: "login", //actual page name
-        newName: "login", //assign new name
-        path: "/login", //change the path
-        file: "@/pages/login.vue", //specify the file
+        name: "index", //actual page name
+        newName: "index", //assign new name
+        path: "/", //change the path //TODO check how this works with new i18n path defining per language
+        file: "@/pages/index.vue", //specify the file
     },
     {
-        name: "contact", //actual page name
-        newName: "contact", //assign new name
-        path: "/contact", //change the path
-        file: "@/pages/contact.vue", //specify the file
+        name: "contact",
+        newName: "contact",
+        path: "/contact",
+        file: "@/pages/contact.vue",
+    },
+
+    //legal pages
+    {
+        name: "terms-and-conditions",
+        newName: "terms",
+        path: "/terms",
+        file: "@/pages/terms-and-conditions.vue",
+    },
+    {
+        name: "privacy-policy",
+        newName: "privacy",
+        path: "/privacy",
+        file: "@/pages/privacy-policy.vue",
+    },
+    {
+        name: "cookies-policy",
+        newName: "cookies",
+        path: "/cookies",
+        file: "@/pages/cookies-policy.vue",
+    },
+
+    //auth pages
+    {
+        name: "login",
+        newName: "login",
+        path: "/login",
+        file: "@/pages/login.vue",
+    },
+    {
+        name: "register",
+        newName: "register",
+        path: "/register",
+        file: "@/pages/register.vue",
+    },
+    {
+        name: "lost-password",
+        newName: "lost-password",
+        path: "/lost-password",
+        file: "@/pages/lost-password.vue",
+    },
+
+    //dashboard pages
+    {
+        name: "dashboard",
+        newName: "dash",
+        path: "/dashboard",
+        file: "@/pages/dashboard/index.vue",
+    },
+    {
+        name: "dashboard-home",
+        newName: "dash-home",
+        path: "/dashboard/home",
+        file: "@/pages/dashboard/home/index.vue",
+    },
+    {
+        name: "dashboard-user-profile",
+        newName: "dash-user-profile",
+        path: "/dashboard/user/profile",
+        file: "@/pages/dashboard/user/profile/index.vue",
     },
 ];
