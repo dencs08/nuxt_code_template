49dbd99b5a6ad7cf8b308e04c66a838bb9738e5f
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-01-31 22:10:11 +0100

    Add FAQ page with metadata

---
+++ pages/faq.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+definePageMeta({
+    auth: false,
+})
+
+const { t, locale } = useI18n()
+const title = computed(() => t('pages.index.title'))
+const description = computed(() => t('pages.index.description'))
+
+useHead({
+    title: title,
+    meta: [
+        { property: 'description', content: description },
+        { property: 'og:description', content: description },
+        { property: 'og:title', content: title },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:locale', content: locale },
+        // { property: 'og:url', content: 'https://nuxt.com' },
+        // { property: 'og:image', content: 'https://nuxt.com/social.jpg' },
+    ]
+})
+</script>
+
+<template>
+    <div class="grid place-content-center h-screen">
+        <h1>FAQ</h1>
+    </div>
+</template>
