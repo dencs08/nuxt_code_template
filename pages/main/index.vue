6bcae4776910918562d8d5aada98e27e8f6609c3
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-02-01 23:25:20 +0100

    Add main page with meta tags

---
+++ pages/main/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'main',
+    auth: false,
+})
+
+const { t, locale } = useI18n()
+const title = computed(() => t('pages.index.title'))
+const description = computed(() => t('pages.index.description'))
+
+useHead({
+    title: title,
+    meta: [
+        { property: 'description', content: description },
+        { property: 'og:description', content: description },
+        { property: 'og:title', content: title },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:locale', content: locale },
+        // { property: 'og:url', content: 'https://nuxt.com' },
+        // { property: 'og:image', content: 'https://nuxt.com/social.jpg' },
+    ]
+})
+</script>
+
+<template>
+    <div class="grid place-content-center h-screen">
+        <h1>Home!</h1>
+    </div>
+</template>
