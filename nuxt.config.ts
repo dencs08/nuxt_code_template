9e44d13c3f1426a33050322c9bcde373e267a0ec
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-02-01 23:24:31 +0100

    Update routeRules in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,14 +48,14 @@ export default defineNuxtConfig({
 
     routeRules: {
         // Homepage pre-rendered at build time
-        "/": { prerender: true },
+        "/": { prerender: true }, //propably need to make SSR instead of prerender
         "/contact": { prerender: true },
         // Product page generated on-demand, revalidates in background
         // '/products/**': { swr: 3600 },
         // // Blog post generated on-demand once until next deploy
         // '/blog/**': { isr: true },
         // // Admin dashboard renders only on client-side
-        // '/admin/**': { ssr: false },
+        // '/dashboard/**': { ssr: false },
         // // Add cors headers on API routes
         // '/api/**': { cors: true },
         // // Redirects legacy urls
