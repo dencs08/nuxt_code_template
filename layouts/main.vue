f9672b64bf97e4a3554130e0c60e093eeaf7f8a5
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-02-01 23:25:15 +0100

    Add main layout component

---
+++ layouts/main.vue
@@ -0,0 +1,39 @@
+<script setup>
+const route = useRoute()
+const { t } = useI18n()
+const head = useLocaleHead({
+    addDirAttribute: true,
+    identifierAttribute: 'id',
+    addSeoAttributes: true
+})
+const title = computed(() => t('layouts.default.title', { title: t(route.meta.title ?? 'TBD') }))
+const isDark = useDark()
+const html = ref();
+
+onMounted(() => {
+    isDark.value = false;
+});
+
+</script>
+
+<template>
+    <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir" ref="html">
+
+    <Head>
+        <Title>{{ title }}</Title>
+        <template v-for="link in head.link" :key="link.id">
+            <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+        </template>
+        <template v-for="meta in head.meta" :key="meta.id">
+            <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
+        </template>
+    </Head>
+
+    <Body class="bg-gray-50 text-gray-900">
+        <main>
+            <slot />
+        </main>
+    </Body>
+
+    </Html>
+</template>
