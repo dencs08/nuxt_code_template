4f6ef61a0f97533638246487f063b8027fb2fe1c
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-02-05 17:13:19 +0100

    Add role.global.js middleware for user access control

---
+++ middleware/role.global.js
@@ -0,0 +1,35 @@
+import { fetchPublicUserSession } from '../services/userSession';
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const auth = to.meta.auth;
+    const access = to.meta.access;
+
+    if (auth === false) {
+        return; // Allow all users regardless of authentication state.
+    } else {
+        if (access === undefined) {
+            return; // Allow all users regardless of role.
+        }
+
+        const userSession = await fetchPublicUserSession();
+        const userRole = userSession?.role;
+
+        // Find the user's role and the minimum access role in the validRoles array
+        const userRoleObj = validRoles.find(role => role.value === userRole);
+        const accessRoleObj = validRoles.find(role => role.value === access);
+
+        // If the user's role or the minimum access role is not found, deny access
+        if (!userRoleObj || !accessRoleObj) {
+            return false;
+        }
+
+        // If the user's role level is less than the minimum access role level, deny access
+        if (userRoleObj.level < accessRoleObj.level) {
+            return false;
+        }
+
+        // If none of the above conditions are met, allow access
+        return true;
+
+    }
+});
