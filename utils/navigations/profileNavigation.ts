4a016984a8286c2313547a83a5f4d829c7c5dda0
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-02-06 17:00:27 +0100

    Update profileNavigation route and comment out unused items

--- utils/navigations/profileNavigation.ts
+++ utils/navigations/profileNavigation.ts
@@ -3,22 +3,16 @@ export function profileNavigation() {
     const localePath = useLocalePath();
 
     return {
-        route: localePath({ name: "dash-user-profile" }),
-        items: [
-            {
-                label: "Account",
-                icon: "pi pi-user",
-                command: () => {
-                    navigateTo(localePath({ name: "dash-user-profile" }));
-                },
-            },
-            {
-                label: "Actions",
-                icon: "pi pi-chart-line",
-                command: () => {
-                    navigateTo(localePath("/dashboard/user/profile/actions"));
-                },
-            },
-        ],
+        route: localePath({ name: "dash-user-account" }),
+        // items: [
+        //     {
+        //         label: "Account",
+        //         icon: "pi pi-user",
+        //         checkRoute: "dash-user-account",
+        //         command: () => {
+        //             navigateTo(localePath({ name: "dash-user-account" }));
+        //         },
+        //     },
+        // ],
     };
 }
