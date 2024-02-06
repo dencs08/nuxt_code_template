74d87bf191f98e949ce48bfe0fceb1ba8dd3b45f
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-02-06 16:58:34 +0100

    Refactor useRoleCheck function to improve role checking logic

--- composables/useRoleCheck.ts
+++ composables/useRoleCheck.ts
@@ -1,19 +1,34 @@
-export function useRoleCheck(requiredRoleName: string) {
+import { type Role, validRoles } from "@/utils/roles";
+export function useRoleCheck(defaultRoleCheck = "user" as string) {
     const { roles } = useRoles();
     const userStore = useUsersStore();
     const user = userStore.userRole;
+    const userRole = computed(() => roles.value.find((role) => role.value === user));
 
-    const requiredRole = roles.value.find((role) => role.value === requiredRoleName);
+    const checkAccess = (requiredRoleValue: Role["value"]): boolean => {
+        const userRoleLevel = userRole.value.level;
+        const requiredRole = validRoles.find((role) => role.value === requiredRoleValue);
+        return requiredRole && userRoleLevel >= requiredRole.level;
+    };
 
-    const userRole = computed(() => {
-        return roles.value.find((role) => role.value === user);
-    });
+    const hasAccess = (roleValue: Role["value"] = "user"): boolean => {
+        return checkAccess(roleValue);
+    };
 
-    const hasAccess = computed(() => {
-        return userRole.value && requiredRole && userRole.value.level >= requiredRole.level;
-    });
+    const accessByRole = (
+        minimalAccessRoleValue: Role["value"]
+    ): Record<Role["value"], boolean> => {
+        const accessMap: Record<Role["value"], boolean> = {} as Record<Role["value"], boolean>;
+        validRoles.forEach((role) => {
+            accessMap[role.value] =
+                role.level >= validRoles.find((r) => r.value === minimalAccessRoleValue).level;
+        });
+        return accessMap;
+    };
 
     return {
         hasAccess,
+        checkAccess,
+        accessByRole,
     };
 }
