3f1b46aa4ebe18199b503fda7af62dba43943420
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-02-07 21:46:30 +0100

    Refactor role.global.js middleware to use useRoleCheck() for access check

--- middleware/role.global.js
+++ middleware/role.global.js
@@ -1,35 +1,21 @@
-import { fetchPublicUserSession } from '../services/userSession';
-
 export default defineNuxtRouteMiddleware(async (to, from) => {
+    const { hasAccess } = useRoleCheck();
     const auth = to.meta.auth;
     const access = to.meta.access;
 
     if (auth === false) {
         return; // Allow all users regardless of authentication state.
     } else {
-        const usersStore = useUsersStore();
         if (access === undefined) {
             return; // Allow all users regardless of role.
         }
-        const userSession = await usersStore.fetchUserSession();
-        const userRole = usersStore.userRole;
 
-        // Find the user's role and the minimum access role in the validRoles array
-        const userRoleObj = validRoles.find(role => role.value === userRole);
-        const accessRoleObj = validRoles.find(role => role.value === access);
-
-        // If the user's role or the minimum access role is not found, deny access
-        if (!userRoleObj || !accessRoleObj) {
-            return false;
-        }
-
-        // If the user's role level is less than the minimum access role level, deny access
-        if (userRoleObj.level < accessRoleObj.level) {
+        // If the user doesn't have access, deny access
+        if (!hasAccess(access)) {
             return false;
         }
 
         // If none of the above conditions are met, allow access
         return true;
-
     }
 });
