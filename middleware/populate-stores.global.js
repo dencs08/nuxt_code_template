12d7bd7cfd3ef88dc1241608fd2dc1a13cec3ba2
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-02-07 21:46:23 +0100

    Refactor populate-stores.global.js middleware

--- middleware/populate-stores.global.js
+++ middleware/populate-stores.global.js
@@ -1,10 +1,4 @@
-//TODO get the all of the users settings + darkmode
-
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    if (to.meta.auth != false) {
-        const usersStore = useUsersStore();
-        if (!usersStore.userSession) {
-            await usersStore.fetchUserSession();
-        }
-    }
+    const usersStore = useUsersStore();
+    await usersStore.fetchUserSession();
 });
