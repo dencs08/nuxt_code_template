53b2e34c4b7e58bf04ada20e056a2dfbf0533b88
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-03-07 21:32:04 +0100

    Fix bug in marquee-base functionality

--- components/design/marquee/marquee-base.vue
+++ components/design/marquee/marquee-base.vue
@@ -1,39 +1,64 @@
 <template>
-    <div :class="['marquee', { vertical: props.vertical, horizontal: !props.vertical, 'cursor-pointer': props.pauseOnClick }]"
-        :style="marqueeStyle" :key="componentKey" @mouseenter="hoverStart" @mouseleave="hoverEnd" @click="togglePause">
-        <div ref="marqueeOverlay" class="marquee-overlay" v-if="props.gradient"></div>
-        <div ref="marqueeContent"
-            :class="['marquee-content', { vertical: props.vertical, horizontal: !props.vertical }]">
+    <div :class="['vue3-marquee', { vertical, horizontal: !vertical, 'cursor-pointer': pauseOnClick }]"
+        :style="getCurrentStyle" @mouseenter="hoverStarted" @mouseleave="hoverEnded" @mousedown="mouseDown"
+        @mouseup="mouseUp" v-if="ready" :key="componentKey">
+        <div class="transparent-overlay" aria-hidden="true" ref="marqueeOverlayContainer"></div>
+        <div v-if="gradient" class="overlay" :class="{ vertical, horizontal: !vertical }" aria-hidden="true"></div>
+        <div class="marquee" ref="marqueeContent">
             <slot></slot>
         </div>
-        <!-- <div v-if="!animateOnOverflowOnly || (animateOnOverflowOnly && !animateOnOverflowPause)"
-            class="additional-content">
-            <slot></slot>
-        </div> -->
-        <div v-if="clone" v-for="num in cloneAmount" :key="num" aria-hidden="true" class="marquee-content cloned">
+        <div v-if="!animateOnOverflowOnly || animateOnOverflowOnly && !animateOnOverflowPause" class="marquee"
+            aria-hidden="true">
             <slot></slot>
         </div>
+        <template v-if="clone" v-for="num in cloneAmount">
+            <div aria-hidden="true" class="marquee cloned">
+                <slot></slot>
+            </div>
+        </template>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 const props = defineProps({
     vertical: Boolean,
-    direction: { type: String, default: 'normal' },
-    duration: { type: Number, default: 20 },
-    delay: { type: Number, default: 0 },
-    loop: { type: Number, default: Infinity },
+    direction: String,
+    duration: Number,
+    delay: Number,
+    loop: Number,
     clone: Boolean,
     gradient: Boolean,
-    gradientColor: { type: Array, default: () => [255, 255, 255] },
+    gradientColor: Array,
     gradientWidth: String,
-    gradientLength: { type: String, default: '200px' },
+    gradientLength: String,
     pauseOnHover: Boolean,
     pauseOnClick: Boolean,
     pause: Boolean,
     animateOnOverflowOnly: Boolean
 });
-defineEmits([
+
+// Refs
+const cloneAmount = ref(0);
+const minWidth = ref("100%");
+const minHeight = ref("100%");
+const componentKey = ref(0);
+const verticalAnimationPause = ref(false);
+const animateOnOverflowPause = ref(false);
+const containerWidth = ref(0);
+const contentWidth = ref(0);
+const containerHeight = ref(0);
+const contentHeight = ref(0);
+const mouseOverMarquee = ref(false);
+const mouseDownMarquee = ref(false);
+const gradientLength = ref("200px");
+const loopInterval = ref(null);
+const loopCounter = ref(0);
+const marqueeContent = ref(null);
+const marqueeOverlayContainer = ref(null);
+const ready = ref(false);
+
+// Emits
+const emit = defineEmits([
     "onComplete",
     "onLoopComplete",
     "onPause",
@@ -42,91 +67,27 @@ defineEmits([
     "onOverflowCleared"
 ]);
 
-const isPaused = ref(false);
-const cloneAmount = ref(0);
-const componentKey = ref(0);
+// Computed
+const getCurrentStyle = computed(() => ({
+    "--duration": `${props.duration || 10}s`,
+    "--delay": `${props.delay || 0}s`,
+    "--direction": props.direction || 'normal',
+    "--pauseOnHover": mouseOverMarquee.value ? 'paused' : 'running',
+    "--pauseOnClick": mouseDownMarquee.value ? 'paused' : 'running',
+    "--pauseAnimation": props.pause || verticalAnimationPause.value || animateOnOverflowPause.value ? 'paused' : 'running',
+    "--loops": props.loop === 0 ? 'infinite' : props.loop || 'infinite',
+    "--gradient-color": `rgba(${props.gradientColor?.join(',') || '255,255,255'}, 1), rgba(${props.gradientColor?.join(',') || '255,255,255'}, 0)`,
+    "--gradient-length": props.gradientLength || gradientLength.value || '100px',
+    "--min-width": minWidth.value || '100px',
+    "--min-height": minHeight.value || '100px',
+    "--orientation": props.vertical ? 'scrollY' : 'scrollX'
+}));
 
-const contentWidth = ref(0);
-const containerWidth = ref(0);
-const contentHeight = ref(0);
-const containerHeight = ref(0);
-
-const minWidth = ref("100%");
-const minHeight = ref("100%");
-
-const marqueeContent = ref<HTMLElement | null>(null);
-const marqueeOverlayContainer = ref<HTMLElement | null>(null);
-const marqueeOverlay = ref<HTMLElement | null>(null);
-const children = ref<HTMLElement[]>([]);
-
-const verticalAnimationPause = ref(false);
-const animateOnOverflowPause = ref(false);
-
-onMounted(async () => {
-    children.value = Array.from(marqueeContent.value.children) as HTMLElement[];
-    console.log(children.value);
-
-    nextTick(() => {
-        if (marqueeContent.value) {
-            children.value = Array.from(marqueeContent.value.children) as HTMLElement[];
-        }
-    });
-});
-
-const marqueeStyle = computed(() => {
-    const baseStyles = {
-        '--duration': `${props.duration}s`,
-        '--delay': `${props.delay}s`,
-        '--direction': props.direction,
-        '--loops': props.loop === Infinity ? 'infinite' : props.loop.toString(),
-        '--gradient-color': props.gradient ? `linear-gradient(${props.vertical ? 'to bottom' : 'to right'}, rgba(${props.gradientColor.join(',')}, 1), rgba(${props.gradientColor.join(',')}, 0))` : '',
-        '--gradient-length': props.gradientLength,
-        '--pauseAnimation': `${isPaused.value ? 'paused' : 'running'}`,
-        '--min-width': `${minWidth.value}`,
-        '--min-height': `${minHeight.value}`
-    };
-
-    // Additional styles based on vertical orientation
-    const orientationStyles = props.vertical ? {
-        '--orientation': 'scrollY'
-    } : {
-        '--orientation': 'scrollX',
-    };
-
-    return {
-        ...baseStyles,
-        ...orientationStyles
-    };
-});
-
-watch(contentWidth, async () => {
-    if (props.clone) {
-        ForcesUpdate();
-    }
-});
-
-watch(containerWidth, async () => {
-    if (props.clone || props.animateOnOverflowOnly) {
-        ForcesUpdate();
-    }
-});
-
-watch(() => props.pause, (newVal, oldVal) => {
-    if (newVal !== oldVal) {
-        if (newVal) {
-            emit("onResume");
-        } else {
-            emit("onPause");
-        }
-    }
-});
-
-const ForcesUpdate = async () => {
-    await checkForClone();
-    componentKey.value++;
-};
-
+// Methods
 const checkForClone = async () => {
+    if (props.vertical) {
+        verticalAnimationPause.value = true;
+    }
     setInterval(() => {
         minWidth.value = "0%";
         minHeight.value = "0%";
@@ -165,148 +126,243 @@ const checkForClone = async () => {
             return 0;
         }
     }, 100);
-}
+};
 
-function hoverStart() {
+const hoverStarted = () => {
     if (props.pauseOnHover) {
-        isPaused.value = true;
+        mouseOverMarquee.value = true;
+        emit("onPause");
     }
-}
-
-function hoverEnd() {
+};
+const hoverEnded = () => {
     if (props.pauseOnHover) {
-        isPaused.value = false;
+        mouseOverMarquee.value = false;
+        emit("onResume");
     }
-}
-
-function mouseDown() {
+};
+const mouseDown = () => {
     if (props.pauseOnClick) {
-        isPaused.value = true;
+        mouseDownMarquee.value = true;
+        emit("onPause");
     }
-}
-
-function mouseUp() {
+};
+const mouseUp = () => {
     if (props.pauseOnClick) {
-        isPaused.value = false;
+        mouseDownMarquee.value = false;
+        emit("onResume");
     }
-}
+};
+const click = () => {
+    if (props.pauseOnClick) {
+        togglePause()
+        emit("onPause");
+    }
+};
+const togglePause = () => {
+    mouseDownMarquee.value = !mouseDownMarquee.value;
+};
 
-function togglePause() {
-    if (props.pauseOnHover && isPaused.value == true) return;
-    isPaused.value = !isPaused.value;
-}
+const ForcesUpdate = async () => {
+    await checkForClone();
+    componentKey.value++;
+};
+
+const setupMarquee = async () => {
+    if (props.vertical) {
+        minHeight.value = "100%";
+        minWidth.value = "auto";
+        if (props.animateOnOverflowOnly) {
+            console.warn("The `animateOnOverflowOnly` prop is not supported for vertical marquees.");
+        }
+    } else {
+        minHeight.value = "auto";
+        if (props.animateOnOverflowOnly) {
+            minWidth.value = "auto";
+        } else {
+            minWidth.value = "100%";
+        }
+    }
+    if (props.gradient) {
+        if (props.gradientWidth) {
+            console.warn("The `gradientWidth` prop has been deprecated and will be removed in a future release. Please use `gradientLength` instead.");
+            gradientLength.value = props.gradientWidth;
+        } else if (props.gradientLength) {
+            gradientLength.value = props.gradientLength;
+        }
+    }
+    if (props.clone || props.animateOnOverflowOnly) {
+        await checkForClone();
+        ForcesUpdate();
+        ready.value = true;
+    } else {
+        ready.value = true;
+    }
+};
+
+// Lifecycle
+onMounted(async () => {
+    setupMarquee();
+    loopInterval.value = setInterval(() => {
+        loopCounter.value++;
+        if (props.loop !== 0 && loopCounter.value === props.loop) {
+            emit("onComplete");
+            clearInterval(loopInterval.value);
+        }
+        emit("onLoopComplete");
+    }, props.duration * 1e3);
+})
+
+onBeforeUnmount(() => {
+    clearInterval(loopInterval.value);
+})
+
+// Watchers
+watch(contentWidth, async () => {
+    if (props.clone) {
+        ForcesUpdate();
+    }
+});
+watch(containerWidth, async () => {
+    if (props.clone || props.animateOnOverflowOnly) {
+        ForcesUpdate();
+    }
+});
+watch(() => props.pause, (newVal, oldVal) => {
+    if (newVal !== oldVal) {
+        if (newVal) {
+            emit("onResume");
+        } else {
+            emit("onPause");
+        }
+    }
+});
 </script>
 
 <style>
-.marquee {
+.vue3-marquee {
     display: flex;
     position: relative;
     overflow: hidden;
 }
 
-.marquee>.marquee-content {
-    /* display: flex;
-    flex: 0 0 auto; */
+.vue3-marquee.horizontal {
+    overflow: hidden;
+    flex-direction: row;
+    width: 100%;
+    height: max-content
+}
+
+.vue3-marquee.vertical {
+    overflow: hidden;
+    flex-direction: column;
+    height: 100%;
+    width: max-content
+}
+
+.vue3-marquee:hover>.marquee {
+    animation-play-state: var(--pauseOnHover)
+}
+
+.vue3-marquee:active>.marquee {
+    animation-play-state: var(--pauseOnClick)
+}
+
+.vue3-marquee>.marquee {
+    flex: 0 0 auto;
+    min-width: var(--min-width);
+    min-height: var(--min-height);
     z-index: 1;
     animation: var(--orientation) var(--duration) linear var(--delay) var(--loops);
     animation-play-state: var(--pauseAnimation);
     animation-direction: var(--direction)
 }
 
-.marquee>.marquee-content.vertical {
-    overflow-y: hidden;
-    display: flex;
-    flex-direction: column;
-    align-items: left;
-}
-
-.marquee>.marquee-content.horizontal {
-    overflow-x: hidden;
+.vue3-marquee.horizontal>.marquee {
     display: flex;
     flex-direction: row;
-    align-items: center;
-    justify-content: left;
+    align-items: center
 }
 
-.marquee>.marquee-overlay {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: var(--gradient-color);
-    mask-image: var(--gradient-color);
-    -webkit-mask-image: var(--gradient-color);
-    mask-size: var(--gradient-length) 100%;
-    -webkit-mask-size: var(--gradient-length) 100%;
+.vue3-marquee.vertical>.marquee {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 }
 
+@keyframes scrollX {
+    0% {
+        transform: translate(0)
+    }
 
-.marquee>.transparent-overlay {
+    to {
+        transform: translate(-100%)
+    }
+}
+
+@keyframes scrollY {
+    0% {
+        transform: translateY(0)
+    }
+
+    to {
+        transform: translateY(-100%)
+    }
+}
+
+.vue3-marquee>.overlay {
+    pointer-events: none;
     position: absolute;
     width: 100%;
     height: 100%
 }
 
-.marquee>.marquee-overlay:before,
-.marquee>.marquee-overlay:after {
+.vue3-marquee>.transparent-overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%
+}
+
+.vue3-marquee>.overlay:before,
+.vue3-marquee>.overlay:after {
     content: "";
     position: absolute;
     z-index: 2
 }
 
-.marquee.horizontal>.marquee-overlay:before,
-.marquee.horizontal>.marquee-overlay:after {
+.vue3-marquee.horizontal>.overlay:before,
+.vue3-marquee.horizontal>.overlay:after {
     background: linear-gradient(to right, var(--gradient-color));
     height: 100%;
     width: var(--gradient-length)
 }
 
-.marquee.vertical>.marquee-overlay:before,
-.marquee.vertical>.marquee-overlay:after {
+.vue3-marquee.vertical>.overlay:before,
+.vue3-marquee.vertical>.overlay:after {
     background: linear-gradient(to bottom, var(--gradient-color));
     height: var(--gradient-length);
     width: 100%
 }
 
-.marquee.horizontal>.marquee-overlay:after {
+.vue3-marquee.horizontal>.overlay:after {
     transform: rotate(180deg)
 }
 
-.marquee.vertical>.marquee-overlay:after {
+.vue3-marquee.vertical>.overlay:after {
     transform: rotate(-180deg)
 }
 
-.marquee>.marquee-overlay:before {
+.vue3-marquee>.overlay:before {
     left: 0;
     top: 0
 }
 
-.marquee.horizontal>.marquee-overlay:after {
+.vue3-marquee.horizontal>.overlay:after {
     right: 0;
     top: 0
 }
 
-.marquee.vertical>.marquee-overlay:after {
+.vue3-marquee.vertical>.overlay:after {
     left: 0;
     bottom: 0
-}
-
-@keyframes scrollX {
-    from {
-        transform: translateX(0);
-    }
-
-    to {
-        transform: translateX(-100%);
-    }
-}
-
-@keyframes scrollY {
-    from {
-        transform: translateY(0);
-    }
-
-    to {
-        transform: translateY(-100%);
-    }
 }
 </style>
