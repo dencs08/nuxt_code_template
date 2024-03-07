8a184c2329c2b364e64d3664caaf496f803c4a7e
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-03-07 21:32:10 +0100

    Refactor i18n-dropdown component

--- components/utils/dropdowns/i18n-dropdown.vue
+++ components/utils/dropdowns/i18n-dropdown.vue
@@ -1,3 +1,28 @@
+<template>
+    <Dropdown :modelValue="selectedLocale" @update:modelValue="changeLocale($event)" :options="availableLocales"
+        optionLabel="iso" optionValue="code" :pt="{
+        root: ({ props, state }) => ({
+            class: [
+                'inline-flex relative bg-surface-0 dark:bg-surface-900 border border-surface-300 dark:border-surface-700 rounded-md transition-all duration-200 hover:border-primary-500 dark:hover:border-primary-300 cursor-pointer select-none',
+
+                'w-30 md:w-32', //control width of this dropdown
+
+                { 'outline-none outline-offset-0 ring ring-primary-400/50 dark:ring-primary-300/50': state.focused },
+                { 'opacity-60': props.disabled, 'pointer-events-none': props.disabled, 'cursor-default': props.disabled }
+            ]
+        })
+    }">
+    </Dropdown>
+    <!-- <div v-auto-animate class="absolute w-screen h-screen top-0 left-0 pointer-events-none">
+            <div v-if="isLoading" class=" bg-gray-900/60 overflow-hidden">
+                <div class="flex flex-col items-center justify-center w-screen h-screen text-center space-y-5">
+                    <Icon name="svg-spinners:ring-resize" class="w-16 h-16 text-white" />
+                    <p class="text-3xl text-white font-bold font-header">Translating in process, please wait</p>
+                </div>
+            </div>
+        </div> -->
+</template>
+
 <script setup>
 const router = useRouter();
 const { locales, setLocale, getLocaleCookie } = useI18n()
@@ -34,28 +59,3 @@ const changeLocale = (newLocale) => {
     document.body.style.overflow = '';
 }
 </script>
-
-<template>
-    <Dropdown :modelValue="selectedLocale" @update:modelValue="changeLocale($event)" :options="availableLocales"
-        optionLabel="iso" optionValue="code" :pt="{
-            root: ({ props, state }) => ({
-                class: [
-                    'inline-flex relative bg-surface-0 dark:bg-surface-900 border border-surface-300 dark:border-surface-700 rounded-md transition-all duration-200 hover:border-primary-500 dark:hover:border-primary-300 cursor-pointer select-none',
-
-                    'w-30 md:w-32', //control width of this dropdown
-
-                    { 'outline-none outline-offset-0 ring ring-primary-400/50 dark:ring-primary-300/50': state.focused },
-                    { 'opacity-60': props.disabled, 'pointer-events-none': props.disabled, 'cursor-default': props.disabled }
-                ]
-            })
-        }">
-    </Dropdown>
-    <!-- <div v-auto-animate class="absolute w-screen h-screen top-0 left-0 pointer-events-none">
-            <div v-if="isLoading" class=" bg-gray-900/60 overflow-hidden">
-                <div class="flex flex-col items-center justify-center w-screen h-screen text-center space-y-5">
-                    <Icon name="svg-spinners:ring-resize" class="w-16 h-16 text-white" />
-                    <p class="text-3xl text-white font-bold font-header">Translating in process, please wait</p>
-                </div>
-            </div>
-        </div> -->
-</template>
