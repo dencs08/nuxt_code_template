19e9663caf2e74173027f06a9733cf103baf62b8
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-03-08 16:18:22 +0100

    Refactor default layout to use TypeScript and improve SEO attributes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,30 @@
-<script setup>
+<script setup lang="ts">
 const route = useRoute()
 const { t } = useI18n()
-const head = useLocaleHead({
+const i18nHead = useLocaleHead({
   addDirAttribute: true,
   identifierAttribute: 'id',
-  addSeoAttributes: true
+  addSeoAttributes: {
+    canonicalQueries: ['']
+  },
 })
-const title = computed(() => t('layouts.default.title', { title: t(route.meta.title ?? 'TBD') }))
+useHead({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs?.lang,
+  },
+  title: t('layouts.default.title', { title: t(route.meta.title || 'TBD') }),
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])]
+})
+
 const isDark = useDark()
-const html = ref();
 
 onMounted(() => {
   isDark.value = false;
 });
-
 </script>
 
 <template>
-  <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir" ref="html">
-
-  <Head>
-    <Title>{{ title }}</Title>
-    <template v-for="link in head.link" :key="link.id">
-      <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
-    </template>
-    <template v-for="meta in head.meta" :key="meta.id">
-      <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
-    </template>
-  </Head>
 
   <Body class="bg-gray-50 text-gray-900">
     <Navbar />
@@ -36,6 +33,4 @@ onMounted(() => {
     </main>
     <Footer />
   </Body>
-
-  </Html>
 </template>
