a34232ef97f9e41ea0c3669f98ed8ae46dfb7b4f
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-03-08 16:18:47 +0100

    Add delayHydration configuration file

---
+++ config/delayHydration.ts
@@ -0,0 +1,11 @@
+// config/delayHydration.ts
+
+export default {
+    // enables nuxt-delay-hydration in dev mode for testing
+    // NOTE: you should disable this once you've finished testing, it will break HMR
+    // 'init' most risky for 3rd party but 90-100% reduction,
+    // 'mount' safer for 3rd party but 70% reduction
+    // 'manual' reduction as used
+    mode: 'init', 
+    //debug: process.env.NODE_ENV === 'development'
+};
