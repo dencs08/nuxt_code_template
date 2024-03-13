436d15a77bfaf54bd976a1a6407560e4d069c072
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-03-13 23:35:49 +0100

    Add API endpoints for deleting and fetching user account

---
+++ server/api/me/index.delete.ts
@@ -0,0 +1,32 @@
+import { serverSupabaseServiceRole, serverSupabaseUser } from "#supabase/server";
+import { defineWrappedResponseHandler } from '@/server/utils/defaultHandler'
+
+export default defineWrappedResponseHandler (async (event) => {
+    const client = serverSupabaseServiceRole(event);
+    const userSession = await serverSupabaseUser(event)
+
+    try {
+        const { data, error } = await client.auth.admin.deleteUser(userSession!.id)
+        if (error) {
+            throw new Error('Error deleting authentication data: ' + error.message);
+        }
+
+        const { error: deleteError } = await client
+            .from('users')
+            .delete()
+            .eq('id', userSession!.id);
+        if (deleteError) {
+            throw createError({
+                statusCode: 500,
+                statusMessage: 'Error deleting user data: ' + deleteError.message,
+            })
+        }
+
+        return { response: 'User account deleted successfully.' };
+    } catch (err: any) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'An error occurred during the deletion process ' + err.message,
+        })
+    }
+}, 'guest');
---
+++ server/api/me/index.get.ts
@@ -0,0 +1,18 @@
+import { serverSupabaseServiceRole, serverSupabaseUser } from "#supabase/server";
+import { defineWrappedResponseHandler } from '@/server/utils/defaultHandler'
+
+export default defineWrappedResponseHandler(async (event) => {
+    const client = serverSupabaseServiceRole(event);
+    const userSession = await serverSupabaseUser(event)
+
+    await checkUserRole(event, 'user');
+
+    try {
+        return { response: 'Account fetched', account: userSession };
+    } catch (err: any) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'An error occurred during the fetching process ' + err.message,
+        })
+    }
+}, 'guest');
