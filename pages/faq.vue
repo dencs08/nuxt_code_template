926689a88b6dc5660c8b9b7baa739c0666ef87cf
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-03-18 19:21:14 +0100

    Refactor faq.vue to update meta tags and add canonical URL

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 const { t, locale } = useI18n()
-const title = computed(() => t('pages.index.title'))
-const description = computed(() => t('pages.index.description'))
+const business = useBusiness()
+const route = useRoute()
+
+const title = computed(() => t('pages.index.meta.title'))
+const description = computed(() => t('pages.index.meta.description'))
+
+const url = business.url.value + route.fullPath;
 
 useHead({
     title: title,
+    link: [{ rel: 'canonical', href: url }],
     meta: [
         { property: 'description', content: description },
         { property: 'og:description', content: description },
