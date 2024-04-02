eafdba5b4ea242e6440313a7f73192e6f3ab033b
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-04-02 11:54:09 +0200

    Add business configuration settings

---
+++ config/general/businessConfig.ts
@@ -0,0 +1,12 @@
+// config/businessConfig.ts
+export default {
+    businessName: 'Business Name',
+    businessUrl: 'https://localhost:3000',
+    
+    businessEmail: 'test@test.pl',
+    businessPhone: '123123123',
+
+    businessCity: 'City Name',
+    businessZip: '12-123',
+    businessStreet: '123 Business St',
+};
