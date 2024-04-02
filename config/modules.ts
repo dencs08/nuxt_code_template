f2c05b5becee87e7142f85da8153060df82add27
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-04-02 11:52:44 +0200

    Add new modules and update dependencies

--- config/modules.ts
+++ config/modules.ts
@@ -3,29 +3,40 @@
 export default [
     //development
     "@nuxt/devtools",
-    "@nuxtjs/storybook",
+    '@nuxtjs/device',
+    'nuxt-typed-router',
+    // "@nuxtjs/storybook",
+
+    //core (required for most template features)
+    "@nuxtjs/supabase",
+    "@nuxtjs/i18n",
+    "@nuxt/content",
+    "@formkit/nuxt",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    // "nuxtjs/apollo", //uncomment when using wordpress backend with graphql
     
     //seo
     '@nuxtjs/web-vitals',
     "@nuxtjs/robots",
     "@nuxtjs/sitemap",
+    'nuxt-link-checker',
+    'nuxt-schema-org',
+    'nuxt-og-image',
+    // "nuxt-gtag", //uncomment for adding gtag integration
     
-    //core
-    "@nuxtjs/supabase",
-    "@nuxtjs/i18n",
+    //ui/ux
     "@nuxtjs/tailwindcss",
-    "@nuxt/content",
-    "@nuxt/image",
-    "@nuxt/fonts",
-    "nuxt-icon",
     "nuxt-primevue",
-    "@formkit/nuxt",
-    "@pinia/nuxt",
-    "@vueuse/nuxt",
+    "@nuxt/image",
+    "nuxt-icon",
+    "@nuxt/fonts",
 
-    // "@nuxtjs/google-fonts", //migrating to official module nuxt-fonts
+    //additional modules not required but used in the project.
+    "@sidebase/nuxt-pdf",
+
+    // "@nuxtjs/google-fonts", //migrating to official module nuxt-fonts probaly will be removed in future.
 
     //optional
     // "@vite-pwa/nuxt", //uncomment for generating a PWA for mobiles.
-    // "nuxt-gtag", //uncomment for adding gtag integration
 ];
