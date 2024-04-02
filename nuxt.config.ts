9dd1b16ab4fa2de085607357e7d9d080d9affcb9
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-04-02 11:51:56 +0200

    Refactor Nuxt config imports and organize sections

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,64 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
-import supabase from "./config/supabase";
-import i18n from "./config/i18n";
+// Core
 import runtimeConfig from "./config/runtime";
 import modules from "./config/modules";
 import components from "./config/components";
 import hooks from "./config/hooks";
+
+// Development
+import storybook from "./config/storybook";
+import linkChecker from "./config/linkChecker";
+import device from "./config/device";
+
+// SEO
+import robots from "./config/robots";
+import ogImage from "./config/ogImage";
+
+// UI/UX
 import primevue from "./config/primevue";
+import fonts from "./config/fonts";
+import pwa from "./config/pwa";
+
+// Other imports
+import supabase from "./config/supabase";
+import i18n from "./config/i18n";
 import content from "./config/content";
 import image from "./config/image";
 import webVitals from "./config/webVitals";
 import site from "./config/site";
-import robots from "./config/robots";
 import routeRules from "./config/routeRules";
-import storybook from "./config/storybook";
-import fonts from "./config/fonts";
-import pwa from "./config/pwa";
-
-// import googleFonts from "./config/googleFonts";
+// import apollo from "./config/apollo"
 
 export default defineNuxtConfig({
+    // Core
     runtimeConfig,
-    supabase,
     modules,
     components,
-    i18n,
     hooks,
-    primevue,
+    routeRules,
+    supabase,
     content,
     image,
-    webVitals,
     site,
-    robots,
-    routeRules,
-    fonts,
-    storybook,
-    // pwa, //when using nuxt pwa module
-    // googleFonts,
+    i18n,
+    // apollo,
 
+    // Development
     devtools: {
         enabled: true,
     },
+    storybook,
+    device,
+    
+    // SEO
+    robots,
+    ogImage,
+    linkChecker,
+
+    // UI/UX
+    primevue,
+    fonts,
+    pwa,
+    webVitals,
 
     app: {
         pageTransition: { name: "page", mode: "out-in" },
