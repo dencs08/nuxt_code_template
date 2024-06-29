3f4fb95ee91adb67bc0c9294b2ecb7a8eca22b92
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-06-29 14:42:49 +0200

    refactor: Update i18n-dropdown component to use Select instead of Dropdown

--- components/utils/forms/dropdown/i18n-dropdown.vue
+++ components/utils/forms/dropdown/i18n-dropdown.vue
@@ -1,18 +1,7 @@
 <template>
-    <Dropdown :modelValue="selectedLocale" @update:modelValue="changeLocale($event)" :options="availableLocales"
-        optionLabel="iso" optionValue="code" :pt="{
-        root: ({ props, state }) => ({
-            class: [
-                'inline-flex relative bg-surface-0 dark:bg-surface-900 border border-surface-300 dark:border-surface-700 rounded-md transition-all duration-200 hover:border-primary-500 dark:hover:border-primary-300 cursor-pointer select-none',
-
-                'w-30 md:w-32', //control width of this dropdown
-
-                { 'outline-none outline-offset-0 ring ring-primary-400/50 dark:ring-primary-300/50': state.focused },
-                { 'opacity-60': props.disabled, 'pointer-events-none': props.disabled, 'cursor-default': props.disabled }
-            ]
-        })
-    }">
-    </Dropdown>
+    <Select :modelValue="selectedLocale" @update:modelValue="changeLocale($event)" :options="availableLocales"
+        optionLabel="iso" optionValue="code">
+    </Select>
     <!-- <div v-auto-animate class="absolute w-screen h-screen top-0 left-0 pointer-events-none">
             <div v-if="isLoading" class=" bg-gray-900/60 overflow-hidden">
                 <div class="flex flex-col items-center justify-center w-screen h-screen text-center space-y-5">
