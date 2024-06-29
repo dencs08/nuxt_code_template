bf0f9dbce57fb86dbfbfc8df1ccb8d934f0d6446
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-06-29 14:42:11 +0200

    refactor: Update PrimeVue configuration for version 4.0 integration

--- config/primevue.ts
+++ config/primevue.ts
@@ -1,22 +1,27 @@
 // config/primevue.ts
 export default {
-    importPT: { from: "@/assets/primevue/lara", as: "Lara" }, //import and apply preset
+    // importPT: { from: "@/assets/primevue/lara", as: "Lara" }, //import and apply preset
+    autoImport: true,
+    importTheme: { from: '@/assets/primevue/app-theme.js' },
     options: {
         ripple: false,
+        unstyled: false,
+        cssLayer: {
+            name: 'primevue',
+            order: 'tailwind-base, primevue, tailwind-utilities'
+        },
         // inputStyle: "outlined",
-        // cssLayerOrder: "reset,primevue",
-        unstyled: true,
     },
-    components: {
-        // prefix: "prime", //breaks sfxformkit componenets...
-        // include: "*",
-        exclude: ['AutoComplete', 'CascadeSelect', 'ColorPicker', 'Knob', 'Galleria', 'TreeTable', 'PickList','DataView', 'OrderList', 'OrganizationChart'],
-    },
-    composables: {
-        // include: "*",
-    },
-    directives: {
-        // include: "*",
-        // exclude: ['Ripple']
-    },
+    // components: {
+    //     // prefix: "prime", //breaks sfxformkit componenets...
+    //     // include: "*",
+    //     exclude: ['AutoComplete', 'CascadeSelect', 'ColorPicker', 'Knob', 'Galleria', 'TreeTable', 'PickList','DataView', 'OrderList', 'OrganizationChart'],
+    // },
+    // composables: {
+    //     // include: "*",
+    // },
+    // directives: {
+    //     // include: "*",
+    //     // exclude: ['Ripple']
+    // },
 };
