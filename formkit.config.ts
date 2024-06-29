5b05ca30592a11c4a1747cee030a4bc8db4de70e
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-06-29 14:39:06 +0200

    chore: Update FormKit configuration and dependencies

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,21 +1,33 @@
 // formkit.config.ts
 import type { DefaultConfigOptions } from "@formkit/vue";
-import { primeInputs } from "@sfxcode/formkit-primevue";
+import { createAutoAnimatePlugin } from '@formkit/addons'
+
+// import { primeInputs } from "@sfxcode/formkit-primevue";
+import { primeInputs } from "@/components/utils/forms/formkit/src";
+// import { appInputs } from "@/components/utils/forms/formkit/definitions";
+
 import { en, pl, de } from "@formkit/i18n";
 
 import { generateClasses } from "@formkit/themes";
 import { genesisIcons } from "@formkit/icons";
-import myTailwindTheme from "./custom_tailwindcss/formKit";
+// import myTailwindTheme from "./custom_tailwindcss/formKit";
 
 const config: DefaultConfigOptions = {
+    plugins: [
+        createAutoAnimatePlugin()
+    ],
+
     icons: {
         ...genesisIcons,
     },
-    config: {
-        classes: generateClasses(myTailwindTheme),
-    },
+    // config: {
+    //     classes: generateClasses(myTailwindTheme),
+    // },
 
-    inputs: primeInputs,
+    inputs: {
+        ...primeInputs,
+        // ...appInputs,
+    },
     locales: { de, pl, en },
 };
 
