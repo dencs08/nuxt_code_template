14c50157ee597f5db84a442eeebf86010a30fd3e
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-06-29 22:28:10 +0200

    update: form-wrapper use Zod for validation & auto clear on submit

--- components/utils/form-wrapper.vue
+++ components/utils/form-wrapper.vue
@@ -1,50 +1,45 @@
 <script setup lang="ts">
+import { createZodPlugin } from '@formkit/zod';
+import type { z, ZodTypeAny } from 'zod';
+import { reset } from '@formkit/core';
+import { v4 as uuidv4 } from 'uuid';
+
 const props = defineProps({
   handleSubmit: {
     type: Function,
     required: true,
   },
+  zodSchema: {
+    type: Object as () => ZodTypeAny,
+    required: true,
+  },
 });
 
+const formId = ref(uuidv4());
+const formRef = ref<HTMLElement | null>(null);
+
+// Use the passed Zod schema
+const [zodPlugin, submitHandler] = createZodPlugin(
+  props.zodSchema,
+  async (formData: object) => {
+    await new Promise((resolve) => { setTimeout(resolve, 250); });
+    await props.handleSubmit(formData);
+    resetFields();
+  }
+);
+
+const resetFields = () => {
+  reset(formId.value);
+}
+
 const data = reactive({})
-const nodes = ref([])
-
-const getNode = (node: HTMLAllCollection) => {
-  nodes.value.push(node)
-}
-
-const reset = () => {
-  nodes.value.forEach(node => {
-    node.reset()
-  })
-  nodes.value = []
-}
-
-const formSubmitted = (data: any) => {
-  return new Promise<void>((resolve, reject) => {
-    const start = Date.now();
-
-    props.handleSubmit(data).then(() => {
-      //always add fake 250ms time to request so formkit has time to show the spinner
-      const duration = Date.now() - start;
-      const delay = Math.max(0, 250 - duration);
-
-      setTimeout(() => {
-        reset();
-        resolve();
-      }, delay);
-    }).catch((error: any) => {
-      reset();
-      reject(error);
-    });
-  });
-};
-
 </script>
 
 <template>
-  <FormKit type="form" v-model="data" :submit-attrs="{ inputClass: 'btn btn-primary btn-sm w-full' }"
-    @submit="formSubmitted" :incomplete-message="false">
-    <slot v-bind:getNode="getNode"></slot>
+  <FormKit :id="formId" type="form" v-model="data" :submit-attrs="{ inputClass: 'btn btn-primary btn-sm w-full' }"
+    @submit="submitHandler" :incomplete-message="false" :plugins="[zodPlugin]">
+    <div ref="formRef">
+      <slot></slot>
+    </div>
   </FormKit>
 </template>
