4eae10f74dd3e6aa81c1c8f96d54daeb881b06ac
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-07 13:59:04 +0200

    refactor: Update form-wrapper component to use Zod schema for improved structure and validation

---
+++ components/utils/forms/form-wrapper.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { createZodPlugin } from "@formkit/zod";
+import { z, type ZodTypeAny } from "zod";
+import { reset } from "@formkit/core";
+import { v4 as uuidv4 } from "uuid";
+
+const props = defineProps({
+  handleSubmit: {
+    type: Function,
+    required: true,
+  },
+  zodSchema: {
+    type: Object as () => ZodTypeAny,
+    required: false,
+  },
+  resetOnSubmit: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const formId = ref(uuidv4());
+const formRef = ref<HTMLElement | null>(null);
+
+const defaultSchema = z.object({});
+const schema = props.zodSchema || defaultSchema;
+
+const [zodPlugin, submitHandler] = createZodPlugin(
+  schema,
+  async (formData: object) => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 250);
+    });
+    await props.handleSubmit(formData);
+    if (props.resetOnSubmit) resetFields();
+  }
+);
+
+const resetFields = () => {
+  reset(formId.value);
+};
+
+const data = reactive({});
+</script>
+
+<template>
+  <FormKit
+    :id="formId"
+    type="form"
+    v-model="data"
+    :submit-attrs="{ inputClass: 'btn btn-primary btn-sm w-full' }"
+    @submit="submitHandler"
+    :incomplete-message="false"
+    :plugins="[zodPlugin]"
+  >
+    <div ref="formRef">
+      <slot></slot>
+    </div>
+  </FormKit>
+</template>
