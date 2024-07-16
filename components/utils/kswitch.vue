d516860245173de256a2a7d1917a1c4f93bf4034
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-16 23:47:32 +0200

    refactor: Add k-switch component for displaying disabled application message

---
+++ components/utils/kswitch.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-screen w-screen flex flex-col justify-center items-center">
+    <h1>Application has been disabled</h1>
+    <h2>
+      Contact the support at
+      <!-- TODO add a global way of defining the support email -->
+      <a class="underline" href="mailto:support@domain.pl">support@domain.pl</a>
+    </h2>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
