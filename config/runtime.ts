246997785dae1b000fed7307103bd55a57650777
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-18 00:04:18 +0200

    refactor: Update config/runtime.ts to include authProvider and backendProvider in public export

--- config/runtime.ts
+++ config/runtime.ts
@@ -1,9 +1,10 @@
 // config/runtime.ts
 
 export default {
-    public: {
-        global_route_access: process.env.GLOBAL_ROUTE_ACCESS,
-
-        apiBase: process.env.BASE_API_URL,
-    },
+  public: {
+    global_route_access: process.env.GLOBAL_ROUTE_ACCESS,
+    apiBase: process.env.BASE_API_URL,
+    authProvider: process.env.AUTH_PROVIDER,
+    backendProvider: process.env.BACKEND_PROVIDER,
+  },
 };
