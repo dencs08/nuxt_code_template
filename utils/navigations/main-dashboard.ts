4809cca6942dae1a458777857e01e1296ad04014
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-17 22:10:57 +0200

    refactor: Update main-dashboard.ts to use new config files

--- utils/navigations/main-dashboard.ts
+++ utils/navigations/main-dashboard.ts
@@ -1,149 +1,88 @@
+import type { RoutesNamesList } from "@typed-router/__routes";
 import { validRoles } from "../roles";
-import { routeSettings } from "@/routes/routeSettings";
+import { routes } from "@/config/common/routes";
+import { dashboardMenu } from "@/config/common/menus";
 const { hasAccess: originalHasAccess } = useRoleCheck();
 const localePath = useLocalePath();
 
 // main-dashboard.ts
 export function mainDashboardNavigation() {
-    const dashboardRoutes = {
-        home: "dash-home",
-        inbox: "dash-mail-inbox",
-        mail: "dash-mail-inbox",
+  const getAccessLevelForRoute = (input: any): string => {
+    // Handle cases where input is an object with a 'route' key
+    let routeName =
+      typeof input === "object" && input !== null ? input.route : input;
 
-        user: {},
+    // Ensure routeName is a string before proceeding
+    if (typeof routeName !== "string") {
+      return ""; // Return a default value indicating no specific access
+    }
 
-        admin: {
-            users: "dash-admin-users",
-            marketing: "dash-admin-marketing",
-            analytics: {
-                route: "dash-admin-analytics",
-                users: "dash-admin-analytics-users",
-            },
-        },
-    };
+    // First, try to get an explicit setting from the combined routes structure
+    const routeMatch = routes.find((route) => route.newName === routeName);
+    if (routeMatch && routeMatch.settings && routeMatch.settings.access) {
+      return routeMatch.settings.access; // If an explicit setting is found, return it immediately
+    }
 
-    const navigation = computed(() => [
-        {
-            label: "Main",
-            icon: "pi pi-home",
-            route: dashboardRoutes.home,
-        },
-        {
-            label: "Marketing",
-            icon: "pi pi-percentage",
-            route: dashboardRoutes.admin.marketing,
-            // badge: 5,
-        },
-        {
-            label: "Mail",
-            icon: "pi pi-envelope",
-            checkRoute: dashboardRoutes.mail,
-            items: [
-                {
-                    label: "Inbox",
-                    icon: "pi pi-inbox",
-                    route: dashboardRoutes.inbox,
-                    badge: 5,
-                    // shortcut: "⌘+N",
-                },
-            ],
-        },
-        {
-            label: "Analytics",
-            icon: "pi pi-chart-bar",
-            checkRoute: dashboardRoutes.admin.analytics, //for checking if the current route is active
-            items: [
-                {
-                    label: "Users",
-                    icon: "pi pi-users",
-                    route: dashboardRoutes.admin.analytics.users,
-                },
-            ],
-        },
-        {
-            label: "Administrative",
-            icon: "pi pi-home",
-            items: [
-                {
-                    label: "Users",
-                    icon: "pi pi-users",
-                    route: dashboardRoutes.admin.users,
-                },
-            ],
-        },
-    ]);
+    // If no explicit setting is found, attempt to determine the access level from the route path
+    const pathSegments = routeName.split("-").filter(Boolean); // Remove any empty segments
+    const reversedSegments = pathSegments.reverse();
 
-    const getAccessLevelForRoute = (input: any): string => {
-        // Handle cases where input is an object with a 'route' key
-        let routeName = typeof input === "object" && input !== null ? input.route : input;
+    for (const segment of reversedSegments) {
+      const roleMatch = validRoles.find(
+        (role) => segment.toLowerCase() === role.value
+      );
+      if (roleMatch) {
+        return roleMatch.value; // If a matching role is found, return its value as the access level
+      }
+    }
 
-        // Ensure routeName is a string before proceeding
-        if (typeof routeName !== "string") {
-            return ""; // Return a default value indicating no specific access
+    return ""; // Default to no specific access level if no matches are found
+  };
+
+  const hasAccess = (accessLevel: string) => {
+    if (!accessLevel) return true; //if no access level is set, allow access
+    return originalHasAccess(accessLevel);
+  };
+
+  const filterNavigationItems = (items: any) => {
+    return items.reduce((acc: any, item: any) => {
+      // Check if the current item or any of its sub-items have access
+      const accessLevel = getAccessLevelForRoute(item.checkRoute || item.route);
+
+      const hasItemAccess = hasAccess(accessLevel);
+
+      if (hasItemAccess || (item.items && item.items.length > 0)) {
+        let newItem = { ...item };
+
+        // Convert route to locale path if route is provided
+        if (newItem.route) {
+          newItem.route = convertToLocalePath(newItem.route);
         }
 
-        // First, try to get an explicit setting from routeSettings
-        const setting = routeSettings[routeName as keyof typeof routeSettings]?.access;
-        if (setting) {
-            return setting; // If an explicit setting is found, return it immediately
+        // Filter sub-items if they exist
+        if (newItem.items && newItem.items.length > 0) {
+          newItem.items = filterNavigationItems(newItem.items);
         }
 
-        // If no explicit setting is found, attempt to determine the access level from the route path
-        const pathSegments = routeName.split("-").filter(Boolean); // Remove any empty segments
-        const reversedSegments = pathSegments.reverse();
-
-        for (const segment of reversedSegments) {
-            const roleMatch = validRoles.find((role) => segment.toLowerCase() === role.value);
-            if (roleMatch) {
-                return roleMatch.value; // If a matching role is found, return its value as the access level
-            }
+        // Only add items that themselves or their sub-items have access
+        if (hasItemAccess || newItem.items.length > 0) {
+          acc.push(newItem);
         }
 
-        return ""; // Default to no specific access level if no matches are found
-    };
+        // console.log(accessLevel, hasItemAccess, newItem);
+      }
 
-    const hasAccess = (accessLevel: string) => {
-        if (!accessLevel) return true; //if no access level is set, allow access
-        return originalHasAccess(accessLevel);
-    };
+      return acc;
+    }, []);
+  };
 
-    const filterNavigationItems = (items: any) => {
-        return items.reduce((acc: any, item: any) => {
-            // Check if the current item or any of its sub-items have access
-            const accessLevel = getAccessLevelForRoute(item.checkRoute || item.route);
+  const convertToLocalePath = (routeName: string) => {
+    return localePath({ name: routeName as RoutesNamesList });
+  };
 
-            const hasItemAccess = hasAccess(accessLevel);
+  const filteredDashboardNavigation = computed(() => {
+    return filterNavigationItems(dashboardMenu);
+  });
 
-            if (hasItemAccess || (item.items && item.items.length > 0)) {
-                let newItem = { ...item };
-
-                // Convert route to locale path if route is provided
-                if (newItem.route) {
-                    newItem.route = convertToLocalePath(newItem.route);
-                }
-
-                // Filter sub-items if they exist
-                if (newItem.items && newItem.items.length > 0) {
-                    newItem.items = filterNavigationItems(newItem.items);
-                }
-
-                // Only add items that themselves or their sub-items have access
-                if (hasItemAccess || newItem.items.length > 0) {
-                    acc.push(newItem);
-                }
-            }
-
-            return acc;
-        }, []);
-    };
-
-    const filteredDashboardNavigation = computed(() => {
-        return filterNavigationItems(navigation.value);
-    });
-
-    const convertToLocalePath = (routeName: string) => {
-        return localePath({ name: routeName });
-    };
-
-    return filteredDashboardNavigation;
+  return filteredDashboardNavigation;
 }
