5a46db887988a13236c101fb918e7c3fba856252
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-17 22:10:02 +0200

    refactor: Update useSubmit.ts to improve error handling and add support for captcha token

--- composables/useSubmit.ts
+++ composables/useSubmit.ts
@@ -1,41 +1,44 @@
 export function useSubmit() {
-    const { addToast } = useToastService();
-    const localePath = useLocalePath();
+  const { addToast } = useToastService();
+  const localePath = useLocalePath();
 
-    const defaultSuccessHandler =
-        (message: string, redirectPath?: string, external?: boolean) => (response: any) => {
-            addToast("success", "Success", message);
-            if (redirectPath) {
-                navigateTo(localePath(redirectPath), { external: external ?? false });
-            }
-        };
-
-    const defaultErrorHandler = (error: any) => {
-        const errorMessage = error.message || "An unknown error occurred.";
-        addToast("error", "Error", errorMessage, 30000);
+  const defaultSuccessHandler =
+    (message: string, redirectPath?: string, external?: boolean) =>
+    (response: any) => {
+      addToast("success", "Success", message);
+      if (redirectPath) {
+        navigateTo(localePath(redirectPath), { external: external ?? false });
+      }
     };
 
-    async function handleSubmit(
-        action: (data: any) => Promise<any>,
-        data: any,
-        successMessage: string,
-        redirectPath?: string,
-        external?: boolean,
-        errorMessage?: string,
-        onSuccess: (response: any) => void = defaultSuccessHandler(
-            successMessage,
-            redirectPath,
-            external
-        ),
-        onError: (error: any) => void = defaultErrorHandler
-    ) {
-        try {
-            const response = await action(data);
-            onSuccess(response);
-        } catch (error) {
-            onError(error);
-        }
-    }
+  const defaultErrorHandler = (error: any) => {
+    const errorMessage = error.message || "An unknown error occurred.";
+    addToast("error", "Error", errorMessage, 30000);
+  };
 
-    return { handleSubmit };
+  async function handleSubmit(
+    action: (data: any) => Promise<any>,
+    data: any,
+    successMessage: string,
+    redirectPath?: string,
+    external?: boolean,
+    errorMessage?: string,
+    captchaToken?: string,
+    onSuccess: (response: any) => void = defaultSuccessHandler(
+      successMessage,
+      redirectPath,
+      external
+    ),
+    onError: (error: any) => void = defaultErrorHandler
+  ) {
+    try {
+      const requestData = captchaToken ? { ...data, captchaToken } : data;
+      const response = await action(requestData);
+      onSuccess(response);
+    } catch (error) {
+      onError(error);
+    }
+  }
+
+  return { handleSubmit };
 }
