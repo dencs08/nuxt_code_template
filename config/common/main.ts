c16c2d7c7625d5838d217e58c179fd80063b0e4c
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-17 22:09:37 +0200

    add TODO: Move common configurations from env to main.ts

---
+++ config/common/main.ts
@@ -0,0 +1,11 @@
+//file for most common configurations
+
+//TODO move the below from env to here
+// GLOBAL_ROUTE_AUTH=true
+// GLOBAL_ROUTE_ACCESS=guest
+
+// #BACKEND_PROVIDER supabase | laravel
+// BACKEND_PROVIDER=supabase
+
+// #AUTH_PROVIDER supabase | sidebase
+// AUTH_PROVIDER=supabase
