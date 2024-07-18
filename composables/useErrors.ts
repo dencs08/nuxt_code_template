eaff287a636077c408469a45268a3e8fb4308d00
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-18 17:04:24 +0200

    refactor: Update useErrorHandler to handle unhandled rejections and display error messages

--- composables/useErrors.ts
+++ composables/useErrors.ts
@@ -1,34 +1,34 @@
 // useError.ts
 export class CustomError extends Error {
-    value: any;
+  value: any;
 
-    constructor(message: string, value: any) {
-        super(message);
-        this.value = value;
-    }
+  constructor(message: string, value?: any) {
+    super(message);
+    this.value = value;
+  }
 }
 
 export function useCustomError() {
-    return { CustomError };
+  return { CustomError };
 }
 
 export function useErrorHandler() {
-    const { addToast } = useToastService();
-    const { CustomError } = useCustomError();
+  const { addToast } = useToastService();
+  const { CustomError } = useCustomError();
 
-    const errorHandler = () => {
-        onMounted(() => {
-            window.addEventListener("unhandledrejection", (event) => {
-                event.preventDefault();
-                const error = event.reason;
-                if (error instanceof CustomError) {
-                    const errorMessage = error.message;
-                    addToast("error", "Error", errorMessage, 30000);
-                } else {
-                    // Do something else
-                }
-            });
-        });
-    };
-    return { errorHandler };
+  const errorHandler = () => {
+    onMounted(() => {
+      window.addEventListener("unhandledrejection", (event) => {
+        event.preventDefault();
+        const error = event.reason;
+        if (error instanceof CustomError) {
+          const errorMessage = error.message;
+          addToast("error", "Error", errorMessage, 30000);
+        } else {
+          // Do something else
+        }
+      });
+    });
+  };
+  return { errorHandler };
 }
