50846bb0dbf3f48fac56edb23b82715132dc1d48
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-22 14:44:18 +0200

    refactor: Update config/runtime.ts to include authProvider and backendProvider in public export

--- config/common/main.ts
+++ config/common/main.ts
@@ -1,11 +1,13 @@
 //file for most common configurations
 
-//TODO move the below from env to here
-// GLOBAL_ROUTE_AUTH=true
-// GLOBAL_ROUTE_ACCESS=guest
+// BACKEND_PROVIDER supabase | laravel
+// AUTH_PROVIDER supabase | sidebase
+// AUTH_SIDEBASE authjs | local | refresh
 
-// #BACKEND_PROVIDER supabase | laravel
-// BACKEND_PROVIDER=supabase
-
-// #AUTH_PROVIDER supabase | sidebase
-// AUTH_PROVIDER=supabase
+export default {
+  GLOBAL_ROUTE_AUTH: "true",
+  GLOBAL_ROUTE_ACCESS: "guest",
+  BACKEND_PROVIDER: "supabase",
+  AUTH_PROVIDER: "supabase",
+  AUTH_SIDEBASE: "authjs",
+};
--- config/runtime.ts
+++ config/runtime.ts
@@ -1,10 +1,11 @@
 // config/runtime.ts
+import mainConfig from "./common/main";
 
 export default {
   public: {
-    global_route_access: process.env.GLOBAL_ROUTE_ACCESS,
+    global_route_access: mainConfig.GLOBAL_ROUTE_ACCESS,
+    authProvider: mainConfig.AUTH_PROVIDER,
+    backendProvider: mainConfig.BACKEND_PROVIDER,
     apiBase: process.env.BASE_API_URL,
-    authProvider: process.env.AUTH_PROVIDER,
-    backendProvider: process.env.BACKEND_PROVIDER,
   },
 };
