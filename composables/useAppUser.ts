3c3b239ea0d74bc1bfcfa83be11282de60bc0a90
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-22 15:00:24 +0200

    refactor: Update useStateUser to fetch user using useAuthentication().getUser

---
+++ composables/useAppUser.ts
@@ -0,0 +1,23 @@
+import { type UserAuthPublicSession } from "../utils/types";
+
+import { ref, onMounted } from "vue";
+const useStateUser = () => {
+  const user = ref<UserAuthPublicSession | null>(null);
+
+  onMounted(() => {
+    useAuthentication()
+      .getUser()
+      .then((fetchedUser: UserAuthPublicSession) => {
+        user.value = fetchedUser;
+      })
+      .catch((error: Error) => {
+        console.error("Failed to fetch user:", error);
+      });
+  });
+
+  return {
+    user,
+  };
+};
+
+export default useStateUser;
