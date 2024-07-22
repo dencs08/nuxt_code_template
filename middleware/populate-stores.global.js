1ab0bb4630320b67f3b9a84d1cdc6c884947fab9
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-22 15:00:53 +0200

    refactor: Update populate-stores.global.js to fetch user based on authentication requirements

--- middleware/populate-stores.global.js
+++ middleware/populate-stores.global.js
@@ -1,4 +1,16 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const usersStore = useUsersStore();
-  await usersStore.fetchUserSession();
+  const authRequired = to.meta.auth;
+
+  if (authRequired && authRequired.unauthenticatedOnly) {
+    if (!usersStore.user) {
+      await usersStore.fetchUser();
+    }
+  }
+
+  if (authRequired === undefined || authRequired === true) {
+    if (!usersStore.user) {
+      await usersStore.fetchUser();
+    }
+  }
 });
