bb1f0c449138db3407ab8ef1f600250dcadc2216
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-22 14:48:58 +0200

    refactor: Optimize profile avatar component for better performance and code readability

--- components/user-applayer/profile/profile-avatar.vue
+++ components/user-applayer/profile/profile-avatar.vue
@@ -1,25 +1,42 @@
 <template>
-    <div
-        class="border border-surface-500 dark:border-surface-600 rounded overflow-hidden shadow-[0_1px_10px_-1px_rgba(0,0,0,0.5)]">
-        <Skeleton v-if="!userSession && !photoSrc || userStore.loading" height="100%" width="100%" class="object-cover">
-        </Skeleton>
-        <nuxt-img v-else-if="!photoSrc" :src="'img/avatar.svg'" class="h-full w-full object-cover"></nuxt-img>
-        <nuxt-img v-else :src="photoSrc" class="h-full w-full object-cover"></nuxt-img>
-    </div>
+  <div
+    class="border border-surface-500 dark:border-surface-600 rounded overflow-hidden shadow-[0_1px_10px_-1px_rgba(0,0,0,0.5)]"
+  >
+    <Skeleton
+      v-if="(!userSession && !photoSrc) || userStore.loading"
+      height="100%"
+      width="100%"
+      class="object-cover"
+    >
+    </Skeleton>
+    <nuxt-img
+      v-else-if="!photoSrc"
+      :src="'img/avatar.svg'"
+      class="h-full w-full object-cover"
+    ></nuxt-img>
+    <nuxt-img
+      v-else
+      :src="photoSrc"
+      class="h-full w-full object-cover"
+    ></nuxt-img>
+  </div>
 </template>
 
 <script setup>
 const props = defineProps({
-    photo: String,
-})
+  photo: String,
+});
 
 const userStore = useUsersStore();
-const userSession = await userStore.getUserSession;
-const photoSrc = ref(props.photo || userSession.photo);
+const userSession = await userStore.getUser;
+const photoSrc = ref(props.photo || user.photo);
 
-watch(() => userStore.userSession.photo, (newPhoto) => {
+watch(
+  () => userStore.user.photo,
+  (newPhoto) => {
     photoSrc.value = newPhoto;
-});
+  }
+);
 </script>
 
 <style></style>
