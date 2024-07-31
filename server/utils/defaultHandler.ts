02b4e44536c6f1a84873674945d96ba090dade1a
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-31 12:29:13 +0200

    refactor: Improve defineWrappedResponseHandler in defaultHandler.ts
    This commit improves the `defineWrappedResponseHandler` function in the `defaultHandler.ts` file. The changes include formatting improvements and code reorganization. The refactoring aims to enhance the readability and maintainability of the function.
    
    #refactor #codeimprovement

--- server/utils/defaultHandler.ts
+++ server/utils/defaultHandler.ts
@@ -1,26 +1,29 @@
-import type { EventHandler, EventHandlerRequest, H3Event } from 'h3'
+import type { EventHandler, EventHandlerRequest, H3Event } from "h3";
 import { serverSupabaseUser } from "#supabase/server";
 
-type ExtendedEventHandler<T extends EventHandlerRequest, D> = (event: H3Event<T>, userSession: any) => Promise<D>;
+type ExtendedEventHandler<T extends EventHandlerRequest, D> = (
+  event: H3Event<T>,
+  userSession: any
+) => Promise<D>;
 
-export const defineWrappedResponseHandler = <T extends EventHandlerRequest, D> (
-    handler: ExtendedEventHandler<T, D>,
-    minRole?: string
+export const defineWrappedResponseHandler = <T extends EventHandlerRequest, D>(
+  handler: ExtendedEventHandler<T, D>,
+  minRole?: string
 ): EventHandler<T, D> =>
-    defineEventHandler<T>(async event => {
-        try {
-            const userSession = await serverSupabaseUser(event)
+  defineEventHandler<T>(async (event) => {
+    try {
+      const userSession = await serverSupabaseUser(event);
 
-            if (minRole) {
-                await checkUserRole(event, minRole);
-            }
+      if (minRole) {
+        await checkUserRole(event, minRole);
+      }
 
-            const response = await handler(event, userSession)
-            return { response }
-        } catch (err: any) {
-            throw createError({
-                statusCode: err.statusCode || 500,
-                statusMessage: err.statusMessage
-            })
-        }
-    });
+      const response = await handler(event, userSession);
+      return { response };
+    } catch (err: any) {
+      throw createError({
+        statusCode: err.statusCode || 500,
+        statusMessage: err.statusMessage,
+      });
+    }
+  });
