0810a7cb8214483b62742db2fb89b3b9468c6d26
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-07-31 12:10:01 +0200

    refactor: Fetch user in authentication middleware and remove unused code

--- app/middleware/populate-stores.global.js
+++ app/middleware/populate-stores.global.js
@@ -1,16 +1,4 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  const userStore = useUserStore();
-  const authRequired = to.meta.auth;
-
-  if (authRequired && authRequired.unauthenticatedOnly) {
-    if (!userStore.user) {
-      await userStore.fetchUser();
-    }
-  }
-
-  if (authRequired === undefined || authRequired === true) {
-    if (!userStore.user) {
-      await userStore.fetchUser();
-    }
-  }
+  // const userStore = useUserStore();
+  // const authRequired = to.meta.auth;
 });
