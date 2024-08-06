42604ae0fe888dbe204a477b3d0d9d0ef1a0564e
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-06 15:03:35 +0200

    refactor: Remove unused code in useAuthListeners.ts
    This commit removes the unused code in the useAuthListeners.ts file. The commented out code related to the Supabase client, AuthEvent, and AuthSubscription
    #refactor #codecleanup

--- app/composables/useAuthListeners.ts
+++ app/composables/useAuthListeners.ts
@@ -1,58 +1,55 @@
-import { type Session } from "@supabase/supabase-js";
-type AuthEvent =
-  | "INITIAL_SESSION"
-  | "SIGNED_IN"
-  | "SIGNED_OUT"
-  | "PASSWORD_RECOVERY"
-  | "TOKEN_REFRESHED"
-  | "USER_UPDATED";
+// import { type Session } from "@supabase/supabase-js";
+// type AuthEvent =
+//   | "INITIAL_SESSION"
+//   | "SIGNED_IN"
+//   | "SIGNED_OUT"
+//   | "PASSWORD_RECOVERY"
+//   | "TOKEN_REFRESHED"
+//   | "USER_UPDATED";
 
-type AuthSubscription = {
-  unsubscribe: () => void;
-};
+// type AuthSubscription = {
+//   unsubscribe: () => void;
+// };
 
 export function useAuthListeners() {
-  const client = useSupabaseClient();
-
-  const handleAuthStateChange = (
-    authEvent: AuthEvent,
-    callback: (event: AuthEvent, session: any) => void
-  ) => {
-    const {
-      data: { subscription },
-    } = client.auth.onAuthStateChange((event, session) => {
-      if (event === authEvent) callback(event, session);
-    });
-
-    const authSubscription = subscription as AuthSubscription;
-
-    // Return a function to cancel the subscription
-    return () => {
-      if (authSubscription) {
-        authSubscription.unsubscribe();
-      }
-    };
-  };
-
-  const useAuthEvent = (
-    authEvent: AuthEvent,
-    callback: (event: AuthEvent, session: Session | null) => void
-  ) => {
-    let unsubscribe: (() => void) | undefined;
-
-    onMounted(() => {
-      unsubscribe = handleAuthStateChange(authEvent, callback);
-    });
-
-    onUnmounted(() => {
-      if (unsubscribe) {
-        unsubscribe();
-      }
-    });
-  };
-
-  return {
-    handleAuthListener: handleAuthStateChange,
-    useAuthEvent,
-  };
+  // const client = useSupabaseClient();
+  // const handleAuthStateChange = (
+  //   authEvent: AuthEvent,
+  //   callback: (event: AuthEvent, session: any) => void
+  // ) => {
+  //   const {
+  //     data: { subscription },
+  //   } = client.auth.onAuthStateChange((event, session) => {
+  //     if (event === authEvent) callback(event, session);
+  //   });
+  //   const authSubscription = subscription as AuthSubscription;
+  //   // Return a function to cancel the subscription
+  //   return () => {
+  //     if (authSubscription) {
+  //       authSubscription.unsubscribe();
+  //     }
+  //   };
 }
+
+// const useAuthEvent = (
+//   authEvent: AuthEvent,
+//   callback: (event: AuthEvent, session: Session | null) => void
+// ) => {
+//   let unsubscribe: (() => void) | undefined;
+
+//   onMounted(() => {
+//     unsubscribe = handleAuthStateChange(authEvent, callback);
+//   });
+
+//   onUnmounted(() => {
+//     if (unsubscribe) {
+//       unsubscribe();
+//     }
+//   });
+// };
+
+// return {
+//   handleAuthListener: handleAuthStateChange,
+//   useAuthEvent,
+// };
+// }
