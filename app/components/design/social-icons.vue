f3588ccf7c5645aa3da8413bc4a8e140386a8e76
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-09 00:54:43 +0200

    refactor: Add social icons component

---
+++ app/components/design/social-icons.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-row gap-3 md:gap-1">
+    <div v-for="item in socialMenu" :key="item.label">
+      <a
+        :href="item.href"
+        class="text-gray-400 hover:text-primary-500 hover:scale-110 transition-all"
+      >
+        <span class="sr-only">{{ item.label }}</span>
+        <Icon
+          :name="item.icon"
+          class="h-8 md:h-6 w-auto text-gray-400 hover:text-primary-400 hover:scale-105 transistion"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { socialMenu } = useNavigation();
+</script>
+
+<style></style>
