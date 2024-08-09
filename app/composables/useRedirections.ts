c1241add0cf0125e59bc1fb7fbc0c598459a3f65
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-09 18:54:31 +0200

    refactor: Add email request change page to useRedirections

--- app/composables/useRedirections.ts
+++ app/composables/useRedirections.ts
@@ -3,9 +3,11 @@ import redirects from "@/config/common/redirects";
 export const useRedirections = () => {
   const passwordChangePage = () => redirects.onPasswordChange();
   const generalPage = () => redirects.onGeneralRedirect();
+  const emailRequestChangePage = () => redirects.onEmailRequestChange();
 
   return {
     passwordChangePage,
     generalPage,
+    emailRequestChangePage,
   };
 };
--- app/config/common/redirects.ts
+++ app/config/common/redirects.ts
@@ -2,4 +2,5 @@
 export default {
   onPasswordChange: () => "update-password",
   onGeneralRedirect: () => "index",
+  onEmailRequestChange: () => "dashboard/account/confirm-email-change",
 };
