6bd6a61879c526b4ef79c1ea10a24e48d8cc9383
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-09 18:55:21 +0200

    add: documentation comments

--- app/middleware/error-code-testing.global.js
+++ app/middleware/error-code-testing.global.js
@@ -1,6 +1,10 @@
+// This middleware is used to test error handling in the application.
+// This will navigate to the /home route and trigger a 500 Internal Server Error.
+// URL: http://localhost:3000/home?errorCode=500
+
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const errorCode = to.query.errorCode;
-    if (errorCode) {
-        throw createError({ statusCode: parseInt(errorCode) });
-    }
+  const errorCode = to.query.errorCode;
+  if (errorCode) {
+    throw createError({ statusCode: parseInt(errorCode) });
+  }
 });
