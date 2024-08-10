b43e7dec4b052475ad308833a3acd88257f788a4
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-10 11:15:13 +0200

    refactor: Add getUser and getMePermissions methods to BackendClient interface

--- types/backend.ts
+++ types/backend.ts
@@ -1,6 +1,7 @@
 export interface BackendClient {
   //users
   getUsers(): Promise<any[]>;
+  getUser(userId: string): Promise<any>;
   getAuthUsers(): Promise<any[]>;
   deleteUser(userId: string): Promise<void>;
   createUser(data: any, event: any): Promise<any>;
@@ -16,6 +17,7 @@ export interface BackendClient {
   putMe(user: any, data: any): Promise<any>;
   updateMeEmail(user: any, body: any): Promise<any>;
   updateMePhoto(userId: any, photoUrl: any): Promise<any>;
+  getMePermissions(): Promise<any>;
 
   //utils
   assignRole(event: any, body: { id: string; role: string }): Promise<any>;
@@ -30,4 +32,22 @@ export interface BackendClient {
     upsert: boolean
   ): Promise<any>;
   deleteFile(bucketName: string, filePath: string): Promise<any>;
+
+  //permissions
+  getPermissions(userId: string): Promise<any>;
+  getAvailablePermissions(): Promise<any>;
+  updatePermissions(userId: string, permissions: any): Promise<any>;
+
+  //roles
+  getRoles(): Promise<any>;
+  getRolePermissions(roleId: number): Promise<any>;
+  addRolePermission(roleId: number, permissionId: number): Promise<any>;
+  removeRolePermission(roleId: number, permissionId: number): Promise<any>;
+  getUserPermissions(userId: string): Promise<any>;
+  getUserRole(userId: string): Promise<any>;
+
+  //newsletter
+  addNewsletterSubscriber(email: string): Promise<any>;
+  deleteNewsletterSubscriber(email: string): Promise<any>;
+  getNewsletterSubscribers(): Promise<any>;
 }
