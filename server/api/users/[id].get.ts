07cebb4af17a2001ff8f07f980f5e36b3bcebbfc
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-10 11:16:10 +0200

    refactor: Add API endpoint and handler for retrieving user by ID

---
+++ server/api/users/[id].get.ts
@@ -0,0 +1,18 @@
+import { getBackendClient } from "~~/lib/backend";
+import { defineWrappedResponseHandler } from "../../utils/defaultHandler";
+
+export default defineWrappedResponseHandler(async (event) => {
+  const client = await getBackendClient(event, true);
+  let user = {};
+  const { id } = event.context.params as { id: string };
+
+  try {
+    user = await client.getUser(id);
+    return user;
+  } catch (err: any) {
+    throw createError({
+      statusCode: err.code,
+      statusMessage: err.message,
+    });
+  }
+}, "admin");
