5654c5014e01b43b904592fc6201da17c5eeb44c
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-11 16:03:59 +0200

    refactor: main-dashboard to use new acess_level instead of role

--- app/utils/navigations/main-dashboard.ts
+++ app/utils/navigations/main-dashboard.ts
@@ -5,26 +5,25 @@ import { dashboardMenu } from "@/config/common/menus";
 const { hasAccess: originalHasAccess } = useRoleCheck();
 const localePath = useLocalePath();
 
-// main-dashboard.ts
 export function mainDashboardNavigation() {
-  const getAccessLevelForRoute = (input: any): string => {
-    // Handle cases where input is an object with a 'route' key
+  const getAccessLevelForRoute = (input: any): number => {
     let routeName =
       typeof input === "object" && input !== null ? input.route : input;
 
-    // Ensure routeName is a string before proceeding
     if (typeof routeName !== "string") {
-      return ""; // Return a default value indicating no specific access
+      return 0; // Default to lowest access level
     }
 
-    // First, try to get an explicit setting from the combined routes structure
     const routeMatch = routes.find((route) => route.newName === routeName);
-    if (routeMatch && routeMatch.settings && routeMatch.settings.access) {
-      return routeMatch.settings.access; // If an explicit setting is found, return it immediately
+    if (
+      routeMatch &&
+      routeMatch.settings &&
+      typeof routeMatch.settings.access === "number"
+    ) {
+      return routeMatch.settings.access;
     }
 
-    // If no explicit setting is found, attempt to determine the access level from the route path
-    const pathSegments = routeName.split("-").filter(Boolean); // Remove any empty segments
+    const pathSegments = routeName.split("-").filter(Boolean);
     const reversedSegments = pathSegments.reverse();
 
     for (const segment of reversedSegments) {
@@ -32,45 +31,37 @@ export function mainDashboardNavigation() {
         (role) => segment.toLowerCase() === role.value
       );
       if (roleMatch) {
-        return roleMatch.value; // If a matching role is found, return its value as the access level
+        return roleMatch.access_level;
       }
     }
 
-    return ""; // Default to no specific access level if no matches are found
+    return 0; // Default to lowest access level
   };
 
-  const hasAccess = (accessLevel: string) => {
-    if (!accessLevel) return true; //if no access level is set, allow access
+  const hasAccess = (accessLevel: number) => {
     return originalHasAccess(accessLevel);
   };
 
   const filterNavigationItems = (items: any) => {
     return items.reduce((acc: any, item: any) => {
-      // Check if the current item or any of its sub-items have access
       const accessLevel = getAccessLevelForRoute(item.checkRoute || item.route);
 
       const hasItemAccess = hasAccess(accessLevel);
-      // console.log(accessLevel, hasItemAccess, hasItemAccess);
 
       if (hasItemAccess || (item.items && item.items.length > 0)) {
         let newItem = { ...item };
 
-        // Convert route to locale path if route is provided
         if (newItem.route) {
           newItem.route = convertToLocalePath(newItem.route);
         }
 
-        // Filter sub-items if they exist
         if (newItem.items && newItem.items.length > 0) {
           newItem.items = filterNavigationItems(newItem.items);
         }
 
-        // Only add items that themselves or their sub-items have access
         if (hasItemAccess || newItem.items.length > 0) {
           acc.push(newItem);
         }
-
-        // console.log(accessLevel, hasItemAccess, newItem);
       }
 
       return acc;
