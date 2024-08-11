0481700d516d90c0b6a088c2ef2d69dba17a9564
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-11 16:02:25 +0200

    refactor: Add lazy loading for user roles in middleware

--- app/middleware/01.populate-stores.global.js
+++ app/middleware/01.populate-stores.global.js
@@ -2,11 +2,16 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const authRequired = to.meta.auth;
   const layout = to.meta.layout;
   const userStore = useUserStore();
+  const rolesStore = useRolesStore();
   const permissionStore = useMyPermissionStore();
 
   if (!userStore.user && authRequired != false) {
     await userStore.fetchUser();
   }
+  if (!rolesStore.roles.value) {
+    await rolesStore.fetchRoles();
+  }
+
   if (layout === "dashboard") {
     await permissionStore.fetchMyPermissions();
   }
