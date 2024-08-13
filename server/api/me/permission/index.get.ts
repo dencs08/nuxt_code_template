904db90fb4064ed6048be7cdf7dde0b9eb50b3bf
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-13 23:05:42 +0200

    refactor: Update error handling in SupabaseClient to use error code and message

--- server/api/me/permission/index.get.ts
+++ server/api/me/permission/index.get.ts
@@ -8,9 +8,19 @@ export default defineWrappedResponseHandler(async (event) => {
     const permissions = await client.getMePermissions();
     return permissions;
   } catch (error) {
-    throw createError({
-      statusCode: 500,
-      message: (error as Error).message,
-    });
+    throw error;
   }
 }, 0);
+
+// export default defineEventHandler(async (event) => {
+//   const client = await getBackendClient(event, true);
+//   try {
+//     const response = await client.getMePermissions();
+//     return { response };
+//   } catch (error: any) {
+//     throw createError({
+//       statusCode: error.code,
+//       message: (error as Error).message,
+//     });
+//   }
+// });
