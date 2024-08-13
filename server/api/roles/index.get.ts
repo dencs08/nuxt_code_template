c7b591998c77b3d0f8720378d70b3db7547ef16b
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-13 23:05:57 +0200

    refactor: Update getRoles API endpoint to use SupabaseClient

--- server/api/roles/index.get.ts
+++ server/api/roles/index.get.ts
@@ -1,19 +1,32 @@
 import { defineWrappedResponseHandler } from "../../utils/defaultHandler";
 import { getBackendClient } from "~~/lib/backend";
-import { getRoles } from "~~/server/utils/role-cache";
 
-export default defineWrappedResponseHandler(async (event) => {
-  const client = await getBackendClient(event, true);
+// export default defineWrappedResponseHandler(async (event) => {
+//   const client = await getBackendClient(event, true);
+
+//   try {
+//     const response = await client.getRoles();
+//     // const response = await getRoles(event);
+
+//     return response;
+//   } catch (err: any) {
+//     throw createError({
+//       statusCode: err.code,
+//       statusMessage: err.message,
+//     });
+//   }
+// }, 0);
+
+export default defineEventHandler(async (event) => {
+  const client = await getBackendClient(event, false);
 
   try {
-    // const response = await client.getRoles();
-    const response = await getRoles(event);
-
-    return response;
+    const response = await client.getRoles();
+    return { response };
   } catch (err: any) {
     throw createError({
       statusCode: err.code,
       statusMessage: err.message,
     });
   }
-}, 25);
+});
