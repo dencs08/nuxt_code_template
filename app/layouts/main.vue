e3f94d582d9d9c07b5aadf24e66b090914a611c1
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-15 02:54:55 +0200

    refactor: Update main layout to include dashboard link in navbar

--- app/layouts/main.vue
+++ app/layouts/main.vue
@@ -1,44 +1,46 @@
 <script setup lang="ts">
-const route = useRoute()
-const { t } = useI18n()
+const route = useRoute();
+const { t } = useI18n();
 
 const i18nHead = useLocaleHead({
-    addDirAttribute: true,
-    identifierAttribute: 'id',
-    addSeoAttributes: {
-        canonicalQueries: ['']
-    },
-})
-
-const title = computed(() => {
-    const titleKey = typeof route.meta.title === 'string' || typeof route.meta.title === 'number'
-        ? route.meta.title
-        : 'TBD';
-    return t('layouts.default.title', { title: t(titleKey) })
-})
-
-useHead({
-    htmlAttrs: {
-        lang: i18nHead.value.htmlAttrs?.lang,
-    },
-    title: title,
-    link: [...(i18nHead.value.link || [])],
-    meta: [...(i18nHead.value.meta || [])]
-})
-
-const isDark = useDark()
-
-onMounted(() => {
-    isDark.value = false;
+  addDirAttribute: true,
+  identifierAttribute: "id",
+  addSeoAttributes: {
+    canonicalQueries: [""],
+  },
 });
 
+const title = computed(() => {
+  const titleKey =
+    typeof route.meta.title === "string" || typeof route.meta.title === "number"
+      ? route.meta.title
+      : "TBD";
+  return t("layouts.default.title", { title: t(titleKey) });
+});
+
+useHead({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs?.lang,
+  },
+  title: title,
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
+});
+
+const isDark = useDark();
+
+onMounted(() => {
+  isDark.value = false;
+});
 </script>
 
 <template>
-
-    <Body class="bg-gray-50 text-gray-900">
-        <main>
-            <slot />
-        </main>
-    </Body>
+  <Body class="bg-gray-50 text-gray-900">
+    <main>
+      <nav>
+        <NuxtLink to="/dashboard">dashboard</NuxtLink>
+      </nav>
+      <slot />
+    </main>
+  </Body>
 </template>
