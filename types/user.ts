36d3bbfcd5df4a1261f2371706a142ec861c4815
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-15 02:54:15 +0200

    refactor: add new app_metadata field

--- types/user.ts
+++ types/user.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { userSchema } from "~~/utils/schemas";
+import { userSchema } from "../utils/schemas";
 
 export type UserForm = z.infer<typeof userSchema>;
 
@@ -13,4 +13,5 @@ export interface UserAuthPublicSession {
   user_roles?: { role_id: number; roles: { name: string } };
   provider?: string;
   metadata?: any;
+  app_metadata?: any;
 }
