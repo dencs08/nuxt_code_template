0dd83a6c19d3870d379ea2eb571a24f8793321b5
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-15 02:56:12 +0200

    refactor: Update getMePermissions API endpoint to accept userId parameter

--- server/api/me/permission/index.get.ts
+++ server/api/me/permission/index.get.ts
@@ -1,26 +1,12 @@
-import { getBackendClient } from "~~/lib/backend";
-import { defineWrappedResponseHandler } from "~~/server/utils/defaultHandler";
+import { defineWrappedResponseHandler } from "../../../utils/defaultHandler";
+import { getBackendClient } from "../../../../lib/backend";
 
-export default defineWrappedResponseHandler(async (event) => {
-  const client = await getBackendClient(event, true);
-
+export default defineWrappedResponseHandler(async (event, userSession) => {
+  const client = await getBackendClient(event);
   try {
-    const permissions = await client.getMePermissions();
-    return permissions;
+    const response = await client.getMePermissions(userSession.id);
+    return response;
   } catch (error) {
     throw error;
   }
 }, 0);
-
-// export default defineEventHandler(async (event) => {
-//   const client = await getBackendClient(event, true);
-//   try {
-//     const response = await client.getMePermissions();
-//     return { response };
-//   } catch (error: any) {
-//     throw createError({
-//       statusCode: error.code,
-//       message: (error as Error).message,
-//     });
-//   }
-// });
