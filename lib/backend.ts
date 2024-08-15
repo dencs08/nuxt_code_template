a848e98bd6e21cb029f226a7e07a400626fc6e36
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-15 02:55:51 +0200

    refactor: Update backend client initialization to use supabaseClient directly and not cache the clients

--- lib/backend.ts
+++ lib/backend.ts
@@ -1,4 +1,4 @@
-import mainConfig from "~~/config/common/main";
+import mainConfig from "../config/common/main";
 import type { BackendClient } from "../types/backend";
 import { SupabaseClient } from "./supabaseClient";
 import { PrismaClient } from "./prismaClient";
@@ -7,33 +7,18 @@ import {
   serverSupabaseServiceRole,
 } from "#supabase/server";
 
-let regularBackendClient: any;
-let bypassedBackendClient: any;
-
 export const getBackendClient = async (
   event: any,
   bypass: boolean = false
 ): Promise<BackendClient> => {
   if (mainConfig.BACKEND_PROVIDER === "supabase") {
-    if (bypass) {
-      if (!bypassedBackendClient) {
-        const supabaseClient = await serverSupabaseServiceRole(event);
-        bypassedBackendClient = new SupabaseClient(supabaseClient);
-      }
-      return bypassedBackendClient;
-    } else {
-      if (!regularBackendClient) {
-        const supabaseClient = await serverSupabaseClient(event);
-        regularBackendClient = new SupabaseClient(supabaseClient);
-      }
-      return regularBackendClient;
-    }
+    const supabaseClient = bypass
+      ? await serverSupabaseServiceRole(event)
+      : await serverSupabaseClient(event);
+    return new SupabaseClient(supabaseClient);
   } else if (mainConfig.BACKEND_PROVIDER === "prisma") {
-    if (!regularBackendClient) {
-      const { default: prisma } = await import("./prisma");
-      regularBackendClient = new PrismaClient(prisma);
-    }
-    return regularBackendClient;
+    const { default: prisma } = await import("./prisma");
+    return new PrismaClient(prisma);
   } else {
     throw new Error(
       `Unsupported backend provider: ${mainConfig.BACKEND_PROVIDER}`
