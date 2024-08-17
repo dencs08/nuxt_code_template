3fa1735b0a755f32cc70ae7471968e609ecd61f6
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-18 00:55:32 +0200

    refactor: Update assignRole function to use backend client and userSession

--- server/api/users/role/[id].post.ts
+++ server/api/users/role/[id].post.ts
@@ -1,10 +1,12 @@
 import { defineWrappedResponseHandler } from "~~/server/utils/defaultHandler";
+import { getBackendClient } from "~~/lib/backend";
 
-export default defineWrappedResponseHandler(async (event) => {
+export default defineWrappedResponseHandler(async (event, userSession) => {
   const body = await readBody(event);
+  const client = await getBackendClient(event, true);
   const { id } = event.context.params as { id: string };
 
-  if (!body.role || !body.id) {
+  if (!body.role_id || !body.id) {
     throw createError({
       statusCode: 400,
       statusMessage: "Missing required data",
@@ -17,6 +19,6 @@ export default defineWrappedResponseHandler(async (event) => {
     });
   }
 
-  const response = await assignRole(event, body);
+  const response = await client.assignRole(body.id, body.role_id, userSession);
   return response;
 }, 75);
