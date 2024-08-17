f76c9d891a21b61f6a755579356204911e422b7d
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-18 00:54:06 +0200

    refactor: Update role-dropdown component to use role name instead of role ID

--- app/components/utils/forms/dropdown/role-dropdown.vue
+++ app/components/utils/forms/dropdown/role-dropdown.vue
@@ -1,25 +1,37 @@
 <template>
-    <Select :modelValue="selectedRole" @update:modelValue="updateRole" :options="mutableRoles" optionLabel="label"
-        optionValue="value">
-        <template #option="slotProps">
-            <Tag :value="slotProps.option.label" :severity="getRoleSeverity(slotProps.option.value)" />
-        </template>
-    </Select>
+  <Select
+    :modelValue="selectedRole"
+    @update:modelValue="updateRole"
+    :options="mutableRoles"
+    optionLabel="name"
+    optionValue="name"
+  >
+    <template #option="slotProps">
+      <Tag
+        :value="slotProps.option.name"
+        :severity="getRoleSeverity(slotProps.option.name)"
+      />
+    </template>
+  </Select>
 </template>
 
 <script setup lang="ts">
+import type { Role } from "~~/types/roles";
 const props = defineProps({
-    modelValue: Object as () => Role | null
+  modelValue: Object as () => Role | null,
 });
 
-const { roles, getRoleSeverity } = useRoles();
-const emit = defineEmits(['update:modelValue']);
+const rolesStore = useRolesStore();
+const { getRoleSeverity } = rolesStore;
+const { roles } = storeToRefs(rolesStore);
+
+const emit = defineEmits(["update:modelValue"]);
 
 const selectedRole = ref<Role | null>(props.modelValue);
 const mutableRoles = computed(() => [...roles.value]);
 
 const updateRole = (newRole: Role) => {
-    selectedRole.value = newRole;
-    emit('update:modelValue', newRole);
+  selectedRole.value = newRole;
+  emit("update:modelValue", newRole);
 };
 </script>
