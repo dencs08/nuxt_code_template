0f91058a5bcacfccc8f2dda93ffbd1b60cb1e4ca
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-18 00:55:03 +0200

    add: new roles.ts config

---
+++ config/common/roles.ts
@@ -0,0 +1,9 @@
+export const roles = [
+  { id: 1, name: "EndUser", access_level: 5 },
+  { id: 2, name: "Guest", access_level: 0 },
+  { id: 3, name: "Subscriber", access_level: 10 },
+  { id: 4, name: "Editor", access_level: 25 },
+  { id: 5, name: "Manager", access_level: 50 },
+  { id: 6, name: "Admin", access_level: 75 },
+  { id: 7, name: "SuperAdmin", access_level: 100 },
+];
