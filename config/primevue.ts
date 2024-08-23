4af23f2f347fb5080fb4bb4a220da0def0343560
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-23 15:16:34 +0200

    refactor: Update PrimeVue configuration and CSS imports

--- config/primevue.ts
+++ config/primevue.ts
@@ -1,27 +1,27 @@
 // config/primevue.ts
 export default {
-    // importPT: { from: "@/assets/primevue/lara", as: "Lara" }, //import and apply preset
-    autoImport: true,
-    importTheme: { from: '@/assets/primevue/app-theme.js' },
-    options: {
-        ripple: false,
-        unstyled: false,
-        cssLayer: {
-            name: 'primevue',
-            order: 'tailwind-base, primevue, tailwind-utilities'
-        },
-        // inputStyle: "outlined",
-    },
-    // components: {
-    //     // prefix: "prime", //breaks sfxformkit componenets...
-    //     // include: "*",
-    //     exclude: ['AutoComplete', 'CascadeSelect', 'ColorPicker', 'Knob', 'Galleria', 'TreeTable', 'PickList','DataView', 'OrderList', 'OrganizationChart'],
+  // importPT: { from: "../app/assets/primevue/aura", as: "Aura" }, //passthrough stylings for unstyled mode
+  importTheme: { from: "@/assets/primevue/app-theme.js" },
+  autoImport: true,
+  options: {
+    ripple: false,
+    unstyled: false,
+    // cssLayer: {
+    //   name: "primevue",
+    //   order: "tailwind-base, primevue, tailwind-utilities",
     // },
-    // composables: {
-    //     // include: "*",
-    // },
-    // directives: {
-    //     // include: "*",
-    //     // exclude: ['Ripple']
-    // },
+    // inputStyle: "outlined",
+  },
+  // components: {
+  //     // prefix: "prime", //breaks sfxformkit componenets...
+  //     // include: "*",
+  //     exclude: ['AutoComplete', 'CascadeSelect', 'ColorPicker', 'Knob', 'Galleria', 'TreeTable', 'PickList','DataView', 'OrderList', 'OrganizationChart'],
+  // },
+  // composables: {
+  //     // include: "*",
+  // },
+  // directives: {
+  //     // include: "*",
+  //     // exclude: ['Ripple']
+  // },
 };
