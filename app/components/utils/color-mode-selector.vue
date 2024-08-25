fd54b7b7a4b3533e07ec8924f8c9b5186b90177f
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-25 13:38:31 +0200

    refactor: Update color-mode-selector component

--- app/components/utils/color-mode-selector.vue
+++ app/components/utils/color-mode-selector.vue
@@ -1,20 +1,29 @@
 <template>
-    <div>
-        <button v-if="type === 'button'" @click="toggle()" class="grid place-content-center">
-            <Icon v-auto-animate :name="isDark ? 'ic:outline-dark-mode' : 'ic:outline-light-mode'" class="text-lg" />
-        </button>
-        <InputSwitch v-else-if="type === 'switch'" v-model="isDark" />
-    </div>
+  <div>
+    <button
+      v-if="type === 'button'"
+      @click="toggle()"
+      class="grid place-content-center"
+    >
+      <Icon
+        v-auto-animate
+        :name="isDark ? 'ic:outline-dark-mode' : 'ic:outline-light-mode'"
+        class="text-lg"
+      />
+    </button>
+    <ToggleSwitch v-else-if="type === 'switch'" v-model="isDark" />
+  </div>
 </template>
 
 <script setup lang="ts">
 const isDark = useDark();
+const test = ref();
 const toggle = useToggle(isDark);
 
 const props = defineProps({
-    type: {
-        type: String,
-        default: 'button'
-    }
-})
+  type: {
+    type: String,
+    default: "button",
+  },
+});
 </script>
