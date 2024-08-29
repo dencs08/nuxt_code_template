c83311917e0ae41859f1752502ec073beeac84eb
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-29 22:30:21 +0200

    refactor: Update dashboard global middleware for improved permission handling

--- app/middleware/04.dashboard.global.ts
+++ app/middleware/04.dashboard.global.ts
@@ -1,7 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const layout = to.meta.layout;
-  if (layout != "dashboard") return;
+  if (layout !== "dashboard") return;
 
   const { hasPermission } = usePermissions();
-  if (!hasPermission("dashboard", "read")) return false;
+
+  if (!hasPermission("dashboard", "read")) {
+    return abortNavigation();
+  }
 });
