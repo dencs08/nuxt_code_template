f2118522f9793c78c50596d0cdbee2e2c57017a5
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-08-30 21:20:57 +0200

    refactor: Add @nuxt/test-utils/module to modules.ts for testing purposes

--- config/modules.ts
+++ config/modules.ts
@@ -5,6 +5,7 @@ export default [
   "@nuxt/devtools",
   "@nuxtjs/device",
   "nuxt-typed-router",
+  "@nuxt/test-utils/module",
   // "@nuxtjs/storybook", //right now breaks the app (enabling porbably forces ssr:false)
 
   //core (required for most template features)
