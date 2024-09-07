ec6aff0c553f61da196827522e7ecf490e548ec3
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-08 00:52:42 +0200

    refactor: Improve useDate formatDate function

--- app/core/composables/useDate.ts
+++ app/core/composables/useDate.ts
@@ -1,15 +1,35 @@
 export function useDate() {
-    const formatDate = (dateString: string) => {
-        let date = new Date(dateString);
+  const formatDate = (
+    dateString: string,
+    options: { includeTime?: boolean; padZeroes?: boolean } = {
+      includeTime: false,
+      padZeroes: true,
+    }
+  ) => {
+    let date = new Date(dateString);
 
-        let day = String(date.getDate()).padStart(2, '0');
-        let month = String(date.getMonth() + 1).padStart(2, '0'); // January is 0
-        let year = date.getFullYear();
+    let day = options.padZeroes
+      ? String(date.getDate()).padStart(2, "0")
+      : String(date.getDate());
+    let month = options.padZeroes
+      ? String(date.getMonth() + 1).padStart(2, "0")
+      : String(date.getMonth() + 1); // January is 0
+    let year = date.getFullYear();
 
-        let hours = String(date.getHours()).padStart(2, '0');
-        let minutes = String(date.getMinutes()).padStart(2, '0');
+    let formattedDate = `${day}-${month}-${year}`;
 
-        return `${day}-${month}-${year} ${hours}:${minutes}`;
-    };
-    return { formatDate };
+    if (options.includeTime) {
+      let hours = options.padZeroes
+        ? String(date.getHours()).padStart(2, "0")
+        : String(date.getHours());
+      let minutes = options.padZeroes
+        ? String(date.getMinutes()).padStart(2, "0")
+        : String(date.getMinutes());
+      formattedDate += ` ${hours}:${minutes}`;
+    }
+
+    return formattedDate;
+  };
+
+  return { formatDate };
 }
