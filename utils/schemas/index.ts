5ff6b8b004bb89bfc9a9cd543e993ee0bb188f0d
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-09 00:51:58 +0200

    refactor: Add new schema files for password update OTP and ban duration

--- utils/schemas/index.ts
+++ utils/schemas/index.ts
@@ -1,5 +1,7 @@
-export { default as userSchema } from './userSchema';
-export { default as loginSchema } from './loginSchema';
-export { default as registerSchema } from './registerSchema';
-export { default as passwordConfirmSchema } from './passwordConfirmSchema';
-export { default as emailSchema } from './emailSchema';
+export { default as userSchema } from "./userSchema";
+export { default as loginSchema } from "./loginSchema";
+export { default as registerSchema } from "./registerSchema";
+export { default as passwordConfirmSchema } from "./passwordConfirmSchema";
+export { default as passwordUpdateOtpSchema } from "./passwordUpdateOtpSchema";
+export { default as emailSchema } from "./emailSchema";
+export { default as banDurationSchema } from "./banDurationSchema";
