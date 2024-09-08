fd848a862b960994a5d15ee7324a2e93c91e3c77
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-09 00:53:58 +0200

    refactor: Users get will now return auth user with public user

--- app/auth/server/api/users/index.get.ts
+++ app/auth/server/api/users/index.get.ts
@@ -3,11 +3,28 @@ import { defineWrappedResponseHandler } from "../../../../../server/utils/defaul
 
 export default defineWrappedResponseHandler(async (event) => {
   const client = await getBackendClient(event, true);
-  let users = [];
 
   try {
-    users = await client.getUsers();
-    return users;
+    // users = await client.getUsers();
+    // return users;
+    const authData = (await client.getAuthUsers()) as any;
+
+    const authUsers: any[] = (authData.users as any[]).map((user) => ({
+      id: user.id,
+      email: user.email,
+    }));
+
+    const profileData = await client.getUsers();
+
+    const combinedData: any[] = (profileData as any[]).map((profile) => {
+      const authUser = authUsers.find((user) => user.id === profile.id);
+      return {
+        ...profile,
+        email: authUser ? authUser.email : null,
+      };
+    });
+
+    return combinedData;
   } catch (err) {
     throw createError({
       statusCode: 500,
