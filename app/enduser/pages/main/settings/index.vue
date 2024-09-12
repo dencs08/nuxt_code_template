79af6c9968e1decf6af5d05cd82d823c90f2de04
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-12 14:21:19 +0200

    feat: Add main app settings page

---
+++ app/enduser/pages/main/settings/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h4>General</h4>
+    <p class="mt-1.5">Main app settings</p>
+    <div
+      class="mt-8 divide-y divide-surface-200/40 dark:divide-surface-700/65 [&>*]:py-4"
+    >
+      <div v-for="setting in settings" :key="setting.name">
+        <div class="flex justify-between items-center">
+          <p
+            class="font-heading font-medium text-surface-500 dark:text-surface-200"
+          >
+            {{ setting.name }}
+          </p>
+          <component :is="setting.component" v-bind="setting.componentProps" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ColorModeSelector from "@/core/components/utils/color-mode-selector.vue";
+import LanguageSelector from "@/core/components/utils/forms/dropdown/i18n-dropdown.vue";
+
+definePageMeta({
+  layout: "main",
+});
+
+const settings = shallowRef([
+  {
+    name: "Dark mode",
+    component: ColorModeSelector,
+    componentProps: {
+      type: "switch",
+    },
+  },
+  {
+    name: "Language",
+    component: LanguageSelector,
+  },
+]);
+</script>
