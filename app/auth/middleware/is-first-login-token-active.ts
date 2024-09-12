6d6f7b4d2a71edd93332b8711d2337355a388e44
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-12 14:19:28 +0200

    feat: Add middleware to check if first login or password reset token is active

---
+++ app/auth/middleware/is-first-login-token-active.ts
@@ -0,0 +1,13 @@
+// middleware/check-reset-token.ts
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  if (import.meta.server) return;
+  try {
+    const data = await $fetch("/api/auth/is-first-login-token-active");
+    if (!data.response.exists) {
+      return navigateTo("/login");
+    }
+  } catch (error) {
+    console.error("Failed to check reset token:", error);
+    throw error;
+  }
+});
---
+++ app/auth/server/api/auth/is-first-login-token-active.ts
@@ -0,0 +1,22 @@
+import { defineWrappedResponseHandler } from "~~/server/utils/defaultHandler";
+
+export default defineWrappedResponseHandler(async (event, userSession) => {
+  const storage = useStorage();
+  try {
+    const isActive = await storage.getItem(
+      `user:${userSession.id}:first-login`
+    );
+    if (!userSession || !isActive) {
+      throw createError({
+        statusCode: 401,
+        statusMessage: "Unauthorized",
+      });
+    }
+    return { exists: true };
+  } catch (err: any) {
+    return {
+      error: "An error occurred",
+      response: err.message,
+    };
+  }
+}, 0);
