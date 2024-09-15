7dd21eeab37384f35a6d993b8215ee44431b682f
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-16 00:54:06 +0200

    refactor: Update runtime configuration to include SMTP credentials

--- config/runtime.ts
+++ config/runtime.ts
@@ -2,10 +2,16 @@
 import mainConfig from "./common/main";
 
 export default {
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PASS: process.env.SMTP_PASS,
   public: {
     global_route_access: mainConfig.GLOBAL_ROUTE_ACCESS,
     authProvider: mainConfig.AUTH_PROVIDER,
     backendProvider: mainConfig.BACKEND_PROVIDER,
     apiBase: process.env.BASE_API_URL,
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_SECURE: process.env.SMTP_SECURE,
+    SMTP_FROM: process.env.SMTP_FROM,
   },
 };
