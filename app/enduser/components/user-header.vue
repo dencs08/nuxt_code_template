6c3b4959b8b0c761c90407321dba7ac82c86ae19
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-17 00:35:17 +0200

    refactor: Update user-header.vue to improve user experience and add user menu functionality

--- app/enduser/components/user-header.vue
+++ app/enduser/components/user-header.vue
@@ -25,9 +25,12 @@
               :exact-active-class="'bg-surface-500/40 rounded'"
               class="text-sm font-semibold leading-6 text-surface-50 cursor-pointer"
             >
-              <Button size="small" class="py-0.5 px-3 text-surface-50" text>{{
-                item.label
-              }}</Button>
+              <Button
+                size="small"
+                class="!py-0.5 !px-3 !text-surface-50"
+                text
+                >{{ item.label }}</Button
+              >
             </NuxtLink>
           </div>
         </div>
@@ -47,18 +50,17 @@
             :to="localePath('/main/account')"
             class="flex flex-gap-2 items-center"
           >
-            <template v-if="isUserLoggedIn">
-              <ProfileAvatar class="rounded-full h-9 w-9 shadow-none" />
-              <template v-if="hasDashboardReadPermission">
-                <NuxtLink :to="localePath('/dashboard')">
-                  <Icon
-                    name="ic:baseline-space-dashboard"
-                    class="h-6 w-6 text-surface-300 hover:contrast-125 hover:brightness-125 transition ml-2"
-                  />
-                </NuxtLink>
+            <ClientOnly>
+              <template v-if="isUserLoggedIn">
+                <ProfileAvatar
+                  @click.prevent="toggleUserMenu"
+                  aria-haspopup="true"
+                  aria-controls="overlay_menu"
+                  class="rounded-full h-9 w-9 shadow-none"
+                />
               </template>
-            </template>
-            <template v-else></template>
+              <template v-else></template>
+            </ClientOnly>
           </NuxtLink>
         </div>
       </nav>
@@ -94,17 +96,19 @@
           <div class="overflow-y-auto mt-1 pl-2 pr-5">
             <div class="mt-3">
               <NuxtLink :to="localePath({ name: 'login' })">
-                <template v-if="isUserLoggedIn">
-                  <Icon name="ic:outline-account-circle" class="h-6 w-6" />
-                  <template v-if="hasDashboardReadPermission">
-                    <NuxtLink :to="localePath('/dashboard')">
-                      <Icon name="ic:outline-dashboard" class="h-6 w-6" />
-                    </NuxtLink>
+                <ClientOnly>
+                  <template v-if="isUserLoggedIn">
+                    <Icon name="ic:outline-account-circle" class="h-6 w-6" />
+                    <template v-if="hasDashboardReadPermission">
+                      <NuxtLink :to="localePath('/dashboard')">
+                        <Icon name="ic:outline-dashboard" class="h-6 w-6" />
+                      </NuxtLink>
+                    </template>
                   </template>
-                </template>
-                <template v-else>
-                  <MyButton class="w-full" size="xs"> Login </MyButton>
-                </template>
+                  <template v-else>
+                    <MyButton class="w-full" size="xs"> Login </MyButton>
+                  </template>
+                </ClientOnly>
               </NuxtLink>
             </div>
             <div class="w-full flex justify-center items-center">
@@ -114,6 +118,39 @@
         </div>
       </template>
     </Drawer>
+
+    <Menu
+      :model="userNavigation"
+      ref="userMenu"
+      id="overlay_menu"
+      :popup="true"
+      class="w-full md:w-56"
+      :pt="{
+        submenuLabel: ['!m-0 !pt-0 !pb-1.5 !px-1.5'],
+      }"
+    >
+      <template #submenulabel="{ item }">
+        <span class="text-xs text-muted-color !p-0 !m-0">{{ item.label }}</span>
+      </template>
+      <template #item="{ item, props }">
+        <a class="flex items-center !mt-0 !p-1" v-bind="props.action">
+          <span
+            :class="item.icon"
+            class="text-surface-200 dark:text-surface-300/50"
+          />
+          <span class="text-surface-400 dark:text-surface-100/80 text-sm">{{
+            item.label
+          }}</span>
+          <Badge v-if="item.badge" class="ml-auto" :value="item.badge" />
+          <span
+            v-if="item.shortcut"
+            class="ml-auto border border-surface-200 dark:border-surface-500 bg-emphasis text-muted-color rounded text-xs p-0.5"
+            >{{ item.shortcut }}</span
+          >
+        </a>
+      </template>
+      <template #end> </template>
+    </Menu>
   </header>
 </template>
 
@@ -124,6 +161,7 @@ const localePath = useLocalePath();
 const { isDarkMode } = useDarkMode();
 const userStore = useUserStore();
 const user = userStore.getUser;
+const { signOut } = useAuthentication();
 
 const sidebarOpen = ref(false);
 const lastScrollPosition = ref(0);
@@ -142,6 +180,73 @@ const { hasPermission } = usePermissions();
 const hasDashboardReadPermission = computed(() =>
   hasPermission("dashboard", "read")
 );
+
+const userMenu = ref();
+
+const toggleUserMenu = (event: any) => {
+  userMenu.value.toggle(event);
+};
+
+const userNavigation = computed(() => {
+  const baseNavigation = [
+    {
+      label: "Hi, " + user.email,
+      items: [
+        {
+          separator: true,
+        },
+        {
+          label: "Your profile",
+          icon: "pi pi-user",
+          command: () => {
+            navigateTo(localePath("/main/account"));
+          },
+        },
+        {
+          separator: true,
+        },
+        {
+          label: "Settings",
+          icon: "pi pi-cog",
+          command: () => {
+            navigateTo(localePath("/main/settings"));
+          },
+        },
+        {
+          label: "Help",
+          icon: "pi pi-question-circle",
+          command: () => {
+            console.log("Help");
+          },
+        },
+        {
+          separator: true,
+        },
+        {
+          label: "Sign out",
+          icon: "pi pi-sign-out",
+          command: async () => {
+            await signOut();
+            navigateTo(localePath({ name: "login" }));
+          },
+        },
+      ],
+    },
+  ];
+
+  // Add dashboard item only if user has permission
+  if (hasDashboardReadPermission.value) {
+    baseNavigation[0].items.splice(3, 0, {
+      label: "Dashboard",
+      icon: "pi pi-home",
+      command: () => {
+        navigateTo(localePath("/dashboard/home"));
+      },
+    });
+  }
+
+  return baseNavigation;
+});
 </script>
 
 <style scoped></style>
