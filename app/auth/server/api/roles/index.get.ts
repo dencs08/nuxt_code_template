8c6d3e2e753d97046aa155487fdc42fa8f97c89b
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-18 10:30:16 +0200

    refactor: Import and use controller functions for roles and users in API endpoints

--- app/auth/server/api/roles/index.get.ts
+++ app/auth/server/api/roles/index.get.ts
@@ -1,4 +1,5 @@
 import { H3Event } from "h3";
+import { getRoles } from "~~/server/controllers/rolesController";
 
 export default defineEventHandler(async (event: H3Event) => {
   const response = await getRoles(event);
--- app/auth/server/api/users/index.get.ts
+++ app/auth/server/api/users/index.get.ts
@@ -1,5 +1,5 @@
 import type { H3Event } from "h3";
-
+import { getUsers } from "~~/server/controllers/usersController";
 export default defineEventHandler(async (event: H3Event) => {
   const response = await getUsers(event);
   return { response };
