68520ab64f7c6a35401dfff8098c1ec0b4fc2366
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-18 10:43:37 +0200

    refactor: Update metrics display and loading state in dashboardhome

--- app/dashboard/pages/dashboard/home/index.vue
+++ app/dashboard/pages/dashboard/home/index.vue
@@ -8,9 +8,10 @@
             :key="index"
             class="flex-none md:flex-grow w-1/2 md:w-1/3 xl:w-1/5"
             :title="stat.title"
-            :value="metrics[stat.metricKey]"
-            :change="metrics[stat.changeKey]"
+            :value="metricsStore.metrics[stat.metricKey]"
+            :change="metricsStore.metrics[stat.changeKey]"
             @period-change="updateMetric(stat.metricKey, $event)"
+            :loading="metricsStore.metricsLoading"
           />
         </div>
       </HorizontalScroller>
@@ -18,20 +19,25 @@
     <div class="flex flex-col md:flex-row gap-2">
       <div class="w-full md:w-2/3 lg:w-3/4 md:overflow-y-auto space-y-2">
         <div>
-          <div v-if="metricsLoading">Loading...</div>
-          <div v-else>
-            <Card class="custom-card">
-              <template #content>
-                <ChartCountries
-                  title="Users"
-                  :data="chartJsData"
+          <Card class="custom-card">
+            <template #content>
+              <div v-if="metricsStore.chartDataLoading">
+                <Skeleton
+                  width="100%"
+                  height="300px"
+                  borderRadius="16px"
+                ></Skeleton>
+              </div>
+              <div v-else>
+                <ChartBase
+                  title="Monthly Metrics"
+                  :datasets="metricsStore.datasets"
+                  :labels="metricsStore.months"
                   type="bar"
-                  xKey="country"
-                  yKey="count"
                 />
-              </template>
-            </Card>
-          </div>
+              </div>
+            </template>
+          </Card>
         </div>
 
         <div>
@@ -43,7 +49,7 @@
                 class="flex-none md:flex-grow md:w-1/3 xl:w-1/4 cursor-pointer"
               >
                 <Card
-                  class="no-content p-5 custom-card"
+                  class="no-content p-5 custom-card group hover:!shadow-md transition-shadow duration-300"
                   @click="handleAction(action, index)"
                 >
                   <template #header>
@@ -61,10 +67,14 @@
                         <div class="flex gap-2">
                           <Icon
                             :name="action.icon"
-                            class="h-6 w-auto transition-colors"
+                            class="h-6 w-auto transition group-hover:scale-110"
                             :class="action.color"
                           />
-                          <p class="text-sm md:text-base">{{ action.title }}</p>
+                          <p
+                            class="text-sm md:text-base group-hover:scale-105 group-hover:translate-x-1 transition"
+                          >
+                            {{ action.title }}
+                          </p>
                         </div>
                       </template>
                     </div>
@@ -102,67 +112,31 @@
 </template>
 
 <script setup lang="ts">
-import { useRouter } from "vue-router";
+import { useMetricsStore } from "~/dashboard/stores/MetricsStore";
+import type { NumericMetricKey, StatDisplayItem } from "~~/types/metrics";
 
 definePageMeta({
   layout: "dashboard",
 });
 
+const metricsStore = useMetricsStore();
 const userStore = useUserStore();
 const user = userStore.getUser;
 const localePath = useLocalePath();
 const { hasAccess } = useRoleCheck();
 const { addToast } = useToastService();
+const { isDarkMode, toggleDarkMode } = useDarkMode();
 const isAdmin = hasAccess(75);
 const router = useRouter();
-const { isDarkMode, toggleDarkMode } = useDarkMode();
 
-type NumericMetricKey =
-  | "newSignups"
-  | "newSignupsChange"
-  | "totalUsers"
-  | "totalUsersChange"
-  | "pageViews"
-  | "pageViewsChange"
-  | "uniqueVisitors"
-  | "uniqueVisitorsChange";
-
-interface MetricsData {
-  newSignups: number;
-  newSignupsChange: number;
-  totalUsers: number;
-  totalUsersChange: number;
-  pageViews: number;
-  pageViewsChange: number;
-  uniqueVisitors: number;
-  uniqueVisitorsChange: number;
-  chartData: any; // Replace 'any' with the appropriate type if possible
-}
-
-// Define the metrics object
-const metrics = ref<
-  Record<NumericMetricKey, number> & {
-    chartData: { [key: string]: number };
+onMounted(() => {
+  if (!metricsStore.metricsLoaded) {
+    metricsStore.fetchMetrics();
   }
->({
-  newSignups: 0,
-  newSignupsChange: 0,
-  totalUsers: 0,
-  totalUsersChange: 0,
-  pageViews: 0,
-  pageViewsChange: 0,
-  uniqueVisitors: 0,
-  uniqueVisitorsChange: 0,
-  chartData: {},
+  if (!metricsStore.chartDataLoaded) {
+    metricsStore.fetchChartData();
+  }
 });
-
-const metricsLoading = ref(true);
-
-interface StatDisplayItem {
-  title: string;
-  metricKey: NumericMetricKey;
-  changeKey: NumericMetricKey;
-}
 
 const statDisplays = ref<StatDisplayItem[]>([
   {
@@ -187,90 +161,34 @@ const statDisplays = ref<StatDisplayItem[]>([
   },
 ]);
 
-const updateMetric = async (metricKey: NumericMetricKey, period: any) => {
-  metricsLoading.value = true;
-  await fetchMetrics(period);
-  metricsLoading.value = false;
-};
-
-const chartJsData = computed(() => {
-  return Object.entries(metrics.value.chartData).map(([country, count]) => ({
-    country,
-    count,
-  }));
-});
-
-const fetchMetrics = async (period = "1d") => {
-  metricsLoading.value = true;
-  try {
-    // Use $fetch to call the API endpoint
-    const metricsData = await $fetch<MetricsData>("/api/v1/analytics", {
-      method: "GET",
-      params: { period },
-    });
-
-    // Update metrics
-    metrics.value = {
-      newSignups: metricsData.newSignups,
-      newSignupsChange: metricsData.newSignupsChange,
-      totalUsers: metricsData.totalUsers,
-      totalUsersChange: metricsData.totalUsersChange,
-      pageViews: metricsData.pageViews,
-      pageViewsChange: metricsData.pageViewsChange,
-      uniqueVisitors: metricsData.uniqueVisitors,
-      uniqueVisitorsChange: metricsData.uniqueVisitorsChange,
-      chartData: metricsData.chartData,
-    };
-  } catch (error) {
-    console.error("Error fetching metrics:", error);
-  } finally {
-    metricsLoading.value = false;
-  }
-};
-
-onMounted(() => {
-  fetchMetrics();
-});
-
-const cacheClear = async () => {
-  try {
-    const response = await $fetch("/api/v1/invalidate-cache", {
-      method: "POST",
-    });
-    addToast("success", "Success", "Cache has been cleared successfully");
-  } catch {
-    addToast("error", "Error", "An error occurred while clearing cache");
-  }
-};
-
 const quickActions = ref([
   {
     title: "View users",
     icon: "ic:round-person",
-    color: "text-primary-500 hover:text-primary-700",
-    action: () => router.push(localePath("/dashboard/admin/users")),
+    color: "text-primary-500 group-hover:text-primary-600",
+    action: () => navigateTo(localePath("/dashboard/admin/users")),
     loading: false,
   },
   {
     title: "Send emails",
     icon: "ic:round-mail",
-    color: "text-secondary-500 hover:text-secondary-700",
-    action: () => router.push(localePath("/dashboard/mail/templates")),
+    color: "text-secondary-500 group-hover:text-secondary-600",
+    action: () => navigateTo(localePath("/dashboard/mail/templates")),
     loading: false,
   },
   {
     title: "Check analytics",
     icon: "ic:sharp-data-exploration",
-    color: "text-primary-500 hover:text-primary-700",
+    color: "text-primary-500 group-hover:text-primary-600",
     action: () => {
-      console.log("Checking analytics...");
+      navigateTo(localePath("/dashboard/admin/analytics/users"));
     },
     loading: false,
   },
   {
     title: "Change color mode",
     icon: `ic:outline-${isDarkMode ? "light" : "dark"}-mode`,
-    color: "text-secondary-500 hover:text-secondary-700",
+    color: "text-secondary-500 group-hover:text-secondary-600",
     action: () => {
       toggleDarkMode();
     },
@@ -279,11 +197,24 @@ const quickActions = ref([
   {
     title: "Clear cache",
     icon: "ic:round-delete",
-    color: "text-red-500 hover:text-red-700",
-    action: cacheClear,
+    color: "text-red-500 group-hover:text-red-600",
+    action: async () => {
+      try {
+        await $fetch("/api/v1/invalidate-cache", {
+          method: "POST",
+        });
+        addToast("success", "Success", "Cache has been cleared successfully");
+      } catch {
+        addToast("error", "Error", "An error occurred while clearing cache");
+      }
+    },
     loading: false,
   },
 ]);
+
+const updateMetric = async (metricKey: NumericMetricKey, period: any) => {
+  await metricsStore.fetchMetrics(period);
+};
 
 const handleAction = async (action: any, index: number) => {
   if (typeof action.action === "function") {
