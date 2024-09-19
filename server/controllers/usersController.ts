7f6a2d0fc29319f9c8ac1ba0f5f9169ef561ff82
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-19 12:04:49 +0200

    refactor: Update getUsers to handle cache bypass based on query parameter (wip)

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -2,6 +2,7 @@ import type { H3Event } from "h3";
 
 const getUsersUncached = defineCachedFunction(async (event: any) => {
   const client = event.context.backendClient;
+  // const timestamp = new Date().toISOString();
 
   try {
     const authData = (await client.getAuthUsers()) as any;
@@ -34,10 +35,24 @@ export const getUsers = defineCachedFunction(
   async (event: H3Event) => await getUsersUncached(event),
   {
     maxAge: 60 * 60, // 1 hour
-    swr: true,
-    staleMaxAge: 60 * 60 * 24, // 24 hours
+    // swr: false,
+    // staleMaxAge: 60 * 60 * 24, // 24 hours
     name: "getUsers",
-    getKey: (event) => "default",
-    shouldBypassCache: (e) => getQuery(e).force === "true",
+    // getKey: (event) => "default",
+    shouldBypassCache: async (e) => {
+      if (getQuery(e).force) {
+        const storage = useStorage("cache");
+        const handlerKeys = await storage.getKeys("nitro:handlers");
+        const functionKeys = await storage.getKeys("nitro:functions:getRoles");
+        const cacheKeys = [...handlerKeys, ...functionKeys];
+        await Promise.all(
+          cacheKeys.map((element) => storage.removeItem(element))
+        );
+        console.log(`cacheKeys: ${cacheKeys}`);
+        return true;
+      } else {
+        return false;
+      }
+    },
   }
 );
