93966741e87e3ae30cdb2832a54c60e0fe19438e
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-09-28 23:14:23 +0200

    refactor: user-creator now isnt a button but a dialog which can be called

--- app/dashboard/components/dashboard/admin/user-creator.vue
+++ app/dashboard/components/dashboard/admin/user-creator.vue
@@ -1,84 +1,72 @@
 <template>
-  <div>
-    <div>
-      <Button
-        size="small"
-        label="Add"
-        icon="pi pi-plus"
-        severity="success"
-        @click="visible = true"
-      />
-    </div>
-    <Dialog
-      v-model:visible="visible"
-      maximizable
-      header="Header"
-      :style="{ width: '25rem' }"
-      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
-      position="bottomleft"
-    >
-      <template #header>
-        <div
-          class="inline-flex align-items-center justify-content-center gap-2"
-        >
-          <span class="font-bold white-space-nowrap">Create a new user</span>
-        </div>
-      </template>
-      <div class="m-0">
-        <FormWrapper
-          :zodSchema="userSchema"
-          :handleSubmit="submitForm"
-          submitLabel="Create"
-        >
-          <div class="space-y-2 mb-5">
-            <FormKit
-              class="w-full"
-              type="primeInputText"
-              name="email"
-              validation="required|email"
-              placeholder="Email"
-            />
-            <FormKit
-              class="w-full"
-              type="primeInputText"
-              name="name"
-              validation="required|length:3"
-              placeholder="Name"
-            />
-            <FormKit
-              class="w-full"
-              type="primePassword"
-              name="password"
-              validation="required|length:6"
-              toggleMask
-              :feedback="true"
-              placeholder="Password"
-            />
-            <FormKit
-              class="w-full"
-              type="primePassword"
-              name="password_confirm"
-              validation="required|confirm"
-              toggleMask
-              placeholder="Repeat password"
-            />
-            <FormKit
-              class="w-full"
-              type="primeSelect"
-              :options="rolesOptions"
-              optionValue="id"
-              optionLabel="name"
-              name="role_id"
-              placeholder="Select user role"
-              validation="required"
-            />
-            <!-- <FileUpload mode="basic" name="demo[]" url="/api/upload" accept="image/*" :maxFileSize="1000000"
-                                @upload="onUpload" chooseLabel="Avatar" /> -->
-          </div>
-        </FormWrapper>
+  <Dialog
+    :visible="visible"
+    @update:visible="$emit('update:visible', $event)"
+    maximizable
+    header="Header"
+    :style="{ width: '25rem' }"
+    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+    position="bottomleft"
+  >
+    <template #header>
+      <div class="inline-flex align-items-center justify-content-center gap-2">
+        <span class="font-bold white-space-nowrap">Create a new user</span>
       </div>
-    </Dialog>
-  </div>
+    </template>
+    <div class="m-0">
+      <FormWrapper
+        :zodSchema="userSchema"
+        :handleSubmit="submitForm"
+        submitLabel="Create"
+      >
+        <div class="space-y-2 mb-5">
+          <FormKit
+            class="w-full"
+            type="primeInputText"
+            name="email"
+            validation="required|email"
+            placeholder="Email"
+          />
+          <FormKit
+            class="w-full"
+            type="primeInputText"
+            name="name"
+            validation="required|length:3"
+            placeholder="Name"
+          />
+          <FormKit
+            class="w-full"
+            type="primePassword"
+            name="password"
+            validation="required|length:6"
+            toggleMask
+            :feedback="true"
+            placeholder="Password"
+          />
+          <FormKit
+            class="w-full"
+            type="primePassword"
+            name="password_confirm"
+            validation="required|confirm"
+            toggleMask
+            placeholder="Repeat password"
+          />
+          <FormKit
+            class="w-full"
+            type="primeSelect"
+            :options="rolesOptions"
+            optionValue="id"
+            optionLabel="name"
+            name="role_id"
+            placeholder="Select user role"
+            validation="required"
+          />
+          <!-- <FileUpload mode="basic" name="demo[]" url="/api/upload" accept="image/*" :maxFileSize="1000000"
+                                @upload="onUpload" chooseLabel="Avatar" /> -->
+        </div>
+      </FormWrapper>
+    </div>
+  </Dialog>
 </template>
 
 <script setup lang="ts">
@@ -86,11 +74,19 @@ import { userSchema } from "~~/utils/schemas";
 import type { UserForm } from "~~/types/user";
 import { useUsersStore, type AddUserRequest } from "@/auth/stores/UsersStore";
 
+const props = defineProps({
+  visible: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:visible", "userCreated"]);
+
 const { submit, error } = useForm();
 const usersStore = useUsersStore();
 const rolesStore = useRolesStore();
 const { roles } = storeToRefs(rolesStore);
-const visible = ref(false);
 
 const rolesOptions = computed(() =>
   roles.value.map((role) => ({ name: role.name, id: role.id }))
@@ -119,13 +115,11 @@ const submitForm = (formData: UserForm) => {
   })
     .then(() => {
       console.log("User creation successful, closing dialog");
-      visible.value = false;
+      emit("update:visible", false);
       emit("userCreated");
     })
     .catch((error) => {
       console.error("Error creating user:", error);
     });
 };
-
-const emit = defineEmits(["userCreated"]);
 </script>
