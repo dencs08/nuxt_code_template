76660d8f87da259ad0847b9fde74fa3d8b1f9d78
dencs08 <59261630+dencs08@users.noreply.github.com>
2024-10-02 15:18:16 +0200

    refactor: getUsers endpoint dynamic return based on params

--- app/auth/server/api/v1/users/index.get.ts
+++ app/auth/server/api/v1/users/index.get.ts
@@ -1,7 +1,14 @@
 import type { H3Event } from "h3";
-import { getUsers } from "~~/server/controllers/usersController";
+import { getUsers, getUsersByIds } from "~~/server/controllers/usersController";
 
 export default defineEventHandler(async (event: H3Event) => {
+  const query = getQuery(event);
+
+  if (query.userIds) {
+    const response = await getUsersByIds(event);
+    return response;
+  }
+
   const response = await getUsers(event);
   return response;
 });
